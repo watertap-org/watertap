a94afb2194d02465207fd40b46960a9a7c3b6dcc
Ethan Young <ethan.young@nrel.gov>
2021-04-22 12:10:36 -0600

    adding unit tests for parallel param sweep tools

--- proteuslib/tools/parallel_manager.py
+++ proteuslib/tools/parallel_manager.py
@@ -5,6 +5,33 @@ import os
 
 # ================================================================
 
+def init_mpi(mpi_comm=None):
+    if mpi_comm is None:
+        try:
+            from mpi4py import MPI
+
+        except:
+            print('The parallel manager functions require a version')
+            print('of mpi4py to be installed in this environment.')
+            print('Defaulting to num_procs = 1, rank = 0.')
+
+            comm = None
+            rank = 0
+            num_procs = 1
+
+        else:
+            comm = MPI.COMM_WORLD
+            rank = comm.Get_rank()
+            num_procs = comm.Get_size()
+
+    else:
+        print('Passing MPI communicators is not currently supported')
+        comm, rank, num_procs = init_mpi()
+
+    return comm, rank, num_procs
+
+# ================================================================
+
 def build_and_divide_combinations(d, rank, num_procs):
 
     param_values = []
@@ -60,46 +87,39 @@ def update_model_values(m, param_dict=None, values=None):
 
 # ================================================================
 
+def aggregate_results(local_results, global_values, comm, num_procs):
+
+    if num_procs > 1:
+        local_results = local_results.astype(np.float64)
+
+        global_results = np.zeros((np.shape(global_values)[0], np.shape(local_results)[1]), dtype=np.float64)
+
+        # Collect the number of result values to be sent from each process
+        send_counts = np.zeros(num_procs, dtype=np.int64)
+        comm.Gather(np.int64(np.size(local_results)), send_counts, root=0)
+
+        # Collect the global results results onto rank 0
+        comm.Gatherv(local_results, (global_results, send_counts), root=0)
+
+    else:
+        global_results = np.copy(local_results)
+
+    return global_results
+
+# ================================================================
+
 def run_param_sweep(m, sweep_params, outputs, output_dir='output', mpi_comm=None,
     num_stages=2, optimization=None, display_metrics=None):
 
     # Get an MPI communicator
-    if mpi_comm is None:
-        try:
-            from mpi4py import MPI
-
-        except:
-            print('The parallel manager functions require a version')
-            print('of mpi4py to be installed in this environment.')
-            print('Defaulting to rank = 0, num_procs = 1.')
-
-            comm = None
-            rank = 0
-            num_procs = 1
-
-        else:
-            comm = MPI.COMM_WORLD
-            rank = comm.Get_rank()
-            num_procs = comm.Get_size()
-
-    else:
-        print('Passing MPI communicators is not currently supported')
-        comm = None
-        rank = 0
-        num_procs = 1
-
-    # Make a directory for saved outputs
-    os.makedirs('%s' % (output_dir), exist_ok=True)
+    comm, rank, num_procs = init_mpi(mpi_comm)
 
     # Enumerate all possibilities and divide the workload between processors
     local_values, global_values = build_and_divide_combinations(sweep_params, rank, num_procs)
 
     # Initialize space to hold results
-    local_num_cases = np.int64(np.shape(local_values)[0])
-    local_results = np.zeros((local_num_cases, len(outputs)), dtype=np.float64)
-
-    global_num_cases = np.int64(np.shape(global_values)[0])
-    global_results = np.zeros((global_num_cases, len(outputs)), dtype=np.float64)
+    local_num_cases = np.shape(local_values)[0]
+    local_results = np.zeros((local_num_cases, len(outputs)))
 
     # ================================================================
     # Run all optimization cases
@@ -131,28 +151,34 @@ def run_param_sweep(m, sweep_params, outputs, output_dir='output', mpi_comm=None
     # Save results
     # ================================================================
 
-    # Save the local results to a file (helpful for parallel debugging)
-    fname = '%s/local_results_%03d.csv' % (output_dir, rank)
-    save_data = np.hstack((local_values, local_results))
+    global_results = aggregate_results(local_results, global_values, comm, num_procs)
 
+    # Make a directory for saved outputs
+    if rank == 0:
+        os.makedirs('%s' % (output_dir), exist_ok=True)
+
+    if num_procs > 1:
+        comm.Barrier()
+
+    # Write a header string for all data files
     data_header = ''
     for k, v in sweep_params.items():
         data_header += '%s, ' % (k)
     data_header += ', '.join(outputs)
 
+    # Create the local filename and data
+    fname = '%s/local_results_%03d.csv' % (output_dir, rank)
+    save_data = np.hstack((local_values, local_results))
+
+    # Save the local data
     np.savetxt(fname, save_data, header=data_header, delimiter=', ', fmt='%.6e')
 
-    # Collect the number of result values to be sent from each process
-    send_counts = np.zeros(num_procs, dtype=np.int64)
-    comm.Gather(np.int64(np.size(local_results)), send_counts, root=0)
-
-    # Collect the global results results onto rank 0
-    comm.Gatherv(local_results, (global_results, send_counts), root=0)
-
     if rank == 0:
-        # Save the global results to a file
+        # Create the global filename and data
         fname = '%s/%d_stage.csv' % (output_dir, num_stages)
         save_data = np.hstack((global_values, global_results))
+
+        # Save the global data
         np.savetxt(fname, save_data, header=data_header, delimiter=', ', fmt='%.6e')
 
 # ================================================================
--- proteuslib/tools/tests/test_parallel_manager.py
+++ proteuslib/tools/tests/test_parallel_manager.py
@@ -1,37 +1,187 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2020, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# Please see the files COPYRIGHT.txt and LICENSE.txt for full copyright and
+# license information, respectively. Both files are also available online
+# at the URL "https://github.com/IDAES/idaes-pse".
+##############################################################################
+
 import pytest
 import numpy as np
-import sys
-import os
 
-from sim_LSRRO_Nstage import build_model, simulate, optimization, display_metrics
-from proteuslib.tools.parallel_manager import run_param_sweep
+from pyomo.environ import (ConcreteModel,
+                           TerminationCondition,
+                           SolverStatus,
+                           value,
+                           Objective,
+                           SolverFactory)
 
-# ================================================================
-# Build and simulate the model once as an initial condition
-# ================================================================
+from idaes.core import FlowsheetBlock
+from idaes.core.util.testing import get_default_solver
+from idaes.core.util.scaling import calculate_scaling_factors
 
-if len(sys.argv) > 1:
-    num_stages = int(sys.argv[1])
-else:
-    num_stages = 2
+from proteuslib.unit_models.reverse_osmosis_0D import ReverseOsmosis0D
+import proteuslib.property_models.NaCl_prop_pack as props
+from proteuslib.tools.parallel_manager import (init_mpi,
+                                               build_and_divide_combinations,
+                                               update_model_values,
+                                               aggregate_results,
+                                               run_param_sweep)
 
-m = build_model(N=num_stages)
-m = simulate(m, N=num_stages);
+# -----------------------------------------------------------------------------
 
-# ================================================================
-# Set parameter sweep options
-# sweep_params['Short/Pretty-print Name'] =
-# (path.to.model.variable, lower_limit, upper_limit, num_samples)
-# ================================================================
+class TestParallelManager():
+    @pytest.fixture(scope="class")
+    def RO_frame(self):
+        m = ConcreteModel()
+        m.fs = FlowsheetBlock(default={"dynamic": False})
 
-sweep_params = dict()
-sweep_params['Membrane Cost'] = (m.fs.costing_param.mem_cost, 15, 60, 4)
-sweep_params['Electricity Cost']  = (m.fs.costing_param.electricity_cost, 0.03, 0.15, 4)
-# sweep_params['Water Permeability'] = (m.fs.Stage[:].A, 2e-12, 10e-12, 4)
+        m.fs.properties = props.NaClParameterBlock()
 
-# ================================================================
-# Run the parameter sweep
-# ================================================================
+        m.fs.unit = ReverseOsmosis0D(default={
+            "property_package": m.fs.properties,
+            "has_pressure_change": True, })
 
-run_param_sweep(m, sweep_params, ['EC', 'LCOW'], output_dir='output',
-    num_stages=num_stages, optimization=optimization, display_metrics=display_metrics)
+        # fully specify system
+        feed_flow_mass = 1
+        feed_mass_frac_NaCl = 0.035
+        feed_pressure = 50e5
+        feed_temperature = 273.15 + 25
+        membrane_pressure_drop = 3e5
+        membrane_area = 50
+        A = 4.2e-12
+        B = 3.5e-8
+        pressure_atmospheric = 101325
+
+        feed_mass_frac_H2O = 1 - feed_mass_frac_NaCl
+        m.fs.unit.inlet.flow_mass_phase_comp[0, 'Liq', 'NaCl'].fix(
+            feed_flow_mass * feed_mass_frac_NaCl)
+        m.fs.unit.inlet.flow_mass_phase_comp[0, 'Liq', 'H2O'].fix(
+            feed_flow_mass * feed_mass_frac_H2O)
+        m.fs.unit.inlet.pressure[0].fix(feed_pressure)
+        m.fs.unit.inlet.temperature[0].fix(feed_temperature)
+        m.fs.unit.deltaP.fix(-membrane_pressure_drop)
+        m.fs.unit.area.fix(membrane_area)
+        m.fs.unit.A_comp.fix(A)
+        m.fs.unit.B_comp.fix(B)
+        m.fs.unit.permeate.pressure[0].fix(pressure_atmospheric)
+        return m
+
+    @pytest.mark.unit
+    def test_init_mpi(self):
+        comm, rank, num_procs = init_mpi()
+
+        assert type(rank) == int
+        assert type(num_procs) == int
+        assert 0 <= rank < num_procs
+
+    @pytest.mark.component
+    def test_build_and_divide_combinations(self):
+        comm, rank, num_procs = init_mpi()
+
+        nn_A = 4
+        nn_B = 5
+        nn_C = 6
+
+        param_dict = dict()
+        param_dict['var_A'] = (None, 0.0, 1.0, nn_A)
+        param_dict['var_B']  = (None, 1.0, 2.0, nn_B)
+        param_dict['var_C']  = (None, 2.0, 3.0, nn_C)
+
+        local_combo_array, full_combo_array = build_and_divide_combinations(param_dict, rank, num_procs)
+
+        test = np.array_split(full_combo_array, num_procs, axis=0)[rank]
+
+        assert np.shape(full_combo_array)[0] == nn_A*nn_B*nn_C
+        assert np.shape(full_combo_array)[1] == 3
+        assert np.shape(local_combo_array)[1] == 3
+
+        assert test[-1, 0] == pytest.approx(local_combo_array[-1, 0])
+        assert test[-1, 1] == pytest.approx(local_combo_array[-1, 1])
+        assert test[-1, 2] == pytest.approx(local_combo_array[-1, 2])
+
+        if rank == 0:
+            assert test[0, 0] == pytest.approx(0.0)
+            assert test[0, 1] == pytest.approx(1.0)
+            assert test[0, 2] == pytest.approx(2.0)
+
+        if rank == num_procs - 1:
+            assert test[-1, 0] == pytest.approx(1.0)
+            assert test[-1, 1] == pytest.approx(2.0)
+            assert test[-1, 2] == pytest.approx(3.0)
+
+    @pytest.mark.component
+    def test_update_model_values(self, RO_frame):
+        m = RO_frame
+
+        param_dict = dict()
+        param_dict['pressure'] = (m.fs.unit.inlet.pressure[0], None, None, None)
+        param_dict['area'] = (m.fs.unit.area, None, None, None)
+
+        assert hasattr(m.fs.unit.inlet, 'pressure')
+        assert hasattr(m.fs.unit, 'area')
+
+        original_pressure = value(m.fs.unit.inlet.pressure[0])
+        original_area = value(m.fs.unit.area)
+
+        new_values = [1.1*original_pressure, 1.1*original_area]
+
+        update_model_values(m, param_dict=param_dict, values=new_values)
+
+        assert value(m.fs.unit.inlet.pressure[0]) == pytest.approx(new_values[0])
+        assert value(m.fs.unit.area) == pytest.approx(new_values[1])
+
+    @pytest.mark.unit
+    def test_aggregate_results(self):
+        comm, rank, num_procs = init_mpi()
+
+        nn = 5
+        np.random.seed(1)
+        local_results = (rank+1)*np.random.rand(nn, 2)
+        global_values = np.random.rand(nn*num_procs, 4)
+
+        global_results = aggregate_results(local_results, global_values, comm, num_procs)
+
+        assert np.shape(global_results)[1] == np.shape(local_results)[1]
+        assert np.shape(global_results)[0] == np.shape(global_values)[0]
+
+        if rank == 0:
+            assert global_results[0, 0] == pytest.approx(local_results[0, 0])
+            assert global_results[0, 1] == pytest.approx(local_results[0, 1])
+            assert global_results[-1, 0] == pytest.approx(num_procs*local_results[-1, 0])
+            assert global_results[-1, 1] == pytest.approx(num_procs*local_results[-1, 1])
+
+    @pytest.mark.component
+    def test_run_param_sweep(self, RO_frame):
+        # m, sweep_params, outputs, output_dir='output', mpi_comm=None, num_stages=2, optimization=None, display_metrics=None
+
+        m = RO_frame
+
+        calculate_scaling_factors(m)
+
+        solver = get_default_solver()
+        solver.options = {'nlp_scaling_method': 'user-scaling'}
+        results = solver.solve(m)
+        print(results)
+
+        assert results.solver.termination_condition == \
+               TerminationCondition.optimal
+        assert results.solver.status == SolverStatus.ok
+
+        m.fs.objective = Objective(expr=m.fs.unit.area)
+
+        m.fs.unit.B_comp.unfix()
+        m.fs.unit.B_comp.setlb(3.5e-8)
+        m.fs.unit.B_comp.setub(3.5e-8 * 1e3)
+
+        # solver = SolverFactory('ipopt')
+        solver = SolverFactory('ipopt')
+        solver.options = {'nlp_scaling_method': 'user-scaling'}
+        results = solver.solve(m, tee=True)
+
+        assert results.solver.termination_condition == TerminationCondition.optimal
