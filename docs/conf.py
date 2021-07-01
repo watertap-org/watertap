0dc9b30cd6b0881b9d28b63919e76154e16fb6f7
Keith Beattie <KSBeattie@lbl.gov>
2021-06-30 17:36:16 -0700

    0.2.0dev (not dev0)

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,9 @@ copyright = '2021, NAWI'
 author = 'NAWI'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0dev0'
+release = '0.2.0dev'
 # The short X.Y version
-version = '0.2.0dev0'
+version = '0.2.0dev'
 # -- General configuration ---------------------------------------------------
 
 
