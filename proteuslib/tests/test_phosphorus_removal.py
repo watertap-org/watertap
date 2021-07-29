25a02b360175bc9ce6a22a4f3c6fee208614ba88
Austin Ladshaw <ladshawap@ornl.gov>
2021-07-29 10:39:19 -0400

    Updated test values based on updates to IDAES molarity calculations

--- proteuslib/tests/test_phosphorus_removal.py
+++ proteuslib/tests/test_phosphorus_removal.py
@@ -1194,7 +1194,7 @@ class TestSimplePhosphorusRemoval:
         phos_precip = value(model.fs.unit.outlet.mole_frac_comp[0, "FePO4(s)"])*total_molar_density
         phos_precip = phos_precip*95000
 
-        assert pytest.approx(7.10286, rel=1e-5) == total_phosphorus
+        assert pytest.approx(7.10260446574586, rel=1e-5) == total_phosphorus
         assert pytest.approx(88.2050, rel=1e-5) == phos_precip
 
         total_iron = value(model.fs.unit.outlet.mole_frac_comp[0, "Fe_3+"])*total_molar_density
@@ -1210,5 +1210,5 @@ class TestSimplePhosphorusRemoval:
         iron_precip = value(model.fs.unit.outlet.mole_frac_comp[0, "FePO4(s)"])*total_molar_density
         iron_precip = iron_precip*55800
 
-        assert pytest.approx(3.99532, rel=1e-5) == total_iron
+        assert pytest.approx(3.9951680694800653, rel=1e-5) == total_iron
         assert pytest.approx(51.8088, rel=1e-5) == iron_precip
