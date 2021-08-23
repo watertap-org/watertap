ff100604f95bd70768704137a394d1ddcbf1f62b
Ludovico Bianchi <lbianchi@lbl.gov>
2021-08-23 08:30:42 -0700

    Update IDAES version from tag on nawi-hub fork

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ SPECIAL_DEPENDENCIES_FOR_PRERELEASE = [
     # update with a tag from the nawi-hub/idaes-pse
     # when a version of IDAES newer than the latest stable release from PyPI
     # will become needed for the proteuslib development
-    "idaes-pse @ https://github.com/IDAES/idaes-pse/archive/ff2dc7301.zip"
+    "idaes-pse @ https://github.com/nawi-hub/idaes-pse/archive/1.10.1.proteuslib.2021.08.23.zip"
 ]
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
