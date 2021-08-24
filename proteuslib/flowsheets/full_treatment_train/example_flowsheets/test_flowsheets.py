5e60090419cdd4d3461183ae052553532709653d
TimBartholomew <timothy.bartholomew@netl.doe.gov>
2021-08-23 21:56:19 -0400

    added two flowsheets using bypass and separator models for NF and RO

--- proteuslib/flowsheets/full_treatment_train/example_flowsheets/test_flowsheets.py
+++ proteuslib/flowsheets/full_treatment_train/example_flowsheets/test_flowsheets.py
@@ -13,6 +13,7 @@
 import pytest
 from pyomo.environ import value
 import proteuslib.flowsheets.full_treatment_train.example_flowsheets.SepNF_SepRO_NoBypass as SepNF_SepRO_NoBypass
+import proteuslib.flowsheets.full_treatment_train.example_flowsheets.SepNF_SepRO as SepNF_SepRO
 
 
 @pytest.mark.component
@@ -30,3 +31,22 @@ def test_SepNF_SepRO_NoBypass_NF_ion_basis_example():
     assert value(m.fs.RO.inlet.flow_mass_phase_comp[0, 'Liq', 'TDS']) == pytest.approx(2.615e-2, rel=1e-3)
     assert value(m.fs.RO.retentate.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.4341, rel=1e-3)
 
+
+@pytest.mark.component
+def test_SepNF_SepRO_NF_salt_basis_example():
+    m = SepNF_SepRO.run_flowsheet_NF_salt_basis_example()
+    assert value(m.fs.mixer.pretreatment.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.6512, rel=1e-3)
+    assert value(m.fs.mixer.pretreatment.flow_mass_phase_comp[0, 'Liq', 'NaCl']) == pytest.approx(1.908e-2, rel=1e-3)
+    assert value(m.fs.mixer.bypass.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.2412, rel=1e-3)
+    assert value(m.fs.mixer.bypass.flow_mass_phase_comp[0, 'Liq', 'NaCl']) == pytest.approx(7.068e-3, rel=1e-3)
+    assert value(m.fs.RO.retentate.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.4462, rel=1e-3)
+
+
+@pytest.mark.component
+def test_SepNF_SepRO_NF_ion_basis_example():
+    m = SepNF_SepRO.run_flowsheet_NF_ion_basis_example()
+    assert value(m.fs.mixer.pretreatment.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.6512, rel=1e-3)
+    assert value(m.fs.mixer.pretreatment.flow_mass_phase_comp[0, 'Liq', 'Na']) == pytest.approx(7.507e-3, rel=1e-3)
+    assert value(m.fs.mixer.bypass.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.2412, rel=1e-3)
+    assert value(m.fs.mixer.bypass.flow_mass_phase_comp[0, 'Liq', 'Cl']) == pytest.approx(5.079e-3, rel=1e-3)
+    assert value(m.fs.RO.retentate.flow_mass_phase_comp[0, 'Liq', 'H2O']) == pytest.approx(0.4462, rel=1e-3)
