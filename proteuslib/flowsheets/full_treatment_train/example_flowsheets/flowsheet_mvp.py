a97545b791ce2c08aae7f7bc4080857c1c406e80
TimBartholomew <timothy.bartholomew@netl.doe.gov>
2021-09-16 09:33:05 -0400

    added permeate inequality for cost optimization stability

--- proteuslib/flowsheets/full_treatment_train/example_flowsheets/flowsheet_mvp.py
+++ proteuslib/flowsheets/full_treatment_train/example_flowsheets/flowsheet_mvp.py
@@ -157,10 +157,12 @@ def set_up_optimization(m, system_recovery=0.7, **kwargs_flowsheet):
         m.fs.inequality_RO_pressure = Constraint(
             expr=m.fs.pump_RO.control_volume.properties_out[0].pressure + min_pressure_increase
                  <= m.fs.pump_RO2.control_volume.properties_out[0].pressure)
+        m.fs.inequality_RO_permeate = Constraint(
+            expr=m.fs.RO.permeate_side.properties_mixed[0].flow_vol_phase['Liq']
+            >= m.fs.RO2.permeate_side.properties_mixed[0].flow_vol_phase['Liq'])
 
     check_dof(m, dof_expected=5 if is_twostage else 3)
     solve_with_user_scaling(m, tee=False, fail_flag=True)
-
 
 def optimize(m):
     solve_with_user_scaling(m, tee=False, fail_flag=True)
