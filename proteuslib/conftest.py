f96fb2927070532925eab52d0bd5b5ea505ee5e2
Adam Atia <aatia@keylogic.com>
2021-09-23 16:13:36 -0400

    Update proteuslib/conftest.py
    typo fix

--- proteuslib/conftest.py
+++ proteuslib/conftest.py
@@ -1,6 +1,6 @@
 
 _MARKERS = {
-    'unit': 'quick tests that do no require a solver, must run in < 2 s',
+    'unit': 'quick tests that do not require a solver, must run in < 2 s',
     'component': 'quick tests that may require a solver',
     'integration': 'long duration tests',
     'build': 'FIXME for building stuff?'
