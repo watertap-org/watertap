e24422398ff0c89aaae45ff12679f8dae83d99cd
adam-a-a <aatia@keylogic.com>
2021-10-08 18:53:00 -0400

    adding to do notes to sw ion test

--- proteuslib/property_models/tests/test_seawater_ion_generic.py
+++ proteuslib/property_models/tests/test_seawater_ion_generic.py
@@ -46,17 +46,13 @@ def test_property_seawater_ions():
     m.fs.stream[0].flow_mol_phase_comp['Liq', 'Cl_-'].fix(0.010479)
     m.fs.stream[0].flow_mol_phase_comp['Liq', 'H2O'].fix(0.979046)
     m.fs.stream[0].flow_vol
-    # m.fs.stream[0].flow_mass_phase_comp['Liq', 'H2O'].fix(
-    #     feed_flow_mass * (1 - sum(x for x in feed_mass_frac.values())))
-    # for s in feed_mass_frac:
-    #     m.fs.stream[0].flow_mass_phase_comp['Liq', s].fix(feed_flow_mass * feed_mass_frac[s])
     m.fs.stream[0].temperature.fix(273.15 + 25)
     m.fs.stream[0].pressure.fix(101325)
 
     m.fs.stream[0].flow_mol_phase_comp
     m.fs.stream[0].mw_comp
     m.fs.stream[0].flow_mass_phase_comp
-    # m.fs.stream[0].mass_frac_comp
+    # m.fs.stream[0].mass_frac_comp # TODO: mass_frac_comp and mass_frac_phase_comp need to be added to generic property model
 
     # # scaling
     # m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1, index=('Liq', 'H2O'))
@@ -65,22 +61,27 @@ def test_property_seawater_ions():
     # m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e3, index=('Liq', 'Mg'))
     # m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e3, index=('Liq', 'SO4'))
     # m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e2, index=('Liq', 'Cl'))
-    # iscale.calculate_scaling_factors(m.fs)
+    iscale.calculate_scaling_factors(m.fs)
     #
     # # checking state block
-    # assert_units_consistent(m)
-    # check_dof(m)
+    assert_units_consistent(m)
+    #TODO: replace check_dof
+    check_dof(m)
     #
     # # initialize
-    # m.fs.stream.initialize(optarg={'nlp_scaling_method': 'user-scaling'})
+    m.fs.stream.initialize()
     #
     # # solve
-    # solve_with_user_scaling(m)
+    #TODO: replace solve_with_user_scaling
+    solve_with_user_scaling(m)
     #
     # # check values
+    #TODO: mass_frac_phase_comp won't work until generic prop modified
+
     # assert value(m.fs.stream[0].mass_frac_phase_comp['Liq', 'H2O']) == pytest.approx(0.9647, rel=1e-3)
-    # assert value(m.fs.stream[0].flow_mol_phase_comp['Liq', 'Na']) == pytest.approx(0.4838, rel=1e-3)
-    # assert value(m.fs.stream[0].flow_mol_phase_comp['Liq', 'Ca']) == pytest.approx(9.531e-3, rel=1e-3)
+    #TODO: flow_mol_p_c is being fixed; change to assert flow_mass_phase_comp, flow_vol
+    # assert value(m.fs.stream[0].flow_mol_phase_comp['Liq', 'Na_+']) == pytest.approx(0.4838, rel=1e-3)
+    # assert value(m.fs.stream[0].flow_mol_phase_comp['Liq', 'Ca_2+']) == pytest.approx(9.531e-3, rel=1e-3)
 
 # class TestPropertySeawaterIons(PropertyTestHarness):
 #     def configure(self):
