c9c3cfee95244e719d94926c311ed00f582eee4a
Adam Atia <aatia@keylogic.com>
2021-10-15 15:15:01 -0400

    delete commented code

--- watertap/property_models/tests/test_seawater_ion_generic.py
+++ watertap/property_models/tests/test_seawater_ion_generic.py
@@ -35,12 +35,6 @@ def test_property_seawater_ions():
     m.fs.stream = m.fs.properties.build_state_block([0], default={'defined_state': True})
 
     # specify
-    # feed_flow_mass = 1
-    # feed_mass_frac = {'Na': 11122e-6,
-    #                   'Ca': 382e-6,
-    #                   'Mg': 1394e-6,
-    #                   'SO4': 2136e-6,
-    #                   'Cl': 20300e-6}
     m.fs.stream[0].flow_mol_phase_comp['Liq', 'Na_+'].fix(0.008845)
     m.fs.stream[0].flow_mol_phase_comp['Liq', 'Ca_2+'].fix(0.000174)
     m.fs.stream[0].flow_mol_phase_comp['Liq', 'Mg_2+'].fix(0.001049)
