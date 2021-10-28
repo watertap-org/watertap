6556e3a73694b88df4b240d76b5a556798bd8c8a
Ludovico Bianchi <lbianchi@lbl.gov>
2021-10-28 13:08:35 -0700

    Use subclass with overridden fixtures instead of separate module for EDB

--- watertap/tests/test_pure_water_pH.py
+++ watertap/tests/test_pure_water_pH.py
@@ -637,3 +637,48 @@ class TestPureWater:
         assert pytest.approx(0.99999, rel=1e-5) == value(
             model.fs.unit.outlet.mole_frac_comp[0.0, "H2O"]
         )
+
+
+class TestPureWaterEDB(TestPureWater):
+
+    @pytest.fixture
+    def thermo_config(self, edb):
+        base = edb.get_one_base("water_reaction")
+        elements = ["H", "O"]
+        components = []
+        # Add the components
+        for c in edb.get_components(element_names=elements):
+            # Need to remove these to avoid errors when using the generated config
+            c.remove("valid_phase_types")
+            c.remove("enth_mol_ig_comp")
+            c.remove("phase_equilibrium_form")
+            c.remove("pressure_sat_comp")
+            base.add(c)
+            components.append(c.name)
+        # Add the reactions
+        for r in edb.get_reactions(component_names=components):
+            r.set_reaction_order('Liq', ('H2O',), ('H_+', 'OH_-'))
+            r._data["type"] = "inherent"
+            base.add(r)
+        return base.idaes_config
+
+
+    @pytest.fixture
+    def water_reaction_config(self, edb):
+        elements = ["H", "O"]
+        components = [c.name for c in edb.get_components(element_names=elements)]
+        base = edb.get_one_base("water_reaction")
+        # Need to remove these to avoid errors when using the generated config
+        base.remove("phases")
+        base.remove("pressure_ref")
+        base.remove("state_bounds")
+        base.remove("state_definition")
+        base.remove("temperature_ref")
+        # Add the reactions
+        for r in edb.get_reactions(component_names=components):
+            # Set a custom reaction order
+            r.set_reaction_order('Liq', ('H2O',), ('H_+', 'OH_-'))
+            # Need to remove this to avoid errors when using the generated config
+            r.remove_parameter("ds_rxn_ref")
+            base.add(r)
+        return base.idaes_config
