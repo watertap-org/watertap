14e45988cf0e61ba3e43643e2f0c638382fa4b1a
Dan Gunter <dkgunter@lbl.gov>
2021-12-06 13:49:23 -0800

    Changes for connecting to cloud mongodb server (#260)
    * fixed commands; simple example
    
    * fixed commands, streamlined api a bit
    
    * didactive improvements and default connection check
    
    * Add nbsphinx support for building Jupyter Notebooks with Sphinx
    
    * Add nbsphinx support for building Jupyter Notebooks with Sphinx
    
    * Move edb example notebook to docs
    
    * this is
    
    * Pandoc
    
    * Run notebooks in docs as a test
    
    * Removed caching of connection test
    
    * improve error message for get_base failure
    
    * improve mongodb detection
    
    * initial code for mongodb connect with cert retry
    
    * update test notebooks
    
    * Some logging
    
    * preview of env var for cloud db password
    
    * tests for cloud db
    
    * ok if certifi is not installed
    
    Co-authored-by: Austin Ladshaw <ladshawap@ornl.gov>
    Co-authored-by: Austin Ladshaw <aladshaw3@outlook.com>
    Co-authored-by: Adam Atia <aatia@keylogic.com>

--- watertap/conftest.py
+++ watertap/conftest.py
@@ -3,25 +3,24 @@ from _pytest.config.argparsing import Parser
 
 
 _MARKERS = {
-    'unit': 'quick tests that do not require a solver, must run in < 2 s',
-    'component': 'quick tests that may require a solver',
-    'integration': 'long duration tests',
-    'build': 'FIXME for building stuff?'
+    "unit": "quick tests that do not require a solver, must run in < 2 s",
+    "component": "quick tests that may require a solver",
+    "integration": "long duration tests",
+    "build": "FIXME for building stuff?",
 }
 
 
 def pytest_configure(config: Config):
 
     for name, descr in _MARKERS.items():
-        config.addinivalue_line(
-            'markers', f'{name}: {descr}'
-        )
+        config.addinivalue_line("markers", f"{name}: {descr}")
 
 
 def pytest_addoption(parser: Parser):
     parser.addoption(
         "--edb-no-mock",
-        help="Force the `edb` fixture to connect to a running MongoDB instance instead of falling back to mongomock",
+        help="Force the `edb` fixture to connect to a running MongoDB instance "
+             "instead of falling back to mongomock",
         action="store_true",
         default=False,
         dest="edb_no_mock",
