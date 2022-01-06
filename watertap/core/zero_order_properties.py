d9b3ee79c6d92f0fc4576dca416bfcfbc3946ec4
Andrew Lee <andrew.lee@netl.doe.gov>
2022-01-06 08:56:01 -0500

    Adding database hookups to zero order proeprties

--- watertap/core/zero_order_properties.py
+++ watertap/core/zero_order_properties.py
@@ -28,6 +28,7 @@ from idaes.core.util.misc import add_object_reference
 from idaes.core.util.initialization import fix_state_vars, revert_state_vars
 import idaes.logger as idaeslog
 import idaes.core.util.scaling as iscale
+from idaes.core.util.exceptions import ConfigurationError
 
 from pyomo.environ import (Expression,
                            Param,
@@ -76,20 +77,36 @@ class WaterParameterBlockData(PhysicalParameterBlock):
 
         self.H2O = Solvent()
 
+        # Get component set from database if provided
+        comp_set = None
+        if self.config.database is not None:
+            comp_set = self.config.database.get_solute_set(
+                self.config.water_source)
+
         # Check definition of solute list
-        if self.config.solute_list is None:
+        solute_list = self.config.solute_list
+        if solute_list is None:
             # No user-provided solute list, look up list from database
-            pass
+            if comp_set is None:
+                # No solute list in database and none provided.
+                raise ConfigurationError(
+                    f"{self.name} no solute_list or database was defined. "
+                    f"Users must provide at least one of these arguments.")
+            else:
+                solute_list = comp_set
         elif self.config.database is not None:
             # User provided custom list and database - check that all
             # components are supported
-            pass
+            for j in solute_list:
+                if j not in comp_set:
+                    _log.warn(f"{self.name} component {j} is not defined in "
+                              f"the water_sources database file.")
         else:
             # User provided list but no database - assume they know what they
             # are doing
             pass
 
-        for j in self.config.solute_list:
+        for j in solute_list:
             self.add_component(str(j), Solute())
 
         # ---------------------------------------------------------------------
