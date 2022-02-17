dbeb18ebbc8dacc0155febff9e39462587caa50e
Ludovico Bianchi <lbianchi@lbl.gov>
2022-02-17 15:24:44 -0600

    Use an enum to define and register custom pytest markers

--- watertap/conftest.py
+++ watertap/conftest.py
@@ -1,19 +1,35 @@
+import contextlib
+import enum
+from typing import Container, Optional, Callable
+
+import pytest
 from _pytest.config import Config
 from _pytest.config.argparsing import Parser
 
 
-_MARKERS = {
-    "unit": "quick tests that do not require a solver, must run in < 2 s",
-    "component": "quick tests that may require a solver",
-    "integration": "long duration tests",
-    "build": "FIXME for building stuff?",
-}
+class MarkerSpec(enum.Enum):
+    unit = "Quick tests that do not require a solver, must run in < 2 s"
+    component = "Quick tests that may require a solver"
+    integration = "Long duration tests"
+    build = "FIXME for building stuff?"
+    requires_idaes_solver = "Tests that require a solver from the IDEAS extensions to pass"
 
+    @property
+    def description(self) -> str:
+        return self.value
+
+    @classmethod
+    def for_item(cls, item: Item) -> Container["MarkerSpec"]:
+        found = []
+        for marker in item.iter_markers():
+            with contextlib.suppress(KeyError):
+                found.append(cls[marker.name])
+        return found
 
 def pytest_configure(config: Config):
 
-    for name, descr in _MARKERS.items():
-        config.addinivalue_line("markers", f"{name}: {descr}")
+    for marker_spec in MarkerSpec:
+        config.addinivalue_line("markers", f"{marker_spec.name}: {marker_spec.description}")
 
 
 def pytest_addoption(parser: Parser):
