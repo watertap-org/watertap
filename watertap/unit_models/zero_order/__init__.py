b28ab293aefebe70498182ea29e0263ac3841042
luohezhiming <chenyu.wang@keylogic.com>
2022-02-24 15:26:56 -0500

    remove new model and keep for another branch e Please enter the commit message for your changes. Lines starting

--- watertap/unit_models/zero_order/__init__.py
+++ watertap/unit_models/zero_order/__init__.py
@@ -20,7 +20,5 @@ from .sedimentation_zo import SedimentationZO
 from .coag_and_floc_zo import CoagulationFlocculationZO
 from .uv_zo import UVZO
 from .uv_aop_zo import UVAOPZO
-from .dual_media_filtration_zo import DualMediaFiltrationZO
-from .anaerobic_digestion_oxidation_zo import AnaerobicDigestionOxidationZO
 from .ion_exchange_zo import IonExchangeZO
 
