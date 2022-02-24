368218155f214d88cf41ab168ff9ad1c48743415
Kurban Sitterley <kurban.sitterley@nrel.gov>
2022-02-24 15:29:17 -0700

    Migrate WT3 ozonation and ozone_aop unit models. (#348)
    * Initial commit for ozonation and ozone_aop unit models.
    
    * Using `is` for object comparison; removing extra lines.
    
    Co-authored-by: Adam Atia <aatia@keylogic.com>
    
    Co-authored-by: bknueven <30801372+bknueven@users.noreply.github.com>
    Co-authored-by: Adam Atia <aatia@keylogic.com>

--- watertap/unit_models/zero_order/__init__.py
+++ watertap/unit_models/zero_order/__init__.py
@@ -20,5 +20,7 @@ from .sedimentation_zo import SedimentationZO
 from .coag_and_floc_zo import CoagulationFlocculationZO
 from .uv_zo import UVZO
 from .uv_aop_zo import UVAOPZO
+from .ozone_zo import OzoneZO
+from .ozone_aop_zo import OzoneAOPZO
 from .ion_exchange_zo import IonExchangeZO
 from .dual_media_filtration_zo import DualMediaFiltrationZO
