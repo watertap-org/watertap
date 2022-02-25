ed7c573d7274ebddf78b137e9015088bca2cb5e3
luohezhiming <chenyu.wang@keylogic.com>
2022-02-25 18:34:24 -0500

    Add buffer tank as pass-through for unit test

--- watertap/data/techno_economic/tests/test_unit_parameter_files.py
+++ watertap/data/techno_economic/tests/test_unit_parameter_files.py
@@ -46,7 +46,8 @@ def test_unit_parameter_files(tech):
     # TODO : Need to check up on this once everything is done
     pass_through = ["chemical_addition",
                     "pump",
-                    "storage_tank"]
+                    "storage_tank",
+                    "buffer_tank"]
     siso_full_recovery = ["uv_aop", "uv", "ion_exchange", "fixed_bed"]
 
     no_energy_electric_flow_vol_inlet = ["energy_recovery",
