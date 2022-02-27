a62439536e9d09bbdf1bfb1d77dcd2c143369f1d
adam-a-a <aatia@keylogic.com>
2022-02-27 17:52:47 -0500

    modifying yaml values to be representative of process; modifying test

--- watertap/unit_models/zero_order/tests/test_anaerobic_mbr_mec_zo.py
+++ watertap/unit_models/zero_order/tests/test_anaerobic_mbr_mec_zo.py
@@ -46,16 +46,11 @@ class TestAnaerobicMBRMECZO:
             "property_package": m.fs.params,
             "database": m.db})
 
-        feed_state = {('flow_vol', None): 4.375e-5,
-                      ('conc_mass_comp', 'cod'): 2.3,
-                      ('conc_mass_comp', 'nonbiodegradable_cod'): 0,
-                      ('conc_mass_comp', 'ammonium_as_nitrogen'): .105,
-                      ('conc_mass_comp', 'phosphate_as_phosphorous'): .05,
-                      }
-        # m.fs.unit.inlet.flow_mass_comp[0, "H2O"].fix(10)
-        # m.fs.unit.inlet.flow_mass_comp[0, "tss"].fix(1)
-        # m.fs.unit.inlet.flow_mass_comp[0, "bod"].fix(1)
-        m.fs.unit.properties_in[0].calculate_state(var_args=feed_state, hold_state=True)
+        m.fs.unit.inlet.flow_mass_comp[0, "H2O"].fix(0.043642594)
+        m.fs.unit.inlet.flow_mass_comp[0, "cod"].fix(1.00625e-4)
+        m.fs.unit.inlet.flow_mass_comp[0, "nonbiodegradable_cod"].fix(1e-20)
+        m.fs.unit.inlet.flow_mass_comp[0, "ammonium_as_nitrogen"].fix(4.59375E-06)
+        m.fs.unit.inlet.flow_mass_comp[0, "phosphate_as_phosphorous"].fix(2.1875E-06)
 
         return m
 
@@ -69,143 +64,7 @@ class TestAnaerobicMBRMECZO:
 
     @pytest.mark.component
     def test_load_parameters(self, model):
-        data = model.db.get_unit_operation_parameters("anaerobic_digestion_oxidation")
-
-        model.fs.unit.load_parameters_from_database()
-
-        assert model.fs.unit.recovery_frac_mass_H2O[0].fixed
-        assert model.fs.unit.recovery_frac_mass_H2O[0].value == \
-            data["recovery_frac_mass_H2O"]["value"]
-
-        for (t, j), v in model.fs.unit.removal_frac_mass_solute.items():
-            assert v.fixed
-            assert v.value == data["removal_frac_mass_solute"][j]["value"]
-
-        assert model.fs.unit.energy_electric_flow_vol_inlet.fixed
-        assert model.fs.unit.energy_electric_flow_vol_inlet.value == data[
-            "energy_electric_flow_vol_inlet"]["value"]
-
-    @pytest.mark.component
-    def test_degrees_of_freedom(self, model):
-        assert degrees_of_freedom(model.fs.unit) == 0
-
-    @pytest.mark.component
-    def test_unit_consistency(self, model):
-        assert_units_consistent(model.fs.unit)
-
-    @pytest.mark.component
-    def test_initialize(self, model):
-        initialization_tester(model)
-
-    @pytest.mark.solver
-    @pytest.mark.skipif(solver is None, reason="Solver not available")
-    @pytest.mark.component
-    def test_solve(self, model):
-        results = solver.solve(model)
-
-        # Check for optimal solution
-        assert_optimal_termination(results)
-
-    @pytest.mark.solver
-    @pytest.mark.skipif(solver is None, reason="Solver not available")
-    @pytest.mark.component
-    def test_solution(self, model):
-        assert (pytest.approx(1.2e-2, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].flow_vol))
-        assert (pytest.approx(83.3333, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].conc_mass_comp["tss"]))
-        assert (pytest.approx(83.3333, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(0.010279, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].flow_vol))
-        assert (pytest.approx(24.3214, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].conc_mass_comp["tss"]))
-        assert (pytest.approx(2.91857, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(1.721e-3, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].flow_vol))
-        assert (pytest.approx(435.7931, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["tss"]))
-        assert (pytest.approx(563.6258, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(6.48, abs=1e-5) ==
-                value(model.fs.unit.electricity[0]))
-
-    @pytest.mark.solver
-    @pytest.mark.skipif(solver is None, reason="Solver not available")
-    @pytest.mark.component
-    def test_conservation(self, model):
-        for j in model.fs.params.component_list:
-            assert 1e-6 >= abs(value(
-                model.fs.unit.inlet.flow_mass_comp[0, j] -
-                model.fs.unit.treated.flow_mass_comp[0, j] -
-                model.fs.unit.byproduct.flow_mass_comp[0, j]))
-
-    @pytest.mark.component
-    def test_report(self, model):
-        stream = StringIO()
-
-        model.fs.unit.report(ostream=stream)
-
-        output = """
-====================================================================================
-Unit : fs.unit                                                             Time: 0.0
-------------------------------------------------------------------------------------
-    Unit Performance
-
-    Variables: 
-
-    Key                   : Value   : Fixed : Bounds
-       Electricity Demand :  6.4800 : False : (None, None)
-    Electricity Intensity : 0.15000 :  True : (None, None)
-     Solute Removal [bod] : 0.97000 :  True : (0, None)
-     Solute Removal [tss] : 0.75000 :  True : (0, None)
-           Water Recovery : 0.99990 :  True : (1e-08, 1.0000001)
-
-------------------------------------------------------------------------------------
-    Stream Table
-                             Inlet   Treated  Byproduct
-    Volumetric Flowrate    0.012000 0.010279 0.0017210 
-    Mass Concentration H2O   833.33   972.76   0.58106 
-    Mass Concentration tss   83.333   24.321    435.79 
-    Mass Concentration bod   83.333   2.9186    563.63 
-====================================================================================
-"""
-
-        assert output in stream.getvalue()
-
-class TestAnaerobicDigestionOxidationZO_w_default_removal:
-    @pytest.fixture(scope="class")
-    def model(self):
-        m = ConcreteModel()
-        m.db = Database()
-
-        m.fs = FlowsheetBlock(default={"dynamic": False})
-        m.fs.params = WaterParameterBlock(
-            default={"solute_list": ["tss", "bod", "foo"]})
-
-        m.fs.unit = AnaerobicDigestionOxidationZO(default={
-            "property_package": m.fs.params,
-            "database": m.db})
-
-        m.fs.unit.inlet.flow_mass_comp[0, "H2O"].fix(10)
-        m.fs.unit.inlet.flow_mass_comp[0, "tss"].fix(1)
-        m.fs.unit.inlet.flow_mass_comp[0, "bod"].fix(1)
-        m.fs.unit.inlet.flow_mass_comp[0, "foo"].fix(1)
-
-        return m
-
-    @pytest.mark.unit
-    def test_build(self, model):
-        assert model.fs.unit.config.database == model.db
-
-        assert isinstance(model.fs.unit.electricity, Var)
-        assert isinstance(model.fs.unit.energy_electric_flow_vol_inlet, Var)
-        assert isinstance(model.fs.unit.electricity_consumption, Constraint)
-
-    @pytest.mark.component
-    def test_load_parameters(self, model):
-        data = model.db.get_unit_operation_parameters("anaerobic_digestion_oxidation")
+        data = model.db.get_unit_operation_parameters("anaerobic_mbr_mec")
 
         model.fs.unit.load_parameters_from_database(use_default_removal=True)
 
@@ -215,7 +74,7 @@ class TestAnaerobicDigestionOxidationZO_w_default_removal:
 
         for (t, j), v in model.fs.unit.removal_frac_mass_solute.items():
             assert v.fixed
-            if j == "foo":
+            if j not in data['removal_frac_mass_solute'].keys():
                 assert v.value == data["default_removal_frac_mass_solute"]["value"]
             else:
                 assert v.value == data["removal_frac_mass_solute"][j]["value"]
@@ -249,31 +108,31 @@ class TestAnaerobicDigestionOxidationZO_w_default_removal:
     @pytest.mark.skipif(solver is None, reason="Solver not available")
     @pytest.mark.component
     def test_solution(self, model):
-        assert (pytest.approx(1.3e-2, rel=1e-5) ==
+        assert (pytest.approx(3780/3600/24/1000, rel=1e-5) ==
                 value(model.fs.unit.properties_in[0].flow_vol))
-        assert (pytest.approx(76.9231, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].conc_mass_comp["tss"]))
-        assert (pytest.approx(76.9231, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(76.9231, rel=1e-5) ==
-                value(model.fs.unit.properties_in[0].conc_mass_comp["foo"]))
-        assert (pytest.approx(0.011279, rel=1e-5) ==
+        assert (pytest.approx(2.3, rel=1e-5) ==
+                value(model.fs.unit.properties_in[0].conc_mass_comp["cod"]))
+        assert (pytest.approx(0, rel=1e-5) ==
+                value(model.fs.unit.properties_in[0].conc_mass_comp["nonbiodegradable_cod"]))
+        assert (pytest.approx(0.105, rel=1e-5) ==
+                value(model.fs.unit.properties_in[0].conc_mass_comp["ammonium_as_nitrogen"]))
+        assert (pytest.approx(0.05, rel=1e-5) ==
+                value(model.fs.unit.properties_in[0].conc_mass_comp["phosphate_as_phosphorous"]))
+
+        assert (pytest.approx(1500/3600/24/1000, rel=1e-2) ==
                 value(model.fs.unit.properties_treated[0].flow_vol))
-        assert (pytest.approx(22.1651, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].conc_mass_comp["tss"]))
-        assert (pytest.approx(2.65981, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(88.6603, rel=1e-5) ==
-                value(model.fs.unit.properties_treated[0].conc_mass_comp["foo"]))
-        assert (pytest.approx(1.721e-3, rel=1e-5) ==
+        assert (pytest.approx(24.3214, rel=1e-5) ==
+                value(model.fs.unit.properties_treated[0].conc_mass_comp["cod"]))
+        assert (pytest.approx(2.91857, rel=1e-5) ==
+                value(model.fs.unit.properties_treated[0].conc_mass_comp["nonbiodegradable_cod"]))
+
+        assert (pytest.approx(1500/3600/24/1000, rel=1e-5) ==
                 value(model.fs.unit.properties_byproduct[0].flow_vol))
         assert (pytest.approx(435.7931, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["tss"]))
+                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["cod"]))
         assert (pytest.approx(563.6258, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["bod"]))
-        assert (pytest.approx(4.6485e-7, rel=1e-5) ==
-                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["foo"]))
-        assert (pytest.approx(7.02, abs=1e-5) ==
+                value(model.fs.unit.properties_byproduct[0].conc_mass_comp["nonbiodegradable_cod"]))
+        assert (pytest.approx(6.48, abs=1e-5) ==
                 value(model.fs.unit.electricity[0]))
 
     @pytest.mark.solver
@@ -282,7 +141,9 @@ class TestAnaerobicDigestionOxidationZO_w_default_removal:
     def test_conservation(self, model):
         for j in model.fs.params.component_list:
             assert 1e-6 >= abs(value(
-                model.fs.unit.inlet.flow_mass_comp[0, j] -
+                model.fs.unit.inlet.flow_mass_comp[0, j] +
+                sum(model.fs.unit.generation_rxn_comp[0, r, j]
+                    for r in model.fs.unit.reaction_set) -
                 model.fs.unit.treated.flow_mass_comp[0, j] -
                 model.fs.unit.byproduct.flow_mass_comp[0, j]))
 
@@ -300,22 +161,25 @@ Unit : fs.unit                                                             Time:
 
     Variables: 
 
-    Key                   : Value   : Fixed : Bounds
-       Electricity Demand :  7.0200 : False : (None, None)
-    Electricity Intensity : 0.15000 :  True : (None, None)
-     Solute Removal [bod] : 0.97000 :  True : (0, None)
-     Solute Removal [foo] :  0.0000 :  True : (0, None)
-     Solute Removal [tss] : 0.75000 :  True : (0, None)
-           Water Recovery : 0.99990 :  True : (1e-08, 1.0000001)
+    Key                                           : Value      : Fixed : Bounds
+                               Electricity Demand :     0.0000 : False : (None, None)
+                            Electricity Intensity :     0.0000 :  True : (None, None)
+    Reaction Extent [cod_to_nonbiodegradable_cod] : 5.0312e-05 : False : (None, None)
+            Solute Removal [ammonium_as_nitrogen] :     0.0000 :  True : (0, None)
+                             Solute Removal [cod] :     1.0000 :  True : (0, None)
+            Solute Removal [nonbiodegradable_cod] :     1.0000 :  True : (0, None)
+        Solute Removal [phosphate_as_phosphorous] :     0.0000 :  True : (0, None)
+                                   Water Recovery :    0.39700 :  True : (1e-08, 1.0000001)
 
 ------------------------------------------------------------------------------------
     Stream Table
-                             Inlet   Treated  Byproduct
-    Volumetric Flowrate    0.013000 0.011279  0.0017210
-    Mass Concentration H2O   769.23   886.51    0.58106
-    Mass Concentration tss   76.923   22.165     435.79
-    Mass Concentration bod   76.923   2.6598     563.63
-    Mass Concentration foo   76.923   88.660 4.6485e-07
+                                                   Inlet     Treated   Byproduct
+    Volumetric Flowrate                         4.3750e-05 1.7333e-05 2.6417e-05
+    Mass Concentration H2O                          997.55     999.61     996.19
+    Mass Concentration cod                          2.3000 4.6155e-05     1.9046
+    Mass Concentration nonbiodegradable_cod     2.2857e-16 4.6155e-05     1.9046
+    Mass Concentration ammonium_as_nitrogen        0.10500    0.26508 3.0283e-05
+    Mass Concentration phosphate_as_phosphorous   0.050000    0.12625 3.0283e-05
 ====================================================================================
 """
 
