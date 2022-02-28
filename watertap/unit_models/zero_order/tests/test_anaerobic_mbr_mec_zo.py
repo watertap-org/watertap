e5d48f2d573d634ddda3032133f5e28cf1df855a
adam-a-a <aatia@keylogic.com>
2022-02-27 19:08:18 -0500

    finish test for AnMBR-MEC

--- watertap/unit_models/zero_order/tests/test_anaerobic_mbr_mec_zo.py
+++ watertap/unit_models/zero_order/tests/test_anaerobic_mbr_mec_zo.py
@@ -17,7 +17,7 @@ import pytest
 
 from io import StringIO
 from pyomo.environ import (
-    ConcreteModel, Constraint, value, Var, assert_optimal_termination)
+    ConcreteModel, Constraint, value, Var, assert_optimal_termination, units as pyunits)
 from pyomo.util.check_units import assert_units_consistent
 
 from idaes.core import FlowsheetBlock
@@ -39,7 +39,8 @@ class TestAnaerobicMBRMECZO:
 
         m.fs = FlowsheetBlock(default={"dynamic": False})
         m.fs.params = WaterParameterBlock(
-            default={"solute_list": ["cod", "nonbiodegradable_cod",
+            default={"solute_list": ["cod",
+                                     "nonbiodegradable_cod",
                                      "ammonium_as_nitrogen", "phosphate_as_phosphorous"]})
 
         m.fs.unit = AnaerobicMBRMECZO(default={
@@ -121,18 +122,22 @@ class TestAnaerobicMBRMECZO:
 
         assert (pytest.approx(1500/3600/24/1000, rel=1e-2) ==
                 value(model.fs.unit.properties_treated[0].flow_vol))
-        assert (pytest.approx(24.3214, rel=1e-5) ==
+        assert (pytest.approx(2.8958, rel=1e-5) ==
                 value(model.fs.unit.properties_treated[0].conc_mass_comp["cod"]))
-        assert (pytest.approx(2.91857, rel=1e-5) ==
+        assert (pytest.approx(4.60445e-05, rel=1e-5) ==
                 value(model.fs.unit.properties_treated[0].conc_mass_comp["nonbiodegradable_cod"]))
 
-        assert (pytest.approx(1500/3600/24/1000, rel=1e-5) ==
+        assert (pytest.approx(2280/3600/24/1000, rel=1e-2) ==
                 value(model.fs.unit.properties_byproduct[0].flow_vol))
-        assert (pytest.approx(435.7931, rel=1e-5) ==
+        assert (pytest.approx(3.0331e-05, rel=1e-5) ==
                 value(model.fs.unit.properties_byproduct[0].conc_mass_comp["cod"]))
-        assert (pytest.approx(563.6258, rel=1e-5) ==
+        assert (pytest.approx(1.90757, rel=1e-5) ==
                 value(model.fs.unit.properties_byproduct[0].conc_mass_comp["nonbiodegradable_cod"]))
-        assert (pytest.approx(6.48, abs=1e-5) ==
+        assert (pytest.approx(4.347, rel=1e-3) ==
+                value(pyunits.convert(
+                    model.fs.unit.properties_byproduct[0].flow_mass_comp["nonbiodegradable_cod"],
+                    to_units=pyunits.kg/pyunits.day)))
+        assert (pytest.approx(0, abs=1e-5) ==
                 value(model.fs.unit.electricity[0]))
 
     @pytest.mark.solver
@@ -166,21 +171,67 @@ Unit : fs.unit                                                             Time:
                             Electricity Intensity :     0.0000 :  True : (None, None)
     Reaction Extent [cod_to_nonbiodegradable_cod] : 5.0312e-05 : False : (None, None)
             Solute Removal [ammonium_as_nitrogen] :     0.0000 :  True : (0, None)
-                             Solute Removal [cod] :     1.0000 :  True : (0, None)
+                             Solute Removal [cod] :     0.0000 :  True : (0, None)
             Solute Removal [nonbiodegradable_cod] :     1.0000 :  True : (0, None)
         Solute Removal [phosphate_as_phosphorous] :     0.0000 :  True : (0, None)
-                                   Water Recovery :    0.39700 :  True : (1e-08, 1.0000001)
+                                   Water Recovery :    0.39680 :  True : (1e-08, 1.0000001)
 
 ------------------------------------------------------------------------------------
     Stream Table
                                                    Inlet     Treated   Byproduct
-    Volumetric Flowrate                         4.3750e-05 1.7333e-05 2.6417e-05
-    Mass Concentration H2O                          997.55     999.61     996.19
-    Mass Concentration cod                          2.3000 4.6155e-05     1.9046
-    Mass Concentration nonbiodegradable_cod     2.2857e-16 4.6155e-05     1.9046
-    Mass Concentration ammonium_as_nitrogen        0.10500    0.26508 3.0283e-05
-    Mass Concentration phosphate_as_phosphorous   0.050000    0.12625 3.0283e-05
+    Volumetric Flowrate                         4.3750e-05 1.7374e-05 2.6376e-05
+    Mass Concentration H2O                          997.55     996.71     998.09
+    Mass Concentration cod                          2.3000     2.8958 3.0331e-05
+    Mass Concentration nonbiodegradable_cod     2.2857e-16 4.6045e-05     1.9076
+    Mass Concentration ammonium_as_nitrogen        0.10500    0.26444 3.0331e-05
+    Mass Concentration phosphate_as_phosphorous   0.050000    0.12595 3.0331e-05
 ====================================================================================
 """
 
         assert output in stream.getvalue()
+
+
+db = Database()
+params = db._get_technology("anaerobic_mbr_mec")
+
+
+class Test_AnMBRMEC_ZO_subtype:
+    @pytest.fixture(scope="class")
+    def model(self):
+        m = ConcreteModel()
+
+        m.fs = FlowsheetBlock(default={"dynamic": False})
+        m.fs.params = WaterParameterBlock(
+            default={"solute_list": ["cod", "nonbiodegradable_cod"]})
+
+        m.fs.unit = AnaerobicMBRMECZO(default={
+            "property_package": m.fs.params,
+            "database": db})
+
+        return m
+
+    @pytest.mark.parametrize("subtype", [params.keys()])
+    @pytest.mark.component
+    def test_load_parameters(self, model, subtype):
+        model.fs.unit.config.process_subtype = subtype
+        data = db.get_unit_operation_parameters("anaerobic_mbr_mec", subtype=subtype)
+
+        model.fs.unit.load_parameters_from_database()
+
+        for (t, j), v in model.fs.unit.removal_frac_mass_solute.items():
+            assert v.fixed
+            assert v.value == data["removal_frac_mass_solute"][j]["value"]
+
+
+@pytest.mark.unit
+def test_ffCOD_in_solute_list():
+    model = ConcreteModel()
+    model.db = Database()
+
+    model.fs = FlowsheetBlock(default={"dynamic": False})
+    model.fs.params = WaterParameterBlock(default={"solute_list": ["cod"]})
+    with pytest.raises(KeyError,
+                       match="nonbiodegradable_cod must be included in the solute list since"
+                             " this unit model converts cod to nonbiodegradable_cod."):
+        model.fs.unit = AnaerobicMBRMECZO(default={"property_package": model.fs.params,
+                                                   "database": model.db})
