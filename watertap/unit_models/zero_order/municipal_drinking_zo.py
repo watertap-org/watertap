90735da3ddb086b6cfec18f26ff061eb1b3b39c5
bknueven <30801372+bknueven@users.noreply.github.com>
2022-03-03 16:30:59 -0700

    NFC: extra period

--- watertap/unit_models/zero_order/municipal_drinking_zo.py
+++ watertap/unit_models/zero_order/municipal_drinking_zo.py
@@ -11,7 +11,7 @@
 #
 ###############################################################################
 """
-This module contains a zero-order representation of municipal drinking water unit.
+This module contains a zero-order representation of municipal drinking water unit
 operation.
 """
 
