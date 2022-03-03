2821030b7e7a64f961db898fd48d55a118c64a24
luohezhiming <98901358+luohezhiming@users.noreply.github.com>
2022-03-03 13:06:19 -0500

    Migrating WT3 catridge filtration model (#367)

--- watertap/unit_models/zero_order/__init__.py
+++ watertap/unit_models/zero_order/__init__.py
@@ -16,6 +16,7 @@ from .aeration_basin_zo import AerationBasinZO
 from .air_flotation_zo import AirFlotationZO
 from .bioreactor_zo import BioreactorZO
 from .chemical_addition_zo import ChemicalAdditionZO
+from .cartridge_filtration_zo import CartridgeFiltrationZO
 from .decarbonator_zo import DecarbonatorZO
 from .feed_zo import FeedZO
 from .media_filtration_zo import MediaFiltrationZO
