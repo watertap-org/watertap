0bd15311ceff7df64dfbf6da373e04aac1b7a289
TimBartholomew <timothy.bartholomew@netl.doe.gov>
2022-03-03 13:48:45 -0500

    three oli case generation

--- watertap/examples/flowsheets/full_treatment_train/analysis/oli_case_generation.py
+++ watertap/examples/flowsheets/full_treatment_train/analysis/oli_case_generation.py
@@ -2,8 +2,23 @@ import sys
 import os
 import time
 
-from pyomo.environ import Expression
+from pyomo.environ import Expression, Param, Constraint
 from watertap.tools.parameter_sweep import _init_mpi, LinearSample, parameter_sweep
+
+def build_mass_frac(blk, state_blk):
+    mw_comp = {'H2O': 18.015e-3,
+               'Na_+': 22.990e-3,
+               'Ca_2+': 40.078e-3,
+               'Mg_2+': 24.305e-3,
+               'SO4_2-': 96.06e-3,
+               'Cl_-': 35.453e-3}
+
+    def mass_frac_calc(blk, j):
+        return (state_blk.flow_mol_phase_comp['Liq', j] * mw_comp[j]
+                / sum(state_blk.flow_mol_phase_comp['Liq', jj] * mw_comp[jj]
+                      for jj in mw_comp.keys()))
+
+    blk.mass_frac_comp = Expression(mw_comp.keys(), rule=mass_frac_calc)
 
 def run_analysis(case_num, nx, RO_type, interp_nan_outputs=True):
 
@@ -24,35 +39,85 @@ def run_analysis(case_num, nx, RO_type, interp_nan_outputs=True):
         # deactivate eNRTL constraint
         m.fs.eq_max_saturation_index_desal.deactivate()
 
-        mw_comp = {'H2O': 18.015e-3,
-                   'Na_+': 22.990e-3,
-                   'Ca_2+': 40.078e-3,
-                   'Mg_2+': 24.305e-3,
-                   'SO4_2-': 96.06e-3,
-                   'Cl_-': 35.453e-3}
-
-        def mass_frac_calc(blk, j):
-            return (blk.desal_saturation.properties[0].flow_mol_phase_comp['Liq', j] * mw_comp[j]
-                    / sum(blk.desal_saturation.properties[0].flow_mol_phase_comp['Liq', jj] * mw_comp[jj]
-                          for jj in mw_comp.keys()))
-        m.fs.mass_frac_comp = Expression(mw_comp.keys(), rule=mass_frac_calc)
+        build_mass_frac(m.fs, m.fs.desal_saturation.properties[0])
 
         sweep_params['System Recovery'] = LinearSample(m.fs.system_recovery_target, 0.3, 0.85, nx)
-        # sweep_params['RO Recovery'] = LinearSample(m.fs.RO_recovery, 0.3, 0.95, nx)
-        # sweep_params['Max Pressure'] = LinearSample(m.fs.max_allowable_pressure, 300e5, 75e5, nx)
 
-        for j in mw_comp.keys():
-            outputs[j] = m.fs.mass_frac_comp[j]
+        for (j, v) in m.fs.mass_frac_comp.items():
+            outputs[j] = v
         outputs['LCOW'] = m.fs.costing.LCOW
 
         output_filename = 'output/oli_cases/results_two_stage_RO.csv'
 
         opt_function = fs_two_stage.optimize
 
-    # elif case_num == 2:
+    elif case_num == 2:
         # ================================================================
-        # NF
+        # NF No Bypass
         # ================================================================
+        import watertap.examples.flowsheets.full_treatment_train.analysis.flowsheet_NF_no_bypass as fs_NF_no_bypass
+
+        m = fs_NF_no_bypass.solve_flowsheet()
+
+        sweep_params['NF Recovery'] = LinearSample(m.fs.NF.recovery_mass_phase_comp[0, 'Liq', 'H2O'], 0.3, 0.85, nx)
+
+        build_mass_frac(m.fs, m.fs.pretrt_saturation.properties[0])
+
+        for (j, v) in m.fs.mass_frac_comp.items():
+            outputs[j] = v
+        outputs['LCOW'] = m.fs.costing.LCOW
+
+        output_filename = 'output/oli_cases/results_NF.csv'
+
+        opt_function = fs_NF_no_bypass.simulate
+        # Need to unfix NF area to simulate with
+        # fixed NF recovery
+        optimize_kwargs = {'unfix_nf_area': True, 'check_termination': False}
+
+    elif case_num == 3:
+        # ================================================================
+        # NF Two Stage
+        # ================================================================
+        import watertap.examples.flowsheets.full_treatment_train.analysis.flowsheet_NF_two_stage as fs_NF_two_stage
+        interp_nan_outputs = False
+
+        m = fs_NF_two_stage.optimize_flowsheet(**desal_kwargs)
+
+        m.fs.eq_max_saturation_index_desal.deactivate()
+        m.fs.eq_max_conc_NF.deactivate()
+
+        m.fs.NF.recovery_mass_phase_comp[0, 'Liq', 'H2O'].fix(0.7)
+        # m.fs.max_recovery_NF_target = Param(initialize=0.7, mutable=True)
+        # m.fs.max_recovery_NF_target_tol = Param(initialize=5e-6, mutable=True)
+        # m.fs.eq_max_recovery_NF = Constraint(
+        #     expr=(0,
+        #           m.fs.NF.recovery_mass_phase_comp[0, 'Liq', 'H2O']
+        #           - m.fs.max_recovery_NF_target,
+        #           m.fs.max_recovery_NF_target_tol))
+
+        # m.fs.RO_recovery_target = Param(initialize=0.7, mutable=True)
+        # m.fs.RO_recovery_target_tol = Param(initialize=5e-6, mutable=True)
+        # m.fs.eq_RO_recovery_target = Constraint(
+        #     expr=(0,
+        #           m.fs.RO_recovery
+        #           - m.fs.RO_recovery_target,
+        #           m.fs.RO_recovery_target_tol))
+
+        sweep_params['NF Split Fraction'] = LinearSample(m.fs.splitter.split_fraction[0, 'pretreatment'], 0.05, 0.5, nx)
+        sweep_params['System Recovery'] = LinearSample(m.fs.system_recovery_target, 0.3, 0.8, nx)
+
+        outputs['RO Recovery'] = m.fs.RO_recovery
+
+        build_mass_frac(m.fs, m.fs.desal_saturation.properties[0])
+
+        for (j, v) in m.fs.mass_frac_comp.items():
+            outputs[j] = v
+        outputs['LCOW'] = m.fs.costing.LCOW
+
+
+        output_filename = 'output/oli_cases/results_NF_two_stage_RO.csv'
+
+        opt_function = fs_NF_two_stage.optimize
 
     else:
         raise ValueError('case_num = %d not recognized.' % (case_num))
