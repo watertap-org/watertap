8a9bfe79abe9eef7385176fbcec969fc1df9b5e3
bknueven <30801372+bknueven@users.noreply.github.com>
2022-03-03 16:36:22 -0700

    NFC: cleaning up comments/docstrings

--- watertap/unit_models/zero_order/sw_onshore_intake_zo.py
+++ watertap/unit_models/zero_order/sw_onshore_intake_zo.py
@@ -11,7 +11,7 @@
 #
 ###############################################################################
 """
-This module contains a zero-order representation of seawater onshore intake unit.
+This module contains a zero-order representation of seawater onshore intake unit
 operation.
 """
 
