85dca71f902e5c8c0c4655a2e58a4a16a65e5a02
luohezhiming <chenyu.wang@keylogic.com>
2022-03-04 12:02:30 -0500

    Migrating WT3 landfill zld model

---
+++ watertap/unit_models/zero_order/landfill_zld_zo.py
@@ -0,0 +1,59 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+"""
+This module contains a zero-order representation of a landfill zld unit operation.
+"""
+
+from pyomo.environ import units as pyunits, Var
+from idaes.core import declare_process_block_class
+
+from watertap.core import build_pt, constant_intensity, ZeroOrderBaseData
+
+# Some more inforation about this module
+__author__ = "Chenyu Wang"
+
+
+@declare_process_block_class("LandfillZLDZO")
+class LandfillZLDZOData(ZeroOrderBaseData):
+    """
+    Zero-Order model for a landfill zld unit operation.
+    """
+
+    CONFIG = ZeroOrderBaseData.CONFIG()
+
+    def build(self):
+        super().build()
+
+        self._tech_type = "landfill_zld"
+
+        build_pt(self)
+        constant_intensity(self)
+
+        self.capacity_basis = Var(self.flowsheet().time,
+                                units=pyunits.kg/pyunits.hr,
+                                doc="capacity basis for capital cost")
+
+        self.total_mass = Var(self.flowsheet().time,
+                                units=pyunits.kg/pyunits.hr,
+                                doc="total mass flow rate")
+
+        self._fixed_perf_vars.append(self.capacity_basis)
+
+        @self.Constraint(self.flowsheet().time,
+                    doc='Total mass constraint')
+        def total_mass_constraint(b, t):
+            return b.total_mass[t] == sum(pyunits.convert(b.inlet.flow_mass_comp[t, m], to_units=pyunits.kg/pyunits.hr)
+                                          for m in b.config.property_package.component_list)
+
+        self._perf_var_dict["Capacity Basis (kg/hr)"] = self.capacity_basis
+        self._perf_var_dict["Total Mass (kg/hr)"] = self.total_mass
