4733320a34303cfc3a92bcdd59e172fd22311adb
Kurban Sitterley <kurban.sitterley@nrel.gov>
2022-03-03 18:09:18 -0700

    Remove Cl2 recovery from .yaml and put in module (#381)
    Co-authored-by: bknueven <30801372+bknueven@users.noreply.github.com>

--- watertap/data/techno_economic/tests/test_unit_parameter_files.py
+++ watertap/data/techno_economic/tests/test_unit_parameter_files.py
@@ -55,7 +55,7 @@ def test_unit_parameter_files(tech):
                     "sw_onshore_intake",
                     ]
 
-    siso_full_recovery = ["uv_aop", "uv", "ion_exchange", "fixed_bed", "decarbonator"]
+    siso_full_recovery = ["uv_aop", "uv", "ion_exchange", "fixed_bed", "decarbonator", "chlorination"]
 
     no_energy_electric_flow_vol_inlet = ["energy_recovery",
                                          "mbr_denitrification",
