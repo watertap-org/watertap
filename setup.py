8f0855957af3a565d0a20f007b1a2464572ebeec
Bernard Knueven <Bernard.Knueven@nrel.gov>
2022-03-09 09:51:44 -0700

    switching to Andrew's costing branch

--- setup.py
+++ setup.py
@@ -43,7 +43,9 @@ SPECIAL_DEPENDENCIES_FOR_PRERELEASE = [
     # update with a tag from the nawi-hub/idaes-pse
     # when a version of IDAES newer than the latest stable release from PyPI
     # will become needed for the watertap development
-    "idaes-pse[prerelease] @ https://github.com/watertap-org/idaes-pse/archive/1.12.1.watertap.2022.02.04.zip",
+    #"idaes-pse[prerelease] @ https://github.com/watertap-org/idaes-pse/archive/1.12.1.watertap.2022.02.04.zip",
+    # until its merged, we need to use Andrew's costing branch to keep moving forward with costing
+    "idaes-pse[prerelease] @ git+https://github.com/andrewlee94/idaes-pse.git@costing_2"
 ]
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
