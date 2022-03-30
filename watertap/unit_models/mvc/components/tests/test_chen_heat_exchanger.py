9e407dedabdd2bcc443424d9d969292610817bef
TimBartholomew <timothy.bartholomew@netl.doe.gov>
2022-03-30 17:10:28 -0400

    refactored tests

--- watertap/unit_models/mvc/components/tests/test_chen_heat_exchanger.py
+++ watertap/unit_models/mvc/components/tests/test_chen_heat_exchanger.py
@@ -13,15 +13,69 @@
 import sys
 import pytest
 from io import StringIO
-from watertap.unit_models.mvc.components.demo_heat_exchanger import main as main_heat_exchanger
+
+from pyomo.environ import (ConcreteModel,
+                           assert_optimal_termination)
+from pyomo.util.check_units import assert_units_consistent
+from idaes.core import FlowsheetBlock
 from idaes.core.util import get_solver
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.generic_models.unit_models.heat_exchanger import (HeatExchanger,
+                                                             HeatExchangerFlowPattern)
+import idaes.core.util.scaling as iscale
+
+from watertap.unit_models.mvc.components.lmtd_chen_callback import delta_temperature_chen_callback
+import watertap.property_models.seawater_prop_pack as props
 
 solver = get_solver()
 
 # -----------------------------------------------------------------------------
 @pytest.mark.skipif(sys.platform.startswith("darwin"), reason="Heat exchanger needs complied IDAES extensions")
 def test_heat_exchanger():
-    m = main_heat_exchanger()
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={'dynamic': False})
+    m.fs.properties = props.SeawaterParameterBlock()
+    m.fs.unit = HeatExchanger(default={
+        "hot_side_name": "hot",
+        "cold_side_name": "cold",
+        "hot": {"property_package": m.fs.properties},
+        "cold": {"property_package": m.fs.properties},
+        "delta_temperature_callback": delta_temperature_chen_callback,
+        "flow_pattern": HeatExchangerFlowPattern.countercurrent})
+
+    # scaling
+    m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1, index=('Liq', 'H2O'))
+    m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e2, index=('Liq', 'TDS'))
+    iscale.set_scaling_factor(m.fs.unit.hot.heat, 1e-3)
+    iscale.set_scaling_factor(m.fs.unit.cold.heat, 1e-3)
+    iscale.set_scaling_factor(m.fs.unit.overall_heat_transfer_coefficient, 1e-3)
+    iscale.set_scaling_factor(m.fs.unit.area, 1)
+    iscale.calculate_scaling_factors(m)
+
+    # ---specifications---
+    # state variables
+    m.fs.unit.hot_inlet.flow_mass_phase_comp[0, 'Liq', 'H2O'].fix(1)
+    m.fs.unit.hot_inlet.flow_mass_phase_comp[0, 'Liq', 'TDS'].fix(0.01)
+    m.fs.unit.hot_inlet.temperature[0].fix(350)
+    m.fs.unit.hot_inlet.pressure[0].fix(2e5)
+
+    m.fs.unit.cold_inlet.flow_mass_phase_comp[0, 'Liq', 'H2O'].fix(0.5)
+    m.fs.unit.cold_inlet.flow_mass_phase_comp[0, 'Liq', 'TDS'].fix(0.01)
+    m.fs.unit.cold_inlet.temperature[0].fix(298)
+    m.fs.unit.cold_inlet.pressure[0].fix(2e5)
+
+    m.fs.unit.area.fix(5)
+    m.fs.unit.overall_heat_transfer_coefficient.fix(1000)
+
+    # solving
+    assert_units_consistent(m)
+    degrees_of_freedom(m)
+
+    m.fs.unit.initialize()
+
+    solver = get_solver()
+    results = solver.solve(m, tee=False)
+    assert_optimal_termination(results)
 
     report_io = StringIO()
     m.fs.unit.report(ostream=report_io)
