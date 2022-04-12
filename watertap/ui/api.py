8f9b71f487c7a7e522919bf50a6ff3888e341c07
dangunter <dkgunter@lbl.gov>
2022-04-12 15:28:57 -0700

    save work

--- watertap/ui/api.py
+++ watertap/ui/api.py
@@ -2,11 +2,19 @@
 API for the UI
 """
 import abc
+import inspect
 from typing import Dict, Iterable
 
 # third-party
+import functools
 from pyomo.environ import ConcreteModel
 from idaes.core.util import get_solver
+import idaes.logger as idaeslog
+
+# Set up logger
+_log = idaeslog.getLogger(__name__)
+import logging
+_log.setLevel(logging.DEBUG)
 
 # Schema
 
@@ -32,11 +40,47 @@ STEP_NAMES = (
 SCHEMA = {k: {} for k in STEP_NAMES}
 
 
+def log_meth(meth):
+    @functools.wraps(meth)
+    def wrapper(*args, **kwargs):
+        name = _get_method_classname(meth)
+        _log.debug(f"Begin {name}")
+        try:
+            result = meth(*args, **kwargs)
+        except Exception as e:
+            error_msg = f"Error in {name}"
+            _log.exception(error_msg)
+            raise e
+        _log.debug(f"End {name}")
+        return result
+    return wrapper
+
+
+def _get_method_classname(m):
+    """Get class name for method, assuming method is bound and class has __dict__.
+    """
+    try:
+        qualname = dict.fromkeys(inspect.getmembers(m))["__qualname__"]
+    except KeyError:
+        qualname = "<unknown>"
+    return qualname
+    # print(f"@@ get classname for {m}")
+    # print(f"@@ members = {inspect.getmembers(m)}")
+    # if inspect.ismethod(m):
+    #     mro = inspect.getmro(m.__self__.__class__)
+    #     print(f"@@ mro = {mro}")
+    #     for cls in mro:
+    #         print(f"@@ check {m.__name__} in class {cls.__name__}")
+    #         if m.__name__ in cls.__dict__:
+    #             return cls.__name__
+    # return "<unknown>"
+
 class WorkflowStep(abc.ABC):
     """Subclasses are an implementation of the Strategy Pattern where the algorithm is, e.g., building
     or initializing or solving the model.
     """
-    def __init__(self):
+    def __init__(self, workflow: "AnalysisWorkflow"):
+        self.workflow = workflow
         self.flowsheet_data = {}
 
     @abc.abstractmethod
@@ -49,6 +93,7 @@ class WorkflowStep(abc.ABC):
 
 
 class Build(WorkflowStep):
+    @log_meth
     def algorithm(self, data):
         return {"model": self.build_model(data)}
 
@@ -112,21 +157,45 @@ class AnalysisWorkflow:
         self._steps[Steps.setup]["data"] = d
 
     def get_result(self, name: str) -> Dict:
+        """Get result from a previously executed step `name`.
+
+        Args:
+            name: Name of the step for which the result is retrieved
+
+        Return:
+            The result of that step (always an empty dictionary if not yet run)
+        """
         name = self._normalize_step_name(name)
         return self._steps[name]["result"]
 
-    def set_strategy(self, name: str, value: WorkflowStep):
+    # some syntactic sugar for common step/result combinations
+
+    @property
+    def model(self):
+        return self.get_result(Steps.build)["model"]
+
+    @property
+    def optimize_result(self):
+        return self.get_result(Steps.optimize)
+
+    # end of syntactic sugar
+
+    def set_strategy(self, name: str, clazz: type, **kwargs):
         """Set  the strategy to use for one of the workflow steps.
 
         Args:
             name: Name of the workflow step
-            value: Strategy for this step. This should be a subclass of WorkflowStep.
+            clazz: Class of strategy for this step. This should be a subclass of WorkflowStep.
+            kwargs: Additional arguments to initialize the strategy class
 
         Raises:
             KeyError: If the step name is invalid
         """
         name = self._normalize_step_name(name)
-        self._steps[name]["strategy"] = value
+        obj = clazz(self, **kwargs)  # instantiate the step
+        self._steps[name]["strategy"] = obj
+        self._steps[name]["input"] = {}
+        self._steps[name]["_kwargs"] = kwargs  # just for debugging
 
     def set_workflow(self, step_names: Iterable[str]) -> Iterable[str]:
         seen_names, wf = {}, []
--- watertap/ui/tests/test_api_seawaterROdesalination.py
+++ watertap/ui/tests/test_api_seawaterROdesalination.py
@@ -9,18 +9,33 @@ from watertap.examples.case_studies.seawater_RO_desalination import seawater_RO_
 class BuildRO(Build):
     def build_model(self, data):
         kwargs = self.flowsheet_data
-        return srd.build(**kwargs)
+        model = srd.build(**kwargs)
+        srd.set_operating_conditions(model)
+        srd.assert_degrees_of_freedom(model, 0)
+        return model
+
+
+class InitializeRO(Initialize):
+    def initialize_model(self, data):
+        model = self.workflow.model
+        srd.initialize_system(model)
+        srd.assert_degrees_of_freedom(model, 0)
+
+
+class OptimizeRO(Optimize):
+    def solve(self, data):
+        return srd.solve(self.workflow.model)
 
 
 @pytest.mark.component
 def test_workflow():
     wf = AnalysisWorkflow()
     wf.set_flowsheet_data({"erd_type": "pressure_exchanger"})
-    # TODO: set strategy and data for each step
-    wf.set_strategy(Steps.build, BuildRO())
-    wf.set_input(Steps.build, {"input": "goes here"})
-    wf.set_workflow((Steps.build,)) # TODO: add other steps
+    # TODO: set strategy and data (if any) for each step
+    wf.set_strategy(Steps.build, BuildRO)
+    wf.set_strategy(Steps.init, InitializeRO)
+    wf.set_strategy(Steps.optimize, OptimizeRO)
+    wf.set_workflow((Steps.build, Steps.init, Steps.optimize)) # TODO: add other steps
     wf.run_all()
     # TODO: Check results
-
-
+    print(wf.optimize_result)
