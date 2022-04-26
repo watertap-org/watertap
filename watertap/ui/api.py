3c2e10ef7cd8cc04ecd0c5faa0b27ef08dd8f6f7
Dan Gunter <dkgunter@lbl.gov>
2022-04-26 12:36:16 -0700

    saving work de novo

--- watertap/ui/api.py
+++ watertap/ui/api.py
@@ -111,12 +111,14 @@ def get_block_interface(block: Block) -> Union["BlockInterface", None]:
     """
     return getattr(block, "ui", None)
 
+
 class BlockSchemaDefinition:
     """Container for the schema definition of JSON used for loading/saving blocks.
 
     The BlockInterface, and thus FlowsheetInterface, classes will inherit from this class
     in order to pick up this definition as a set of class constants.
     """
+
     # Standard keys for data fields
     BLKS_KEY = "blocks"
     NAME_KEY = "name"
@@ -125,6 +127,19 @@ class BlockSchemaDefinition:
     VARS_KEY = "variables"
     VALU_KEY = "value"
     INDX_KEY = "index"
+    UNIT_KEY = "units"
+
+    # Convenient form for all keys together (e.g. as kwargs)
+    ALL_KEYS = dict(
+        name_key=NAME_KEY,
+        disp_key=DISP_KEY,
+        desc_key=DESC_KEY,
+        vars_key=VARS_KEY,
+        valu_key=VALU_KEY,
+        indx_key=INDX_KEY,
+        blks_key=BLKS_KEY,
+        unit_key=UNIT_KEY,
+    )
 
     BLOCK_SCHEMA = {
         "$schema": "http://json-schema.org/draft-07/schema#",
@@ -144,6 +159,7 @@ class BlockSchemaDefinition:
                                 "$name_key": {"type": "string"},
                                 "$disp_key": {"type": "string"},
                                 "$desc_key": {"type": "string"},
+                                "$unit_key": {"type": "string"},
                                 # scalar or indexed value
                                 # two forms:
                                 #  {value: 1.34}  -- scalar
@@ -160,17 +176,17 @@ class BlockSchemaDefinition:
                                                         "items": {
                                                             "oneOf": [
                                                                 {"type": "number"},
-                                                                {"type": "string"}
+                                                                {"type": "string"},
                                                             ]
-                                                        }
+                                                        },
                                                     },
                                                     "$valu_key": {
                                                         "oneOf": [
                                                             {"type": "number"},
                                                             {"type": "string"},
                                                         ]
-                                                    }
-                                                }
+                                                    },
+                                                },
                                             },
                                         },
                                         {"type": "number"},
@@ -261,38 +277,43 @@ class BlockInterface(BlockSchemaDefinition):
             c = {self.NAME_KEY: item["name"]}  # one result
             # get the Pyomo Var from the block
             v = getattr(self._block, item["name"])
-            # copy contents of Var into result
-            if v.is_indexed():
-                indexed_values = []
-                for idx in v.index_set():
-                    try:
-                        idx_tuple = tuple(idx)
-                    except TypeError:
-                        idx_tuple = (idx,)
-                    indexed_values.append({self.VALU_KEY: value(v[idx]), self.INDX_KEY: idx_tuple})
-                block_value = {self.VALU_KEY: indexed_values}
-            else:
-                block_value = value(v)
-            c[self.VALU_KEY] = block_value
+            c[self.VALU_KEY] = self._variable_value(v)
             if self.DISP_KEY not in c:
                 c[self.DISP_KEY] = v.local_name
             if self.DESC_KEY not in c:
                 c[self.DESC_KEY] = v.doc
-            c["units"] = str(v.get_units())
+            c[self.UNIT_KEY] = str(v.get_units())
             # generate one result
             yield c
+
+    def _variable_value(self, v: Var) -> Union[List, int, float, str]:
+        """Reformat simple or indexed variable value for export."""
+        if v.is_indexed():
+            var_value = []
+            # create a list of the values for each index
+            for idx in v.index_set():
+                try:
+                    idx_tuple = tuple(idx)
+                except TypeError:
+                    idx_tuple = (idx,)
+                var_value.append(
+                    {self.VALU_KEY: value(v[idx]), self.INDX_KEY: idx_tuple}
+                )
+        else:
+            var_value = value(v)  # assume int/float or str
+        return var_value
 
 
 class WorkflowActions:
     build = "build"
     solve = "solve"
 
+
 class FlowsheetInterface(BlockInterface):
     """Interface to the UI for a flowsheet."""
 
     # Actions in the flowsheet workflow
     ACTIONS = [WorkflowActions.build, WorkflowActions.solve]
-
 
     def __init__(self, flowsheet: Block, options):
         """Constructor.
@@ -304,16 +325,7 @@ class FlowsheetInterface(BlockInterface):
         super().__init__(flowsheet, options)
         self._actions = {a: (None, None) for a in self.ACTIONS}
         self._vis = None
-        self._block_schema = Schema(
-            self.BLOCK_SCHEMA,
-            name_key=self.NAME_KEY,
-            disp_key=self.DISP_KEY,
-            desc_key=self.DESC_KEY,
-            vars_key=self.VARS_KEY,
-            valu_key=self.VALU_KEY,
-            indx_key=self.INDX_KEY,
-            blks_key=self.BLKS_KEY,
-        )
+        self._block_schema = Schema(self.BLOCK_SCHEMA, **self.ALL_KEYS)
 
     # Public methods
 
@@ -328,9 +340,7 @@ class FlowsheetInterface(BlockInterface):
     @log_meth
     def save(self, file_or_stream: Union[str, Path, TextIO]):
         """Save the current state of this instance to a file."""
-        fp = open_file_or_stream(
-            file_or_stream, "write", mode="w", encoding="utf-8"
-        )
+        fp = open_file_or_stream(file_or_stream, "write", mode="w", encoding="utf-8")
         data = self.as_dict()
         json.dump(data, fp)
 
@@ -339,9 +349,7 @@ class FlowsheetInterface(BlockInterface):
         cls, file_or_stream: Union[str, Path, TextIO], fs_block: Block
     ) -> "FlowsheetInterface":
         """Load from saved state in a file into the flowsheet block ``fs_block``."""
-        fp = open_file_or_stream(
-            file_or_stream, "read", mode="r", encoding="utf-8"
-        )
+        fp = open_file_or_stream(file_or_stream, "read", mode="r", encoding="utf-8")
         data = json.load(fp)
         root = data["blocks"]
         cls._load(root, fs_block)
@@ -349,19 +357,18 @@ class FlowsheetInterface(BlockInterface):
         ui = get_block_interface(fs_block)
         if ui is None:
             raise ValueError(
-                f"Flowsheet object must define FlowsheetInterface using ``set_block_interface()`` "
-                f"during construction. obj={fs_block}"
+                f"Flowsheet object must define FlowsheetInterface using "
+                f"``set_block_interface()`` during construction. obj={fs_block}"
             )
         ui.set_visualization(data["vis"])
         return ui
-
 
     @log_meth
     def get_variables(self) -> Dict:
         """Get all the variables exported by this flowsheet and its sub-blocks.
 
         Returns:
-            A dict with the variables. See the class attribute VARIABLES_SCHEMA for expected form.
+            A dict with the variables. See :class:`BlockSchemaDefinition` for details.
         """
         block_map = self._get_block_map()
         return {"blocks": block_map}
@@ -442,9 +449,7 @@ class FlowsheetInterface(BlockInterface):
         # add new item at leaf
         for sub_block in m[self.BLKS_KEY]:
             if sub_block[self.NAME_KEY] == leaf:
-                raise ValueError(
-                    f"Add mapping key failed: Already present. key={leaf}"
-                )
+                raise ValueError(f"Add mapping key failed: Already present. key={leaf}")
         m[self.BLKS_KEY].append(new_data)
 
     @classmethod
