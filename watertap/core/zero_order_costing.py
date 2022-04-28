1370c2efe007ba20c1381b25539149b1784e094f
bknueven <30801372+bknueven@users.noreply.github.com>
2022-04-28 12:26:44 -0600

    rename costing method

--- watertap/core/zero_order_costing.py
+++ watertap/core/zero_order_costing.py
@@ -805,7 +805,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
             blk.unit_model.electricity[t0], "electricity"
         )
 
-    def cost_electroN_P(blk):
+    def cost_electro_np(blk):
         """
         General method for costing electrochemical nutrient recovery. Capital cost
         is based on the volumetirc flowrate and HRT of the incoming stream. Chemical
@@ -2194,7 +2194,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         CoagulationFlocculationZO: cost_coag_and_floc,
         DeepWellInjectionZO: cost_deep_well_injection,
         DMBRZO: cost_dmbr,
-        ElectroNPZO: cost_electroN_P,
+        ElectroNPZO: cost_electro_np,
         FixedBedZO: cost_fixed_bed,
         GACZO: cost_gac,
         LandfillZO: cost_landfill,
