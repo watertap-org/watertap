bb79d32ef8033f9fd6099b9056caa6f1e5d69722
Dan Gunter <dkgunter@lbl.gov>
2022-05-10 14:35:26 -0700

    Example UI wrapper for METAB flowsheet

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/metab/metab_api.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/metab/metab_api.py
@@ -9,16 +9,6 @@ from watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.meta
 
 def flowsheet_for_ui():
     model = metab.build()
-    feed_api = BlockInterface(
-        model.fs.feed,
-        {
-            "display_name": "Zero-order feed",
-            "description": "Zero-Order feed block",
-            "variables": [
-                {"name": "flow_vol"},
-            ],
-        },
-    )
     return FlowsheetInterface(
         model.fs, {"display_name": "METAB treatment train", "variables": []}
     )
