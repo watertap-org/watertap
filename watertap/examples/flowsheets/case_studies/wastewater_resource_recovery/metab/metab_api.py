f0a29cb9212276499eee08880efd53dd40a753c4
Dan Gunter <dkgunter@lbl.gov>
2022-05-10 14:46:41 -0700

    added driver

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/metab/metab_api.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/metab/metab_api.py
@@ -9,15 +9,54 @@ from watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.meta
 
 def flowsheet_for_ui():
     model = metab.build()
-    return FlowsheetInterface(
+    fsi = FlowsheetInterface(
         model.fs, {"display_name": "METAB treatment train", "variables": []}
     )
 
 
-if __name__ == "__main__":
+def cli_driver(fsi):
+    """Dumb little interactive driver for CLI testing.
+    """
+    commands = {"print": print_json, "quit": exit_program}
+    while True:
+        try:
+            cmd = input("Command> ")
+        except EOFError:
+            break
+        cmd = cmd.strip().lower()
+        if cmd in commands:
+            commands[cmd](fsi)
+        else:
+            print_help(cmd, commands)
+    exit_program()
+
+
+def print_help(user_cmd, commands):
+    if user_cmd != "":
+        print(f"'{user_cmd}' is not a command.")
+    print(f"Commands:")
+    for cmd_key, cmd_val in commands.items():
+        desc = cmd_val.__doc__.strip()
+        print(f"  {cmd_key} - {desc}")
+
+
+def exit_program(*args):
+    """Exit the program"""
+    import sys
+    print("Exit program.")
+    sys.exit(0)
+
+
+def print_json(fsi):
+    """Print the flowsheet as JSON"""
     import json
 
-    fsi = flowsheet_for_ui()
     print("Flowsheet data")
     print("--------------")
     print(json.dumps(fsi.as_dict(), indent=2))
+    print("--------------")
+
+
+if __name__ == "__main__":
+    fsi = flowsheet_for_ui()
+    cli_driver(fsi)
