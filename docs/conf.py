05f46bfa98acef5ddc0e68a50551a17347dcad12
adam-a-a <aatia@keylogic.com>
2022-05-31 15:05:03 -0400

    ran black on doc/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,7 @@ extensions = [
     "nbsphinx",  # Jupyter notebooks as docs
 ]
 
-mathjax3_config = {'chtml': {'displayAlign': 'left',
-                             'displayIndent': '2em'}}
+mathjax3_config = {"chtml": {"displayAlign": "left", "displayIndent": "2em"}}
 
 autosectionlabel_prefix_document = True
 autodoc_warningiserror = False  # suppress warnings during autodoc
