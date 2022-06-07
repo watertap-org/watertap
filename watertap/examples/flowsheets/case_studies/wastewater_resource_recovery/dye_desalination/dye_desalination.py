e68df257fd3847908b6d4d5ba2e7fdb784da9a7c
arao53 <akshay.rao@keylogic.com>
2022-06-06 17:34:56 -0700

    Removed feed from display results.

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/dye_desalination.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/dye_desalination.py
@@ -251,7 +251,7 @@ def add_costing(m):
 
 
 def display_results(m):
-    unit_list = ["feed", "P1", "nanofiltration"]
+    unit_list = ["P1", "nanofiltration"]
     for u in unit_list:
         m.fs.component(u).report()
 
