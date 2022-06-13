2a98def9b71a27aaf2797cab8e27e72dd458f7dc
arao53 <akshay.rao@keylogic.com>
2022-06-13 14:52:33 -0700

    Added tests for dye desalination with RO

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/tests/test_dye_desalination_withRO.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/tests/test_dye_desalination_withRO.py
@@ -18,7 +18,7 @@ from pyomo.environ import (
 from idaes.core.util import get_solver
 from watertap.core.util.initialization import assert_degrees_of_freedom
 from pyomo.util.check_units import assert_units_consistent
-from watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_desalination_withRO.dye_desalination_withRO import (
+from watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_desalination.dye_desalination_withRO import (
     build,
     set_operating_conditions,
     initialize_system,
@@ -30,3 +30,106 @@ from watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_
 )
 
 solver = get_solver()
+
+
+class TestDyewROFlowsheet:
+    @pytest.fixture(scope="class")
+    def system_frame(self):
+        m = build()
+        return m
+
+    @pytest.mark.unit
+    def test_build(self, system_frame):
+        m = system_frame
+        assert_degrees_of_freedom(m, 28)
+        assert_units_consistent(m)
+
+    @pytest.mark.component
+    def test_set_operating_conditions(self, system_frame):
+        m = system_frame
+        set_operating_conditions(m)
+        initialize_system(m)
+
+        # test feed
+        assert pytest.approx(947.50, rel=1e-5) == value(
+            m.fs.feed.flow_mass_comp[0, "H2O"]
+        )
+
+        assert pytest.approx(50.0, rel=1e-5) == value(
+            m.fs.feed.flow_mass_comp[0, "tds"]
+        )
+
+        assert pytest.approx(2.50, rel=1e-5) == value(
+            m.fs.feed.flow_mass_comp[0, "dye"]
+        )
+
+        # test wwtp
+        assert pytest.approx(16.799, rel=1e-5) == value(
+            m.fs.pretreatment.wwtp.electricity
+        )
+
+        # test pump block
+        assert pytest.approx(6.895, rel=1e-5) == value(
+            m.fs.dye_separation.P1.applied_pressure[0]
+        )
+
+        # test nanofiltration
+        assert pytest.approx(128.88, rel=1e-5) == value(
+            m.fs.dye_separation.nanofiltration.area
+        )
+
+        # check products
+        assert pytest.approx(0.31008, rel=1e-3) == value(
+            m.fs.wwtp_retentate.flow_mass_comp[0, "tds"]
+        )
+        assert pytest.approx(0.08199, rel=1e-3) == value(
+            m.fs.dye_retentate.flow_mass_comp[0, "dye"]
+        )
+
+        assert pytest.approx(0.965, rel=1e-5) == value(
+            m.fs.permeate.flow_mass_phase_comp[0, "Liq", "H2O"]
+        )
+
+        assert pytest.approx(0.035, rel=1e-5) == value(
+            m.fs.brine.flow_mass_phase_comp[0, "Liq", "TDS"]
+        )
+
+    @pytest.mark.component
+    def test_solve(self, system_frame):
+        m = system_frame
+        results = solve(m)
+
+        # check products
+        assert pytest.approx(0.31008, rel=1e-3) == value(
+            m.fs.wwtp_retentate.flow_mass_comp[0, "tds"]
+        )
+        assert pytest.approx(7.8958, rel=1e-3) == value(
+            m.fs.dye_retentate.flow_mass_comp[0, "H2O"]
+        )
+
+        assert pytest.approx(0.00463, rel=1e-3) == value(
+            m.fs.permeate.flow_mass_phase_comp[0, "Liq", "TDS"]
+        )
+
+        assert pytest.approx(11.716, rel=1e-5) == value(
+            m.fs.brine.flow_mass_phase_comp[0, "Liq", "H2O"]
+        )
+
+    def test_costing(self, system_frame):
+        m = system_frame
+
+        add_costing(m)
+        initialize_costing(m)
+
+        results = solve(m)
+        assert_optimal_termination(results)
+
+        # check costing
+        assert pytest.approx(0.60113, rel=1e-3) == value(m.LCOT)
+        assert pytest.approx(0.19835, rel=1e-3) == value(m.LCOW)
+
+    @pytest.mark.component
+    def test_display(self, system_frame):
+        m = system_frame
+        display_results(m)
+        display_costing(m)
