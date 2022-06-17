d339db6eb5b35d2f07cb2e9d262cd497533dfd0d
arao53 <akshay.rao@keylogic.com>
2022-06-17 09:52:53 -0700

    Resolved differences between sweeps for dye_desalination and dye_desalination_withRO

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/dye_sweep.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/dye_desalination/dye_sweep.py
@@ -17,27 +17,49 @@ import time
 
 from pyomo.environ import Constraint
 from watertap.tools.parameter_sweep import _init_mpi, LinearSample, parameter_sweep
-import watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_desalination.dye_desalination as dye_separation
+import watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_desalination.dye_desalination as dye_desalination
+import watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.dye_desalination.dye_desalination_withRO as dye_desalination_withRO
 
 
-def set_up_sensitivity(m):
+def set_up_sensitivity(m, withRO):
     outputs = {}
-    optimize_kwargs = {"check_termination": False}
-    opt_function = dye_separation.solve
 
-    # create outputs
-    #    outputs["LCOW"] = m.fs.costing.LCOW_dye_recovered
+    # LCOT is an output for both flowsheets
     outputs["LCOT"] = m.LCOT
 
+    # choose the right flowsheet and if ro is enabled add lcow
+    if withRO:
+        outputs["LCOW"] = m.LCOW
+        opt_function = dye_desalination_withRO.solve
+    else:
+        opt_function = dye_desalination.solve
+
+    optimize_kwargs = {"check_termination": False}
     return outputs, optimize_kwargs, opt_function
 
 
-def run_analysis(case_num, nx, interpolate_nan_outputs=True):
+def run_analysis(case_num=4, nx=11, interpolate_nan_outputs=True, withRO=True):
+    # when from the command line
+    case_num = int(case_num)
+    nx = int(nx)
+    interpolate_nan_outputs = bool(interpolate_nan_outputs)
+    withRO = bool(withRO)
 
-    m = dye_separation.main()[0]
+    if not withRO and case_num > 7:
+        raise ValueError(
+            "Case numbers 8 are only for dye_desalination_withRO. Please set 'withRO=True'"
+        )
 
-    outputs, optimize_kwargs, opt_function = set_up_sensitivity(m)
+    # select flowsheet
+    if withRO:
+        m = dye_desalination_withRO.main()[0]
+    else:
+        m = dye_desalination.main()[0]
 
+    # set up sensitivities
+    outputs, optimize_kwargs, opt_function = set_up_sensitivity(m, withRO)
+
+    # choose parameter sweep from case structure
     sweep_params = {}
 
     if case_num == 1:
@@ -95,15 +117,55 @@ def run_analysis(case_num, nx, interpolate_nan_outputs=True):
         sweep_params["feed_flow"] = LinearSample(
             m.fs.feed.flow_vol, 0.01 / 3600, 120 / 3600, nx
         )
+    elif case_num == 8:
+        m.fs.zo_costing.recovered_water_cost.unfix()
+        sweep_params["recovered_water_value"] = LinearSample(
+            m.fs.zo_costing.recovered_water_cost, 0.0, 1.5, nx
+        )
+        m.fs.zo_costing.dye_mass_cost.unfix()
+        sweep_params["recovered_dye_value"] = LinearSample(
+            m.fs.zo_costing.dye_mass_cost, 0.0, 1, nx
+        )
+    elif case_num == 9:
+        m.fs.zo_costing.recovered_water_cost.unfix()
+        sweep_params["recovered_water_value"] = LinearSample(
+            m.fs.zo_costing.recovered_water_cost, 0.0, 1.5, nx
+        )
+        m.fs.zo_costing.waste_disposal_cost.unfix()
+        sweep_params["waste_disposal_cost"] = LinearSample(
+            m.fs.zo_costing.waste_disposal_cost, 0.0, 10, nx
+        )
+    elif case_num == 10:
+        m.fs.zo_costing.electricity_cost.unfix()
+        m.fs.zo_costing.recovered_water_cost.unfix()
+        sweep_params["recovered_water_value"] = LinearSample(
+            m.fs.zo_costing.recovered_water_cost, 0.0, 1.5, nx
+        )
+        sweep_params["electricity_cost"] = LinearSample(
+            m.fs.zo_costing.electricity_cost, 0.0, 0.25, nx
+        )
+    elif case_num == 11:
+        m.fs.dye_separation.nanofiltration.removal_frac_mass_solute[0, "dye"].unfix()
+        sweep_params["NF_dye_removal"] = LinearSample(
+            m.fs.dye_separation.nanofiltration.removal_frac_mass_solute[0, "dye"],
+            0.2,
+            1,
+        )
+        m.fs.desalination.RO.A_comp.unfix()
+        sweep_params["RO_permeability"] = LinearSample(
+            m.fs.desalination.RO.A_comp, 1e-12, 1e-11
+        )
     else:
         raise ValueError("case_num = %d not recognized." % (case_num))
 
-    output_filename = "sensitivity_" + str(case_num)
+    # output csv in the same directory as this sweep file
+    output_filename = "sensitivity_" + str(case_num) + ".csv"
     output_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         output_filename,
     )
 
+    # run sweep
     global_results = parameter_sweep(
         m,
         sweep_params,
@@ -114,22 +176,10 @@ def run_analysis(case_num, nx, interpolate_nan_outputs=True):
         interpolate_nan_outputs=interpolate_nan_outputs,
     )
 
+    print(global_results)
+
     return global_results, sweep_params, m
 
 
-def main(case_num=1, nx=11, interpolate_nan_outputs=False):
-    # when from the command line
-    case_num = int(case_num)
-    nx = int(nx)
-    interpolate_nan_outputs = bool(interpolate_nan_outputs)
-
-    global_results, sweep_params, m = run_analysis(
-        case_num, nx, interpolate_nan_outputs
-    )
-    print(global_results)
-
-    return global_results, m
-
-
 if __name__ == "__main__":
-    results, model = main(*sys.argv[1:])
+    results, params, model = run_analysis(*sys.argv[1:])
