f569808cb10c05be838e22d8f11e045d52f58ce9
tarnold17 <travis.arnold17@gmail.com>
2022-06-28 14:52:32 -0700

    add initial unit tests

--- watertap/examples/flowsheets/case_studies/groundwater_treatment/tests/test_groundwater_treatment.py
+++ watertap/examples/flowsheets/case_studies/groundwater_treatment/tests/test_groundwater_treatment.py
@@ -10,3 +10,39 @@
 # "https://github.com/watertap-org/watertap/"
 #
 ###############################################################################
+
+import pytest
+
+from pyomo.util.check_units import assert_units_consistent
+from pyomo.environ import (
+    value,
+    assert_optimal_termination,
+)
+
+from watertap.core.util.initialization import assert_degrees_of_freedom
+
+from watertap.examples.flowsheets.case_studies.groundwater_treatment.groundwater_treatment import (
+    build,
+    set_operating_conditions,
+    initialize_system,
+    solve,
+    add_costing,
+    display_costing,
+    display_results,
+)
+
+
+class TestGroundwaterTreatmentFlowsheet:
+    @pytest.fixture(scope="class")
+    def system_frame(self):
+        m = build()
+        return m
+
+    @pytest.mark.unit
+    def test_build(self, system_frame):
+        m = system_frame
+        assert_degrees_of_freedom(m, 21)
+        assert_units_consistent(m)
+
+
+# TODO write more tests
