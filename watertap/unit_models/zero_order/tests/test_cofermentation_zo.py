129998cb876ff8c85c60ea1b6d2cb0e31fd71e99
adam-a-a <aatia@keylogic.com>
2022-07-15 14:47:50 -0400

    replace solute with comp from latest merged changes

--- watertap/unit_models/zero_order/tests/test_cofermentation_zo.py
+++ watertap/unit_models/zero_order/tests/test_cofermentation_zo.py
@@ -90,10 +90,10 @@ class TestCofermentationZO:
 
         for (t, j), v in model.fs.unit.removal_frac_mass_comp.items():
             assert v.fixed
-            if j not in data["removal_frac_mass_solute"].keys():
-                assert v.value == data["default_removal_frac_mass_solute"]["value"]
+            if j not in data["removal_frac_mass_comp"].keys():
+                assert v.value == data["default_removal_frac_mass_comp"]["value"]
             else:
-                assert v.value == data["removal_frac_mass_solute"][j]["value"]
+                assert v.value == data["removal_frac_mass_comp"][j]["value"]
 
     @pytest.mark.component
     def test_degrees_of_freedom(self, model):
