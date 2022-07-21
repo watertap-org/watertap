e4d551c2d89735ed42e6859444e9e2d0b7e06297
Kinshuk Panda <kinshuk.panda@nrel.gov>
2022-07-21 09:24:28 -0600

    splitting of do_param_Sweep and addition of test_function option passes existing param_sweep tests in serial. parallel and additional tests pending.

--- watertap/tools/parameter_sweep.py
+++ watertap/tools/parameter_sweep.py
@@ -770,6 +770,58 @@ def _read_output_h5(filepath):
 
 # ================================================================
 
+def _param_sweep_kernel(model, optimize_function,
+    optimize_kwargs,
+    reinitialize_before_sweep,
+    reinitialize_function,
+    reinitialize_kwargs,
+    reinitialize_values
+    ):
+
+    run_successful = False  # until proven otherwise
+
+    # Forced reinitialization of the flowsheet if enabled
+    if reinitialize_before_sweep:
+        if reinitialize_function is None:
+            raise ValueError(
+                "Reinitialization function was not specified. The model will not be reinitialized."
+            )
+        else:
+            for v, val in reinitialize_values.items():
+                if not v.fixed:
+                    v.set_value(val, skip_validation=True)
+            reinitialize_function(model, **reinitialize_kwargs)
+
+    try:
+        # Simulate/optimize with this set of parameter
+        with capture_output():
+            results = optimize_function(model, **optimize_kwargs)
+        pyo.assert_optimal_termination(results)
+
+    except:
+        # run_successful remains false. We try to reinitialize and solve again
+        if reinitialize_function is not None:
+            for v, val in reinitialize_values.items():
+                if not v.fixed:
+                    v.set_value(val, skip_validation=True)
+            try:
+                reinitialize_function(model, **reinitialize_kwargs)
+                with capture_output():
+                    results = optimize_function(model, **optimize_kwargs)
+                pyo.assert_optimal_termination(results)
+
+            except:
+                pass  # run_successful is still False
+            else:
+                run_successful = True
+
+    else:
+        # If the simulation suceeds, report stats
+        run_successful = True
+
+    return run_successful
+
+# ================================================================
 
 def _do_param_sweep(
     model,
@@ -781,6 +833,7 @@ def _do_param_sweep(
     reinitialize_function,
     reinitialize_kwargs,
     reinitialize_before_sweep,
+    test_function,
     comm,
 ):
 
@@ -800,6 +853,8 @@ def _do_param_sweep(
         reinitialize_values = ComponentMap()
         for v in model.component_data_objects(pyo.Var):
             reinitialize_values[v] = v.value
+    else:
+        reinitialize_values = None
 
     # ================================================================
     # Run all optimization cases
@@ -811,44 +866,50 @@ def _do_param_sweep(
 
         run_successful = False  # until proven otherwise
 
-        # Forced reinitialization of the flowsheet if enabled
-        if reinitialize_before_sweep:
-            if reinitialize_function is None:
-                raise ValueError(
-                    "Reinitialization function was not specified. The model will not be reinitialized."
-                )
-            else:
-                for v, val in reinitialize_values.items():
-                    if not v.fixed:
-                        v.set_value(val, skip_validation=True)
-                reinitialize_function(model, **reinitialize_kwargs)
-
-        try:
-            # Simulate/optimize with this set of parameter
-            with capture_output():
-                results = optimize_function(model, **optimize_kwargs)
-            pyo.assert_optimal_termination(results)
-
-        except:
-            # run_successful remains false. We try to reinitialize and solve again
-            if reinitialize_function is not None:
-                for v, val in reinitialize_values.items():
-                    if not v.fixed:
-                        v.set_value(val, skip_validation=True)
-                try:
-                    reinitialize_function(model, **reinitialize_kwargs)
-                    with capture_output():
-                        results = optimize_function(model, **optimize_kwargs)
-                    pyo.assert_optimal_termination(results)
-
-                except:
-                    pass  # run_successful is still False
-                else:
-                    run_successful = True
-
-        else:
-            # If the simulation suceeds, report stats
-            run_successful = True
+        if test_function is None:
+            run_successful = _param_sweep_kernel(model, optimize_function, optimize_kwargs, reinitialize_before_sweep,
+                reinitialize_function, reinitialize_kwargs, reinitialize_values)
+        elif test_function(model):
+            run_successful = _param_sweep_kernel(model, optimize_function, optimize_kwargs, reinitialize_before_sweep,
+                reinitialize_function, reinitialize_kwargs, reinitialize_values)
+            # Forced reinitialization of the flowsheet if enabled
+            # if reinitialize_before_sweep:
+            #     if reinitialize_function is None:
+            #         raise ValueError(
+            #             "Reinitialization function was not specified. The model will not be reinitialized."
+            #         )
+            #     else:
+            #         for v, val in reinitialize_values.items():
+            #             if not v.fixed:
+            #                 v.set_value(val, skip_validation=True)
+            #         reinitialize_function(model, **reinitialize_kwargs)
+            #
+            # try:
+            #     # Simulate/optimize with this set of parameter
+            #     with capture_output():
+            #         results = optimize_function(model, **optimize_kwargs)
+            #     pyo.assert_optimal_termination(results)
+            #
+            # except:
+            #     # run_successful remains false. We try to reinitialize and solve again
+            #     if reinitialize_function is not None:
+            #         for v, val in reinitialize_values.items():
+            #             if not v.fixed:
+            #                 v.set_value(val, skip_validation=True)
+            #         try:
+            #             reinitialize_function(model, **reinitialize_kwargs)
+            #             with capture_output():
+            #                 results = optimize_function(model, **optimize_kwargs)
+            #             pyo.assert_optimal_termination(results)
+            #
+            #         except:
+            #             pass  # run_successful is still False
+            #         else:
+            #             run_successful = True
+            #
+            # else:
+            #     # If the simulation suceeds, report stats
+            #     run_successful = True
 
         # Update the loop based on the reinitialization
         _update_local_output_dict(
@@ -969,6 +1030,7 @@ def parameter_sweep(
     reinitialize_function=None,
     reinitialize_kwargs=None,
     reinitialize_before_sweep=False,
+    test_function=None,
     mpi_comm=None,
     debugging_data_dir=None,
     interpolate_nan_outputs=False,
@@ -1104,6 +1166,7 @@ def parameter_sweep(
         reinitialize_function,
         reinitialize_kwargs,
         reinitialize_before_sweep,
+        test_function,
         comm,
     )
 
