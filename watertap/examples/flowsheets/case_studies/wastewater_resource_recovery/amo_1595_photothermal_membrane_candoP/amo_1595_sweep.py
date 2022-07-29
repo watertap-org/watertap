2c38ba527e2f5be60608d7c638829f7c56fdf5b6
arao53 <akshay.rao@keylogic.com>
2022-07-29 10:59:35 -0700

    Implemented tempfile approach to removing unwanted csvs in amo_1595 sweeps

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/amo_1595_photothermal_membrane_candoP/amo_1595_sweep.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/amo_1595_photothermal_membrane_candoP/amo_1595_sweep.py
@@ -31,7 +31,7 @@ def set_up_sensitivity(m):
     return outputs, optimize_kwargs, opt_function
 
 
-def run_analysis(case_num=1, nx=11, interpolate_nan_outputs=True, save_outputs=False):
+def run_analysis(case_num=1, nx=11, interpolate_nan_outputs=True, save_outputs=None):
     m = amo_1595.main()[0]
     outputs, optimize_kwargs, opt_function = set_up_sensitivity(m)
     sweep_params = {}
@@ -47,21 +47,12 @@ def run_analysis(case_num=1, nx=11, interpolate_nan_outputs=True, save_outputs=F
     else:
         raise ValueError(f"{case_num} is not yet implemented")
 
-    if save_outputs is False:
-        output_path = None
-    else:
-        output_filename = "sensitivity_" + str(case_num) + ".csv"
-
-        output_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            output_filename,
-        )
     # run sweep
     global_results = parameter_sweep(
         m,
         sweep_params,
         outputs,
-        csv_results_file_name=output_path,
+        csv_results_file_name=save_outputs,
         optimize_function=opt_function,
         optimize_kwargs=optimize_kwargs,
         interpolate_nan_outputs=interpolate_nan_outputs,
