2bbc19673d8b58a8749bba708295ce838e62b6bd
hunterbarber <bjh0005@mix.wvu.edu>
2022-08-29 14:04:29 -0400

    adding ed_0d as test frame

--- watertap/core/util/tests/test_scaling.py
+++ watertap/core/util/tests/test_scaling.py
@@ -49,6 +49,7 @@ from watertap.unit_models.gac import (
     FilmTransferCoefficientType,
     SurfaceDiffusionCoefficientType,
 )
+from watertap.unit_models.electrodialysis_0D import Electrodialysis0D
 from watertap.costing import WaterTAPCosting
 
 __author__ = "Hunter Barber"
@@ -57,7 +58,7 @@ solver = get_solver()
 
 
 # -----------------------------------------------------------------------------
-# pull gaC_frame_simplified from test_gac
+# pull gac_frame_simplified from test_gac
 class TestGACSimplified:
     @pytest.fixture(scope="class")
     def gac_frame_simplified(self):
@@ -149,6 +150,122 @@ class TestGACSimplified:
 
         # test variable sens generator with gac model
         sens_var_lst = list(variable_sens_generator(m_gac, zero=1e-8))
+        for i in sens_var_lst:
+            print(i)
+
+        assert sens_var_lst == []
+
+
+# -----------------------------------------------------------------------------
+# pull electrodialysis_cell_frame from test_electrodialysis_0D.py
+class TestElectrodialysisVoltageConst:
+    @pytest.fixture(scope="class")
+    def electrodialysis_cell_frame(self):
+        m_ed = ConcreteModel()
+
+        m_ed.fs = FlowsheetBlock(default={"dynamic": False})
+        ion_dict = {
+            "solute_list": ["Na_+", "Cl_-"],
+            "mw_data": {"H2O": 18e-3, "Na_+": 23e-3, "Cl_-": 35.5e-3},
+            "electrical_mobility_data": {"Na_+": 5.19e-8, "Cl_-": 7.92e-8},
+            "charge": {"Na_+": 1, "Cl_-": -1},
+        }
+
+        m_ed.fs.properties = DSPMDEParameterBlock(default=ion_dict)
+        m_ed.fs.unit = Electrodialysis0D(
+            default={
+                "property_package": m_ed.fs.properties,
+                "operation_mode": "Constant_Voltage",
+            }
+        )
+
+        # Specify a system
+        # Note: Testing scenarios in this file are primarily in accord with an experimental
+        # setup reported by Campione et al. in Desalination 465 (2019): 79-93.
+        # set the operational parameters
+        m_ed.fs.unit.water_trans_number_membrane["cem"].fix(5.8)
+        m_ed.fs.unit.water_trans_number_membrane["aem"].fix(4.3)
+        m_ed.fs.unit.water_permeability_membrane["cem"].fix(2.16e-14)
+        m_ed.fs.unit.water_permeability_membrane["aem"].fix(1.75e-14)
+        m_ed.fs.unit.voltage.fix(0.5)
+        m_ed.fs.unit.electrodes_resistance.fix(0)
+        m_ed.fs.unit.cell_pair_num.fix(10)
+        m_ed.fs.unit.current_utilization.fix(1)
+        m_ed.fs.unit.spacer_thickness.fix(2.7e-4)
+        m_ed.fs.unit.membrane_areal_resistance["cem"].fix(1.89e-4)
+        m_ed.fs.unit.membrane_areal_resistance["aem"].fix(1.77e-4)
+        m_ed.fs.unit.cell_width.fix(0.1)
+        m_ed.fs.unit.cell_length.fix(0.79)
+        m_ed.fs.unit.membrane_thickness["aem"].fix(1.3e-4)
+        m_ed.fs.unit.membrane_thickness["cem"].fix(1.3e-4)
+        m_ed.fs.unit.solute_diffusivity_membrane["cem", "Na_+"].fix(1.8e-10)
+        m_ed.fs.unit.solute_diffusivity_membrane["aem", "Na_+"].fix(1.25e-10)
+        m_ed.fs.unit.solute_diffusivity_membrane["cem", "Cl_-"].fix(1.8e-10)
+        m_ed.fs.unit.solute_diffusivity_membrane["aem", "Cl_-"].fix(1.25e-10)
+        m_ed.fs.unit.ion_trans_number_membrane["cem", "Na_+"].fix(1)
+        m_ed.fs.unit.ion_trans_number_membrane["aem", "Na_+"].fix(0)
+        m_ed.fs.unit.ion_trans_number_membrane["cem", "Cl_-"].fix(0)
+        m_ed.fs.unit.ion_trans_number_membrane["aem", "Cl_-"].fix(1)
+
+        # set the inlet stream
+        m_ed.fs.unit.inlet_diluate.pressure.fix(101325)
+        m_ed.fs.unit.inlet_diluate.temperature.fix(298.15)
+        m_ed.fs.unit.inlet_diluate.flow_mol_phase_comp[0, "Liq", "H2O"].fix(2.40e-1)
+        m_ed.fs.unit.inlet_diluate.flow_mol_phase_comp[0, "Liq", "Na_+"].fix(7.38e-4)
+        m_ed.fs.unit.inlet_diluate.flow_mol_phase_comp[0, "Liq", "Cl_-"].fix(7.38e-4)
+        m_ed.fs.unit.inlet_concentrate.pressure.fix(101325)
+        m_ed.fs.unit.inlet_concentrate.temperature.fix(298.15)
+        m_ed.fs.unit.inlet_concentrate.flow_mol_phase_comp[0, "Liq", "H2O"].fix(2.40e-1)
+        m_ed.fs.unit.inlet_concentrate.flow_mol_phase_comp[0, "Liq", "Na_+"].fix(
+            7.38e-4
+        )
+        m_ed.fs.unit.inlet_concentrate.flow_mol_phase_comp[0, "Liq", "Cl_-"].fix(
+            7.38e-4
+        )
+
+        return m_ed
+
+    @pytest.mark.component
+    def test_initialization_scaling(self, electrodialysis_cell_frame):
+        m_ed = electrodialysis_cell_frame
+
+        assert degrees_of_freedom(m_ed) == 0
+
+        # set default scaling for state vars
+        m_ed.fs.properties.set_default_scaling(
+            "flow_mol_phase_comp", 1e2, index=("Liq", "H2O")
+        )
+        m_ed.fs.properties.set_default_scaling(
+            "flow_mol_phase_comp", 1e4, index=("Liq", "Na_+")
+        )
+        m_ed.fs.properties.set_default_scaling(
+            "flow_mol_phase_comp", 1e4, index=("Liq", "Cl_-")
+        )
+
+        calculate_scaling_factors(m_ed.fs)
+        initialization_tester(m_ed)
+
+        badly_scaled_var_values = {
+            var.name: val for (var, val) in badly_scaled_var_generator(m_ed)
+        }
+        assert not badly_scaled_var_values
+
+        results = solver.solve(m_ed)
+        check_optimal_termination(results)
+
+        badly_scaled_var_values = {
+            var.name: val for (var, val) in badly_scaled_var_generator(m_ed)
+        }
+        assert not badly_scaled_var_values
+
+    @pytest.mark.unit
+    def test_variable_sens_generator_electrodialysis_0D(
+        self, electrodialysis_cell_frame
+    ):
+        m_ed = electrodialysis_cell_frame
+
+        # test variable sens generator with electrodialysis 0D model
+        sens_var_lst = list(variable_sens_generator(m_ed))
         for i in sens_var_lst:
             print(i)
 
