8ad7973732a8db688d73c916ff99cf56c2e1be90
tarnold17 <travis.arnold17@gmail.com>
2022-09-06 19:28:47 -0500

    costing updates for 1595

--- watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/amo_1595_photothermal_membrane_candoP/amo_1595_sweep.py
+++ watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/amo_1595_photothermal_membrane_candoP/amo_1595_sweep.py
@@ -25,13 +25,14 @@ def set_up_sensitivity(m):
 
     # create outputs
     outputs["LCOW"] = m.fs.costing.LCOW
-    outputs["LCOW_bcp"] = m.fs.costing.LCOW_bcp
+    outputs["LCOW_with_revenue"] = m.fs.costing.LCOW_with_revenue
     outputs["LCOT"] = m.fs.costing.LCOT
+    outputs["LCOT_with_revenue"] = m.fs.costing.LCOT_with_revenue
 
     return outputs, optimize_kwargs, opt_function
 
 
-def run_analysis(case_num=1, nx=11, interpolate_nan_outputs=True, save_outputs=None):
+def run_analysis(case_num=1, nx=2, interpolate_nan_outputs=True, save_outputs=None):
     m = amo_1595.main()[0]
     outputs, optimize_kwargs, opt_function = set_up_sensitivity(m)
     sweep_params = {}
@@ -39,8 +40,6 @@ def run_analysis(case_num=1, nx=11, interpolate_nan_outputs=True, save_outputs=N
     if case_num == 1:
         sweep_params["bcp_cost"] = LinearSample(m.fs.costing.bcp_cost, 0.1, 1, nx)
     elif case_num == 2:
-        sweep_params["water_cost"] = LinearSample(m.fs.costing.water_cost, 0, 2, nx)
-    elif case_num == 3:
         sweep_params["nitrous_oxide_cost"] = LinearSample(
             m.fs.costing.nitrous_oxide_cost, 0, 5, nx
         )
