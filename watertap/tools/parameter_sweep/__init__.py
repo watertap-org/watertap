1ecbad0dbb1f2def9a762ef27640ca56f1d21b63
Bernard Knueven <Bernard.Knueven@nrel.gov>
2022-09-14 08:41:45 -0600

    fixing import

--- watertap/tools/parameter_sweep/__init__.py
+++ watertap/tools/parameter_sweep/__init__.py
@@ -11,3 +11,7 @@ from watertap.tools.parameter_sweep.parameter_sweep import parameter_sweep
 from watertap.tools.parameter_sweep.recursive_parameter_sweep import (
     recursive_parameter_sweep,
 )
+from watertap.tools.parameter_sweep.parameter_sweep_input_parser import (
+    get_sweep_params_from_yaml,
+    set_defaults_from_yaml,
+)
