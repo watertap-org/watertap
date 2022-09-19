69876f6168274ff89bef09debcd3ffac6cd3b835
MarcusHolly <marcus.holly@keylogic.com>
2022-09-19 13:03:24 -0400

    Fixes zero order costing issue

--- watertap/core/zero_order_costing.py
+++ watertap/core/zero_order_costing.py
@@ -45,8 +45,8 @@ from watertap.unit_models.zero_order import (
     FilterPressZO,
     FixedBedZO,
     GACZO,
-    HTGZO,
     HRCSZO,
+    HTGZO,
     IonExchangeZO,
     IronManganeseRemovalZO,
     LandfillZO,
@@ -106,7 +106,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
     def build_global_params(self):
         """
         To minimize overhead, only create global parameters for now.
-
         Unit-specific parameters will be added as sub-Blocks on a case-by-case
         basis as a unit of that type is costed.
         """
@@ -332,7 +331,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
     def add_LCOW(self, flow_rate):
         """
         Add Levelized Cost of Water (LCOW) to costing block.
-
         Args:
             flow_rate - flow rate of water (volumetric) to be used in
                         calculating LCOW
@@ -354,7 +352,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
     def add_electricity_intensity(self, flow_rate):
         """
         Add calculation of overall electricity intensity to costing block.
-
         Args:
             flow_rate - flow rate of water (volumetric) to be used in
                         calculating electricity intensity
@@ -373,12 +370,9 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing equipment based on power law form. This is
         the most common costing form for zero-order models.
-
         CapCost = A*(F/Fref)**B
-
         This method also registers electricity demand as a costed flow (if
         present in the unit operation model).
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -644,7 +638,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing brine concentration processes. Capital cost
         is based on the volumetirc flowrate and TDS of the incoming stream and
         the water recovery.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -713,10 +706,8 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing chemical addition processes. Capital cost is
         based on the mass flow rate of chemical added.
-
         This method also registers the chemical flow and electricity demand as
         costed flows.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -764,7 +755,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing chlorination units. Capital cost is based on
         the both inlet flow and dosage of chlorine.
-
         This method also registers the chemical flow and electricity demand as
         costed flows.
         """
@@ -833,7 +823,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing coagulation/flocculation processes. Capital cost
         is based on the alum flowrate and the polymer flowrate of the incoming stream.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -1044,7 +1033,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing deep well injection processes. Capital cost
         is based on the cost of pump and pipe.
         This method also registers the electricity demand as a costed flow.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -1112,7 +1100,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing dynamic membrane bioreactor. Capital cost
         is based on the cost of membrane area.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -1163,7 +1150,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing electrochemical nutrient recovery. Capital cost
         is based on the volumetirc flowrate and HRT of the incoming stream. Chemical
         dosing cost is based on MgCl2 cost.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -1220,10 +1206,8 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing fixed bed units. This primarily calls the
         cost_power_law_flow method.
-
         This method also registers demand for a number of additional material
         flows.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -1260,7 +1244,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing granular activated carbon processes. Capital
         cost is based on the inlet flow rate of liquid and the empty bed
         contacting time.
-
         This method also registers electricity and activated carbon consumption
         as costed flows.
         """
@@ -1516,7 +1499,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing membrane aerated biofilm reactor. Capital cost
         is based on the cost of reactor and blower.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -1576,7 +1558,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing the metab reactor. Capital cost
         is based on the cost of reactor, mixer, METAB beads, membrane,
         and vacuum pump.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -1677,7 +1658,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         blk.eq_DCC_membrane = pyo.Constraint(
             expr=blk.DCC_membrane
             == pyo.units.convert(
-                blk.unit_model.get_inlet_flow(0)
+                blk.unit_model.get_inlet_flow(t0)
                 * membrane_sidestream_fraction
                 * membrane_specific_size
                 * membrane_cost,
@@ -1687,7 +1668,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         blk.eq_DCC_vacuum = pyo.Constraint(
             expr=blk.DCC_vacuum
             == pyo.units.convert(
-                blk.unit_model.properties_byproduct[0].flow_mass_comp[
+                blk.unit_model.properties_byproduct[t0].flow_mass_comp[
                     blk.unit_model._gas_comp
                 ]
                 * vacuum_cost,
@@ -1736,7 +1717,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         )
         blk.config.flowsheet_costing_block.cost_flow(blk.unit_model.heat[t0], "heat")
         blk.config.flowsheet_costing_block.cost_flow(
-            blk.unit_model.properties_byproduct[0].flow_mass_comp[
+            blk.unit_model.properties_byproduct[t0].flow_mass_comp[
                 blk.unit_model._gas_comp
             ],
             blk.unit_model._gas_comp + "_product",
@@ -1747,7 +1728,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         Two methods for costing ion exchange:
         (1) General method for costing ion exchange units. Capital cost is based on
         the both inlet flow and TDS.
-
         This method also registers the NaCl demand, resin replacement and
         electricity demand as costed flows.
         (2) General method using unit capex and unit opex cost parameters, tailored for AMO
@@ -1889,9 +1869,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing iron and manganese removal processes. Capital cost
         is based on the cost of air blower, backwash and dual media filter.
-
         This method also registers the electricity demand as a costed flow.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -1970,7 +1948,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing low pressure pump. Capital cost
         is based on the cost of inlet flow rate.
-
         This method also registers the electricity demand as a costed flow.
         """
         t0 = blk.flowsheet().time.first()
@@ -2017,7 +1994,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing sedimentaion processes. Capital cost is
         based on the surface area of the basin.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -2117,7 +2093,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing storage tanks. Capital cost is based on the
         volume of the tank.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -2459,19 +2434,17 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         )
 
         # Get costing parameter sub-block for this technology
-        A, B, C, D = _get_tech_parameters(
+        A, B = _get_tech_parameters(
             blk,
             parameter_dict,
             blk.unit_model.config.process_subtype,
             [
-                "uv_capital_a_parameter",
-                "uv_capital_b_parameter",
-                "uv_capital_c_parameter",
-                "uv_capital_d_parameter",
+                "reactor_cost",
+                "lamp_cost",
             ],
         )
 
-        expr = ZeroOrderCostingData._get_uv_capital_cost(blk, A, B, C, D)
+        expr = ZeroOrderCostingData._get_uv_capital_cost(blk, A, B)
 
         # Determine if a costing factor is required
         factor = parameter_dict["capital_cost"]["cost_factor"]
@@ -2504,22 +2477,20 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         )
 
         # Get costing parameter sub-block for this technology
-        A, B, C, D, E, F = _get_tech_parameters(
+        A, B, C, D = _get_tech_parameters(
             blk,
             parameter_dict,
             blk.unit_model.config.process_subtype,
             [
-                "uv_capital_a_parameter",
-                "uv_capital_b_parameter",
-                "uv_capital_c_parameter",
-                "uv_capital_d_parameter",
+                "reactor_cost",
+                "lamp_cost",
                 "aop_capital_a_parameter",
                 "aop_capital_b_parameter",
             ],
         )
 
-        expr = ZeroOrderCostingData._get_uv_capital_cost(blk, A, B, C, D)
-        expr += ZeroOrderCostingData._get_aop_capital_cost(blk, E, F)
+        expr = ZeroOrderCostingData._get_uv_capital_cost(blk, A, B)
+        expr += ZeroOrderCostingData._get_aop_capital_cost(blk, C, D)
 
         # Determine if a costing factor is required
         factor = parameter_dict["capital_cost"]["cost_factor"]
@@ -2657,7 +2628,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         """
         General method for costing landfill. Capital cost is based on the total mass and
         capacity basis.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -2756,7 +2726,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         General method for costing nanofiltration. Costing is carried out
         using either the general_power_law form or the standard form which
         computes membrane cost and replacement rate.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -2976,6 +2945,14 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         # Register flows
         blk.config.flowsheet_costing_block.cost_flow(
             blk.unit_model.electricity[t0], "electricity"
+        )
+        blk.config.flowsheet_costing_block.cost_flow(
+            blk.unit_model.properties_in[t0].flow_mass_comp["filtration_media"],
+            "filtration_media",
+        )
+        blk.config.flowsheet_costing_block.cost_flow(
+            blk.unit_model.properties_byproduct[t0].flow_mass_comp["filtration_media"],
+            "filtration_media_disposal",
         )
 
     def cost_vfa_recovery(blk):
@@ -3250,31 +3227,21 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
 
         return expr
 
-    def _get_uv_capital_cost(blk, A, B, C, D):
+    def _get_uv_capital_cost(blk, A, B):
         """
         Generate expression for capital cost of UV reactor.
         """
         t0 = blk.flowsheet().time.first()
 
         Q = pyo.units.convert(
-            pyo.units.convert(
-                blk.unit_model.properties_in[t0].flow_vol,
-                to_units=pyo.units.Mgallons / pyo.units.day,
-            )
-            / (pyo.units.Mgallons / pyo.units.day),
-            to_units=pyo.units.dimensionless,
+            blk.unit_model.properties_in[t0].flow_vol,
+            to_units=pyo.units.m**3 / pyo.units.hr,
         )
 
-        uv_dose = pyo.units.convert(
-            blk.unit_model.uv_reduced_equivalent_dose[t0]
-            / (pyo.units.mJ / pyo.units.cm**2),
-            to_units=pyo.units.dimensionless,
-        )
-
-        uvt_in = blk.unit_model.uv_transmittance_in[t0]
+        E = pyo.units.convert(blk.unit_model.electricity[t0], to_units=pyo.units.kW)
 
         expr = pyo.units.convert(
-            A * Q + B * uv_dose * Q + C * (Q * uvt_in) ** 7 + D * uv_dose * Q * uvt_in,
+            A * Q + B * E,
             to_units=blk.config.flowsheet_costing_block.base_currency,
         )
 
@@ -3306,7 +3273,6 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
     ):
         """
         General method for building power law costing expressions.
-
         Args:
             number_of_parallel_units (int, optional) - cost this unit as
                         number_of_parallel_units parallel units (default: 1)
@@ -3474,7 +3440,6 @@ def _get_unit_cost_method(blk):
 def _load_case_study_definition(self):
     """
     Load data from case study definition file into a Python dict.
-
     If users did not provide a definition file as a config argument, the
     default definition from the WaterTap techno-economic database is used.
     """
