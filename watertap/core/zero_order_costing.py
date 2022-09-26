a96d027ae461d3010a374ebce0cfadb0569f76ec
tarnold17 <travis.arnold17@gmail.com>
2022-09-26 09:35:18 -0500

    initial cloth media flitration, membrane evaporator, and anaerobic digestion reactive ZO models

--- watertap/core/zero_order_costing.py
+++ watertap/core/zero_order_costing.py
@@ -34,6 +34,7 @@ from watertap.unit_models.zero_order import (
     CANDOPZO,
     ChemicalAdditionZO,
     ChlorinationZO,
+    ClothMediaFiltrationZO,
     CoagulationFlocculationZO,
     CofermentationZO,
     ConstructedWetlandsZO,
@@ -49,6 +50,7 @@ from watertap.unit_models.zero_order import (
     IronManganeseRemovalZO,
     LandfillZO,
     MABRZO,
+    MembraneEvaporatorZO,
     MetabZO,
     MicrobialBatteryZO,
     NanofiltrationZO,
@@ -3040,7 +3042,8 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
 
         # Get parameter dict from database
         parameter_dict = blk.unit_model.config.database.get_unit_operation_parameters(
-            blk.unit_model._tech_type, subtype=blk.unit_model.config.process_subtype
+            blk.unit_model._tech_type,
+            subtype=blk.unit_model.config.process_subtype,
         )
 
         # Get costing parameter sub-block for this technology
@@ -3061,6 +3064,101 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
 
         expr = pyo.units.convert(
             blk.unit_model.properties_in[t0].flow_vol * sizing_cost,
+            to_units=blk.config.flowsheet_costing_block.base_currency,
+        )
+
+        # Determine if a costing factor is required
+        factor = parameter_dict["capital_cost"]["cost_factor"]
+
+        if factor == "TPEC":
+            expr *= blk.config.flowsheet_costing_block.TPEC
+        elif factor == "TIC":
+            expr *= blk.config.flowsheet_costing_block.TIC
+
+        blk.capital_cost_constraint = pyo.Constraint(expr=blk.capital_cost == expr)
+
+        # Register flows
+        blk.config.flowsheet_costing_block.cost_flow(
+            blk.unit_model.electricity[t0], "electricity"
+        )
+
+    def cost_cloth_media_filtration(blk):
+        """
+        General method for costing cloth media filtration unit.
+        """
+        t0 = blk.flowsheet().time.first()
+
+        # Get parameter dict from database
+        parameter_dict = blk.unit_model.config.database.get_unit_operation_parameters(
+            blk.unit_model._tech_type,
+            subtype=blk.unit_model.config.process_subtype,
+        )
+
+        # Get costing parameter sub-block for this technology
+        sizing_cost = _get_tech_parameters(
+            blk,
+            parameter_dict,
+            blk.unit_model.config.process_subtype,
+            ["sizing_cost"],
+        )
+
+        # Add cost variable and constraint
+        blk.capital_cost = pyo.Var(
+            initialize=1,
+            units=blk.config.flowsheet_costing_block.base_currency,
+            bounds=(0, None),
+            doc="Capital cost of unit operation",
+        )
+
+        expr = pyo.units.convert(
+            blk.unit_model.properties_in[t0].flow_vol * sizing_cost,
+            to_units=blk.config.flowsheet_costing_block.base_currency,
+        )
+
+        # Determine if a costing factor is required
+        factor = parameter_dict["capital_cost"]["cost_factor"]
+
+        if factor == "TPEC":
+            expr *= blk.config.flowsheet_costing_block.TPEC
+        elif factor == "TIC":
+            expr *= blk.config.flowsheet_costing_block.TIC
+
+        blk.capital_cost_constraint = pyo.Constraint(expr=blk.capital_cost == expr)
+
+        # Register flows
+        blk.config.flowsheet_costing_block.cost_flow(
+            blk.unit_model.electricity[t0], "electricity"
+        )
+
+    def cost_membrane_evaporator(blk):
+        """
+        General method for costing membrane evaporator unit.
+        """
+        t0 = blk.flowsheet().time.first()
+
+        # Get parameter dict from database
+        parameter_dict = blk.unit_model.config.database.get_unit_operation_parameters(
+            blk.unit_model._tech_type, subtype=blk.unit_model.config.process_subtype
+        )
+
+        # Get costing parameter sub-block for this technology
+        memb_cost = _get_tech_parameters(
+            blk,
+            parameter_dict,
+            blk.unit_model.config.process_subtype,
+            ["membrane_cost"],
+        )
+
+        # Add cost variable and constraint
+        blk.capital_cost = pyo.Var(
+            initialize=1,
+            units=blk.config.flowsheet_costing_block.base_currency,
+            bounds=(0, None),
+            doc="Capital cost of unit operation",
+        )
+
+        expr = pyo.units.convert(
+            blk.unit_model.membrane_area * memb_cost,
             to_units=blk.config.flowsheet_costing_block.base_currency,
         )
 
@@ -3190,6 +3288,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         BrineConcentratorZO: cost_brine_concentrator,
         ChemicalAdditionZO: cost_chemical_addition,
         ChlorinationZO: cost_chlorination,
+        ClothMediaFiltrationZO: cost_cloth_media_filtration,
         CoagulationFlocculationZO: cost_coag_and_floc,
         CofermentationZO: cost_cofermentation,
         ConstructedWetlandsZO: cost_constructed_wetlands,
@@ -3203,6 +3302,7 @@ class ZeroOrderCostingData(FlowsheetCostingBlockData):
         HTGZO: cost_hydrothermal_gasification,
         IonExchangeZO: cost_ion_exchange,
         IronManganeseRemovalZO: cost_iron_and_manganese_removal,
+        MembraneEvaporatorZO: cost_membrane_evaporator,
         MetabZO: cost_metab,
         NanofiltrationZO: cost_nanofiltration,
         OzoneZO: cost_ozonation,
