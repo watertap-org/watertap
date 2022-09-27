9c916789bab45a61cec7b6e426ee9e1caf62c03d
Ludovico Bianchi <lbianchi@lbl.gov>
2022-09-26 21:30:51 -0500

    Update setup.py
    Co-authored-by: Adam Atia <aatia@keylogic.com>

--- setup.py
+++ setup.py
@@ -148,6 +148,8 @@ setup(
             "metab = watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.metab.metab_ui",
             "RO = watertap.examples.flowsheets.case_studies.seawater_RO_desalination.seawater_RO_desalination",
             "suboxic_ASM = watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.suboxic_activated_sludge_process.suboxic_ASM_ui",
+            "Magprex = watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.amo_1575_magprex.magprex_ui",
+            "biomembrane_filtration = watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.biomembrane_filtration.biomembrane_filtration_ui",
         ],
     },
 )
