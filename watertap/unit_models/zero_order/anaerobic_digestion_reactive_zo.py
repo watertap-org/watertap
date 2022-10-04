21d6fc1c8a97878a5c90ee461f7d84f4ce72294a
luohezhiming <98901358+luohezhiming@users.noreply.github.com>
2022-10-04 12:53:00 -0400

    Add GLSD Anaerobic digester model (#789)
    * Add GLSD AD model
    
    * Add tests for flowsheet
    
    * Update watertap/data/techno_economic/anaerobic_digestion_reactive.yaml
    
    Co-authored-by: Adam Atia <aatia@keylogic.com>
    
    * Update watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/GLSD_anaerobic_digester/GLSD_anaerobic_digestion.py
    
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    
    * Revise file
    
    * Revise subtype
    
    * Update watertap/examples/flowsheets/case_studies/wastewater_resource_recovery/GLSD_anaerobic_digester/GLSD_anaerobic_digestion_global_costing.yaml
    
    Co-authored-by: Adam Atia <aatia@keylogic.com>
    
    * Revise test values
    
    Co-authored-by: Adam Atia <aatia@keylogic.com>
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>
    (cherry picked from commit fb8424fd3b3c72c419be28c5dcfd879b7a1a6025)

--- watertap/unit_models/zero_order/anaerobic_digestion_reactive_zo.py
+++ watertap/unit_models/zero_order/anaerobic_digestion_reactive_zo.py
@@ -41,30 +41,63 @@ class AnaerobicDigestionReactiveZOData(ZeroOrderBaseData):
         build_sido_reactive(self)
         constant_intensity(self)
 
-        # Create water flux variable
-        self.biogas_tss_ratio = Var(
-            units=pyunits.m**3 / pyunits.kg,
-            bounds=(0, None),
-            doc="Ratio of m^3 biogas produced / kg TSS in influent",
-        )
-        self._perf_var_dict[
-            "Ratio of m^3 biogas produced / kg TSS in influent"
-        ] = self.biogas_tss_ratio
-        self._fixed_perf_vars.append(self.biogas_tss_ratio)
+        if (
+            self.config.process_subtype == "default"
+            or self.config.process_subtype is None
+        ):
 
-        self.biogas_production = Var(
-            self.flowsheet().time,
-            units=pyunits.m**3 / pyunits.s,
-            bounds=(0, None),
-            doc="Biogas production",
-        )
-        self._perf_var_dict["Biogas production"] = self.biogas_production
-
-        @self.Constraint(self.flowsheet().time, doc="Constraint for biogas production")
-        def biogas_prod(b, t):
-            return b.biogas_production[t] == (
-                pyunits.convert(
-                    b.biogas_tss_ratio * b.properties_in[t].flow_mass_comp["tss"],
-                    to_units=pyunits.m**3 / pyunits.s,
-                )
+            # Create water flux variable
+            self.biogas_tss_ratio = Var(
+                units=pyunits.m**3 / pyunits.kg,
+                bounds=(0, None),
+                doc="Ratio of m^3 biogas produced / kg TSS in influent",
             )
+            self._perf_var_dict[
+                "Ratio of m^3 biogas produced / kg TSS in influent"
+            ] = self.biogas_tss_ratio
+            self._fixed_perf_vars.append(self.biogas_tss_ratio)
+
+            self.biogas_production = Var(
+                self.flowsheet().time,
+                units=pyunits.m**3 / pyunits.s,
+                bounds=(0, None),
+                doc="Biogas production",
+            )
+            self._perf_var_dict["Biogas production"] = self.biogas_production
+
+            @self.Constraint(
+                self.flowsheet().time, doc="Constraint for biogas production"
+            )
+            def biogas_prod(b, t):
+                return b.biogas_production[t] == (
+                    pyunits.convert(
+                        b.biogas_tss_ratio * b.properties_in[t].flow_mass_comp["tss"],
+                        to_units=pyunits.m**3 / pyunits.s,
+                    )
+                )
+
+        if self.config.process_subtype == "GLSD_anaerobic_digester":
+            self.HRT = Var(
+                units=pyunits.hour,
+                doc="Hydraulic retention time",
+            )
+
+            self._fixed_perf_vars.append(self.HRT)
+            self._perf_var_dict["Hydraulic retention time"] = self.HRT
+
+            self.reactor_volume = Var(
+                self.flowsheet().time,
+                units=pyunits.m**3,
+                doc="Reactor volume",
+            )
+
+            @self.Constraint(self.flowsheet().time, doc="Constraint for reactor volume")
+            def reactor_volume_cons(b, t):
+                return b.reactor_volume[t] == (
+                    pyunits.convert(
+                        b.HRT * b.properties_in[t].flow_vol,
+                        to_units=pyunits.m**3,
+                    )
+                )
+
+            self._perf_var_dict["Reactor volume"] = self.reactor_volume
