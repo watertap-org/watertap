ef53b137685ec6fb617ba2d5b7c4325cdd545cf6
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2022-10-21 13:46:39 -0400

    Remove unused imports in property_models, tests, and tools

--- watertap/property_models/seawater_ion_generic.py
+++ watertap/property_models/seawater_ion_generic.py
@@ -26,7 +26,6 @@ Vol. 276.
 from pyomo.environ import Param, units as pyunits
 
 from idaes.core import (
-    LiquidPhase,
     AqueousPhase,
     Solvent,
     Apparent,
@@ -37,7 +36,6 @@ from idaes.core import (
 from idaes.models.properties.modular_properties.eos.enrtl import ENRTL
 from idaes.models.properties.modular_properties.eos.enrtl_reference_states import (
     Symmetric,
-    Unsymmetric,
 )
 from idaes.models.properties.modular_properties.base.generic_property import StateIndex
 from idaes.models.properties.modular_properties.state_definitions import FpcTP
