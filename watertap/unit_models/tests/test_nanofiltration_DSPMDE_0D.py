2f281b5e2dcce2ded24376c8bc6bdc6dcdcb3244
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2022-10-26 20:03:40 -0400

    Add idaeslog import

--- watertap/unit_models/tests/test_nanofiltration_DSPMDE_0D.py
+++ watertap/unit_models/tests/test_nanofiltration_DSPMDE_0D.py
@@ -14,6 +14,7 @@
 import pytest
 import numpy as np
 from math import log
+import idaes.logger as idaeslog
 from pyomo.environ import (
     ConcreteModel,
     Constraint,
@@ -60,7 +61,6 @@ from idaes.core.util.scaling import (
 )
 from idaes.core.util.initialization import propagate_state
 from idaes.generic_models.unit_models import Feed
-import idaes.logger as idaeslog
 
 # -----------------------------------------------------------------------------
 # Get default solver for testing
