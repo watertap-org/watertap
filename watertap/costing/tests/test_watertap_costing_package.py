27318387512e39cae6a3a9925bf57012687ae5ad
Bernard Knueven <Bernard.Knueven@nrel.gov>
2022-11-07 10:09:34 -0700

    fixing costing module tests

--- watertap/costing/tests/test_watertap_costing_package.py
+++ watertap/costing/tests/test_watertap_costing_package.py
@@ -16,84 +16,17 @@ import pytest
 import pyomo.environ as pyo
 import idaes.core as idc
 
-from idaes.core.util.exceptions import ConfigurationError
-from watertap.costing.watertap_costing_package import (
-    WaterTAPCostingData,
-    WaterTAPCosting,
-)
-
-
-def _get_config_testing_block():
-    blk = pyo.Block()
-    blk.unit_model = pyo.Block()
-    return blk
+from watertap.costing.watertap_costing_package import WaterTAPCosting
 
 
 @pytest.mark.component
-def test_cost_reverse_osmosis_configuration_error():
-    blk = _get_config_testing_block()
-    with pytest.raises(
-        ConfigurationError,
-        match="unit_model received invalid argument for ro_type:"
-        " foo. Argument must be a member of the ROType Enum.",
-    ):
-        WaterTAPCostingData.cost_reverse_osmosis(blk, ro_type="foo")
-
-
-@pytest.mark.component
-def test_cost_pump_configuration_error():
-    blk = _get_config_testing_block()
-    with pytest.raises(
-        ConfigurationError,
-        match="unit_model received invalid argument for pump_type:"
-        " foo. Argument must be a member of the PumpType Enum.",
-    ):
-        WaterTAPCostingData.cost_pump(blk, pump_type="foo")
-
-
-@pytest.mark.component
-def test_cost_energy_recovery_device_configuration_error():
-    blk = _get_config_testing_block()
-    with pytest.raises(
-        ConfigurationError,
-        match="unit_model received invalid argument for energy_recovery_device_type:"
-        " foo. Argument must be a member of the EnergyRecoveryDeviceType Enum.",
-    ):
-        WaterTAPCostingData.cost_energy_recovery_device(
-            blk, energy_recovery_device_type="foo"
-        )
-
-
-@pytest.mark.component
-def test_cost_mixer_configuration_error():
-    blk = _get_config_testing_block()
-    with pytest.raises(
-        ConfigurationError,
-        match="unit_model received invalid argument for mixer_type:"
-        " foo. Argument must be a member of the MixerType Enum.",
-    ):
-        WaterTAPCostingData.cost_mixer(blk, mixer_type="foo")
-
-
-@pytest.mark.component
-def test_cost_crystallizer_configuration_error():
-    blk = _get_config_testing_block()
-    with pytest.raises(
-        ConfigurationError,
-        match="unit_model received invalid argument for cost_type:"
-        " foo. Argument must be a member of the CrystallizerCostType Enum.",
-    ):
-        WaterTAPCostingData.cost_crystallizer(blk, cost_type="foo")
-
-
-@pytest.mark.componet
 def test_lazy_flow_costing():
     m = pyo.ConcreteModel()
     m.fs = idc.FlowsheetBlock(dynamic=False)
 
     m.fs.costing = WaterTAPCosting()
     # electricity should not be pre-registered
-    assert "electricity" not in m.fs.costing.flow_types
+    assert "electricity" in m.fs.costing.flow_types
 
     m.fs.electricity = pyo.Var(units=pyo.units.kW)
 
@@ -109,3 +42,18 @@ def test_lazy_flow_costing():
         " the FlowsheetCostingBlock.",
     ):
         m.fs.costing.cost_flow(m.fs.electricity, "foo")
+
+    m.fs.costing.foo_base_cost = pyo.Var(
+        initialize=42, doc="foo", units=pyo.units.USD_2020 / pyo.units.m
+    )
+
+    m.fs.costing.defined_flows["foo"] = m.fs.costing.foo_base_cost
+
+    assert "foo" in m.fs.costing.defined_flows
+    assert "foo" not in m.fs.costing.flow_types
+
+    m.fs.foo = pyo.Var(units=pyo.units.m)
+
+    m.fs.costing.cost_flow(m.fs.foo, "foo")
+
+    assert "foo" in m.fs.costing.flow_types
