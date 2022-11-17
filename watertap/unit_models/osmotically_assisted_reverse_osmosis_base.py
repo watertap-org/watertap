c11ba7c0fe39d9a9033b23c8242d37861975eecb
Bernard Knueven <Bernard.Knueven@nrel.gov>
2022-11-17 16:41:01 +0000

    Revert "eliminate overspecification by removing excess isothermal equations; updates some tests and continue"
    This reverts commit f2277f902dcbe81287be2741f5780d98f9734c7b.

--- watertap/unit_models/osmotically_assisted_reverse_osmosis_base.py
+++ watertap/unit_models/osmotically_assisted_reverse_osmosis_base.py
@@ -183,25 +183,15 @@ class OsmoticallyAssistedReverseOsmosisBaseData(UnitModelBlockData):
 
         # # ==========================================================================
         # Feed and permeate-side isothermal conditions
-        # @self.Constraint(
-        #     self.flowsheet().config.time,
-        #     self.length_domain,
-        #     doc="Isothermal assumption for permeate",
-        # )
-        # def eq_permeate_isothermal(b, t, x):
-        #     return (
-        #         b.feed_side.properties[t, x].temperature
-        #         == b.permeate_side.properties[t, x].temperature
-        #     )
         @self.Constraint(
             self.flowsheet().config.time,
-            # self.length_domain,
-            doc="Isothermal assumption for permeate and feed",
+            self.length_domain,
+            doc="Isothermal assumption for permeate",
         )
-        def eq_isothermal(b, t):
+        def eq_permeate_isothermal(b, t, x):
             return (
-                b.feed_side.properties[t, 1].temperature
-                == b.permeate_side.properties[t, 1].temperature
+                b.feed_side.properties[t, x].temperature
+                == b.permeate_side.properties[t, x].temperature
             )
 
         # ==========================================================================
