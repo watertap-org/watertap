46db8aa11925a9a48a6ad4160e592f981209cb8e
Ludovico Bianchi <lbianchi@lbl.gov>
2022-12-01 18:07:02 -0600

    Remove import from unused and deprecated costing module

--- watertap/unit_models/cstr_injection.py
+++ watertap/unit_models/cstr_injection.py
@@ -36,7 +36,6 @@ from idaes.core.util.config import (
     is_physical_parameter_block,
     is_reaction_parameter_block,
 )
-import idaes.core.util.unit_costing as costing
 
 from watertap.core import InitializationMixin
 
