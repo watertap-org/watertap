431252d264de5c9740c733721e3f395af4a11a4c
MarcusHolly <marcus.holly@keylogic.com>
2022-12-19 16:05:18 -0500

    Start making helper function

---
+++ watertap/core/util/optimal_termination.py
@@ -0,0 +1,24 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+
+import pyomo.environ as pyo
+from pyomo.environ import check_optimal_termination
+
+
+def optimal_termination(self):
+    if check_optimal_termination(self):
+        _log.warning(
+            "The solver failed to converge to an optimal solution."
+            "This suggests that the user provided infeasible inputs or that the model "
+            "is poorly scaled, poorly initialized, or degenerate. "
+        )
