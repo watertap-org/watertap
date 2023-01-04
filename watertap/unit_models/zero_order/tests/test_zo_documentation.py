e608d8f2496ff3dd435d0c3d25b696ea0b473dbe
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-01-04 12:20:21 -0700

    consolidating tests; skip if no docs directory

--- watertap/unit_models/zero_order/tests/test_zo_documentation.py
+++ watertap/unit_models/zero_order/tests/test_zo_documentation.py
@@ -16,85 +16,44 @@ Tests that documentation exists for all zero-order unit models
 import pytest
 import os
 
-rst_path = os.path.join(
+doc_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     "..",
     "..",
     "..",
     "..",
     "docs",
+)
+
+rst_path = os.path.join(
+    doc_path,
     "technical_reference",
     "unit_models",
     "zero_order_unit_models",
 )
-rst_list = []
-exclude_rst_files = ["index.rst"]
-for file in os.listdir(rst_path):
-    filename = os.fsdecode(file)
-    if filename.endswith(".rst") and filename not in exclude_rst_files:
-        rst_list.append(filename[:-4])
-
 py_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-py_list = []
-exclude_py_files = ["__init__.py"]
-for file in os.listdir(py_path):
-    filename = os.fsdecode(file)
-    if filename.endswith(".py") and filename not in exclude_py_files:
-        py_list.append(filename[:-3])
 
 
-@pytest.mark.unit
-def test_rst_path():
-    assert os.path.exists(rst_path) == True
-
-    assert rst_path == os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "..",
-        "..",
-        "..",
-        "..",
-        "docs",
-        "technical_reference",
-        "unit_models",
-        "zero_order_unit_models",
-    )
-
-    for file in os.listdir(rst_path):
+def generate_model_list(path, extension, exclude):
+    file_list = []
+    for file in os.listdir(path):
         filename = os.fsdecode(file)
-        if filename.endswith(".rst") and filename not in exclude_rst_files:
+        if filename.endswith(extension) and filename not in exclude:
             file_path = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)),
-                "..",
-                "..",
-                "..",
-                "..",
-                "docs",
-                "technical_reference",
-                "unit_models",
-                "zero_order_unit_models",
+                path,
                 f"{file}",
             )
-            assert os.path.isfile(file_path) == True
-
-
-@pytest.mark.unit
-def test_py_path():
-    assert os.path.exists(py_path) == True
-
-    assert py_path == os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-
-    for file in os.listdir(py_path):
-        filename = os.fsdecode(file)
-        if filename.endswith(".py") and filename not in exclude_py_files:
-            file_path = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "..", f"{file}"
-            )
-            assert os.path.isfile(file_path) == True
+            assert os.path.isfile(file_path)
+            file_list.append(filename[: -len(extension)])
+    return file_list
 
 
 @pytest.mark.integration
+@pytest.mark.skipif(not os.path.exists(doc_path), reason="No docs directory")
 def test_lists_match():
-    assert len(py_list) == len(rst_list)
+
+    rst_list = generate_model_list(rst_path, ".rst", ("index.rst",))
+    py_list = generate_model_list(py_path, ".py", ("__init__.py",))
 
     # Run pytest -vv to see how the lists are different
     assert sorted(py_list) == sorted(rst_list)
