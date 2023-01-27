f23f44234274307e201ffa73f511f9858ef86627
agarciadiego <>
2023-01-27 16:28:37 -0500

    delete import

--- watertap/unit_models/anaerobic_digestor.py
+++ watertap/unit_models/anaerobic_digestor.py
@@ -53,7 +53,6 @@ from idaes.core.util.config import (
     is_physical_parameter_block,
     is_reaction_parameter_block,
 )
-import idaes.core.util.unit_costing as costing
 
 import idaes.logger as idaeslog
 from idaes.core.util import scaling as iscale
