c7379949f452032b4a49bc209d9b73316a636708
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-01-31 21:14:40 +0000

    Revert "Change names of friction factor equations to be the same"
    This reverts commit 16ce47bfaf6f5263a01abadae66ce3610564992e.

--- watertap/unit_models/tests/test_osmotically_assisted_reverse_osmosis_0D.py
+++ watertap/unit_models/tests/test_osmotically_assisted_reverse_osmosis_0D.py
@@ -232,7 +232,7 @@ def test_option_friction_factor_spiral_wound():
     )
 
     assert m.fs.unit.config.friction_factor == FrictionFactor.spiral_wound
-    assert isinstance(m.fs.unit.feed_side.eq_friction_factor, Constraint)
+    assert isinstance(m.fs.unit.eq_friction_factor_spiral_wound, Constraint)
 
 
 class TestOsmoticallyAssistedReverseOsmosis:
