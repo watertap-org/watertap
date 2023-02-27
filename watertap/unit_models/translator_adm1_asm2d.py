2ed9938fa560fbcbb41e58ed53c9aa4779d7d629
luohezhiming <chenyu.wang@keylogic.com>
2023-02-27 11:11:23 -0500

    Revise copyright

--- watertap/unit_models/translator_adm1_asm2d.py
+++ watertap/unit_models/translator_adm1_asm2d.py
@@ -1,17 +1,17 @@
-#################################################################################
-# The Institute for the Design of Advanced Energy Systems Integrated Platform
-# Framework (IDAES IP) was produced under the DOE Institute for the
-# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021
-# by the software owners: The Regents of the University of California, through
-# Lawrence Berkeley National Laboratory,  National Technology & Engineering
-# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia University
-# Research Corporation, et al.  All rights reserved.
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
 #
-# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
-# license information.
-#################################################################################
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
 """
-Modified translator block.
+Translator block representing the ADM1/ASM2D interface.
 This is copied from the Generic template for a translator block.
 Assumptions:
      * Steady-state only
