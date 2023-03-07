11470cd05d3171477802d92c3bd5fec44d1af3d0
MarcusHolly <marcus.holly@keylogic.com>
2023-03-07 15:58:03 -0500

    Update copyright headers

--- watertap/unit_models/translator_adm1_asm2d.py
+++ watertap/unit_models/translator_adm1_asm2d.py
@@ -1,15 +1,14 @@
-###############################################################################
-# WaterTAP Copyright (c) 2021, The Regents of the University of California,
-# through Lawrence Berkeley National Laboratory, Oak Ridge National
-# Laboratory, National Renewable Energy Laboratory, and National Energy
-# Technology Laboratory (subject to receipt of any required approvals from
-# the U.S. Dept. of Energy). All rights reserved.
+#################################################################################
+# WaterTAP Copyright (c) 2020-2023, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
+# National Renewable Energy Laboratory, and National Energy Technology
+# Laboratory (subject to receipt of any required approvals from the U.S. Dept.
+# of Energy). All rights reserved.
 #
 # Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
 # information, respectively. These files are also available online at the URL
 # "https://github.com/watertap-org/watertap/"
-#
-###############################################################################
+#################################################################################
 """
 Translator block representing the ADM1/ASM2d interface.
 This is copied from the Generic template for a translator block.
@@ -93,6 +92,7 @@ class TranslatorDataADM1ASM2D(TranslatorData):
         ),
     )
 
+    # TODO: Revisit how build() is set up to close match Alejandro's PR
     def build(self):
         """
         Begin building model.
