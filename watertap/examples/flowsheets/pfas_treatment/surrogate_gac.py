e025063d4bcce4123dcd4bae4bf7b75dbcfe7517
hunterbarber <bjh0005@mix.wvu.edu>
2023-03-21 12:59:12 -0400

    accurate surrogate models

--- watertap/examples/flowsheets/pfas_treatment/surrogate_gac.py
+++ watertap/examples/flowsheets/pfas_treatment/surrogate_gac.py
@@ -10,176 +10,236 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 
-import pyomo.environ as pyo
-from idaes.core.surrogate import AlamoTrainer, AlamoSurrogate
-from idaes.core.util.model_statistics import (
-    variables_near_bounds_generator,
-    total_constraints_set,
-)
-from idaes.core.util.scaling import (
-    badly_scaled_var_generator,
-)
+import numpy as np
 import pandas as pd
-from pyomo.environ import (
-    Var,
-    Param,
-    Set,
-    Constraint,
-    Piecewise,
-    Suffix,
-    NonNegativeReals,
-    PositiveIntegers,
-    units as pyunits,
-    check_optimal_termination,
-)
-from pyomo.common.config import Bool, ConfigBlock, ConfigValue, In
-from enum import Enum, auto
-
-from idaes.core import (
-    declare_process_block_class,
-    MaterialBalanceType,
-    EnergyBalanceType,
-    MomentumBalanceType,
-    UnitModelBlockData,
-    useDefault,
-)
-from idaes.core.solvers import get_solver
-from idaes.core.util.constants import Constants
-from idaes.core.util.config import is_physical_parameter_block
-from idaes.core.util.tables import create_stream_table_dataframe
-from idaes.core.util.exceptions import ConfigurationError, InitializationError
-import idaes.core.util.scaling as iscale
-import idaes.logger as idaeslog
 import idaes.core.surrogate.pysmo_surrogate as surrogate
 from idaes.core.surrogate.plotting.sm_plotter import (
     surrogate_scatter2D,
     surrogate_parity,
     surrogate_residual,
 )
-import os
-
-from watertap.core import ControlVolume0DBlock, InitializationMixin
 
 __author__ = "Hunter Barber"
 
-_log = idaeslog.getLogger(__name__)
+
+def main():
+
+    min_st_regression()
+    throughput_regression()
 
 
-freund_ninv = 0.564
-N_Bi = 23.4
+def min_st_regression():
+    # ---------------------------------------------------------------------
+    # minimum stanton number equation parameter lookup
 
-# minimum stanton number equation parameter lookup
-min_st_param_df = pd.read_csv(
-    "watertap/examples/flowsheets/pfas_treatment/min_st_parameters.csv",
-    index_col=["1/n", "Bi_min", "Bi_max", "% within constant pattern"],
-)
-print(min_st_param_df)
-freund_ninv_param_list = min_st_param_df.index.get_level_values(0).values
-freund_ninv_param_closest = freund_ninv_param_list[
-    min(
-        range(len(freund_ninv_param_list)),
-        key=lambda i: abs(freund_ninv_param_list[i] - freund_ninv),
+    min_st_ninv = list(np.linspace(0.1, 0.9, 9))
+    min_st_ninv.insert(0, 0.05)
+    min_st_bi = list(np.linspace(0.5, 10, 20)) + list(np.linspace(10, 100, 19))
+
+    ninv_list, bi_list, min_st_list = [], [], []
+
+    min_st_param_df = pd.read_csv(
+        "watertap/examples/flowsheets/pfas_treatment/min_st_parameters.csv",
+        index_col=["1/n", "Bi_min", "Bi_max", "% within constant pattern"],
     )
-]
-if N_Bi < 10:
-    lookup = min_st_param_df.loc[freund_ninv_param_closest, 0.5, 10, 0]
-else:
-    lookup = min_st_param_df.loc[freund_ninv_param_closest, 10, float("NaN"), 0]
-a0_lookup = lookup[0]
-a1_lookup = lookup[1]
+    ninv_param_list = min_st_param_df.index.get_level_values(0).values
 
-print("a0_lookup", a0_lookup)
-print("a1_lookup", a1_lookup)
+    for ninv in min_st_ninv:
+        for bi in min_st_bi:
 
-# throughput equation parameter lookup
-throughput_param_df = pd.read_csv(
-    "watertap/examples/flowsheets/pfas_treatment/throughput_parameters.csv",
-    index_col=["1/n", "Bi"],
-)
-print(throughput_param_df)
-freund_ninv_param_list = throughput_param_df.index.get_level_values(0).values
-freund_ninv_param_closest = freund_ninv_param_list[
-    min(
-        range(len(freund_ninv_param_list)),
-        key=lambda i: abs(freund_ninv_param_list[i] - freund_ninv),
+            ninv_param_closest = ninv_param_list[
+                min(
+                    range(len(ninv_param_list)),
+                    key=lambda i: abs(ninv_param_list[i] - ninv),
+                )
+            ]
+
+            if bi < 10:
+                lookup = min_st_param_df.loc[ninv_param_closest, 0.5, 10, 0]
+            else:
+                lookup = min_st_param_df.loc[ninv_param_closest, 10, float("NaN"), 0]
+
+            a0_lookup = lookup[0]
+            a1_lookup = lookup[1]
+
+            ninv_list.append(ninv)
+            bi_list.append(bi)
+            min_st = a0_lookup * bi + a1_lookup
+            min_st_list.append(min_st)
+
+    min_st_df = pd.DataFrame(
+        {
+            "ninv": ninv_list,
+            "bi": bi_list,
+            "min_st": min_st_list,
+        }
     )
-]
-N_Bi_param_list = throughput_param_df.loc[freund_ninv_param_closest, :].index.values
-N_Bi_param_closest = N_Bi_param_list[
-    min(
-        range(len(N_Bi_param_list)),
-        key=lambda i: abs(N_Bi_param_list[i] - N_Bi),
+
+    # ---------------------------------------------------------------------
+    # minimum stanton number surrogate training
+
+    trainer = surrogate.PysmoKrigingTrainer(
+        input_labels=["ninv", "bi"],
+        output_labels=["min_st"],
+        training_dataframe=min_st_df,
     )
-]
-lookup = throughput_param_df.loc[freund_ninv_param_closest, N_Bi_param_closest]
-b0_lookup = lookup[0]
-b1_lookup = lookup[1]
-b2_lookup = lookup[2]
-b3_lookup = lookup[3]
-b4_lookup = lookup[4]
+    pysmo_surr_expr = trainer.train_surrogate()
 
-print("b0_lookup", b0_lookup)
-print("b1_lookup", b1_lookup)
-print("b2_lookup", b2_lookup)
-print("b3_lookup", b3_lookup)
-print("b4_lookup", b4_lookup)
+    input_labels = trainer._input_labels
+    output_labels = trainer._output_labels
+    xmin, xmax = [0, 0.5], [1, 100]
+    input_bounds = {
+        input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))
+    }
 
-# trainer min stanton
-throughput_param_df = pd.read_csv(
-    "watertap/examples/flowsheets/pfas_treatment/throughput_parameters.csv",
-)
-# after reading or generating a DataFrame object called `data_training`
-trainer = surrogate.PysmoKrigingTrainer(
-    input_labels=["1/n", "Bi"],
-    output_labels=["B0", "B1", "B2", "B3", "B4"],
-    training_dataframe=throughput_param_df,
-)
-pysmo_surr_expr = trainer.train_surrogate()
+    show = False
+    pysmo_surr = surrogate.PysmoSurrogate(
+        pysmo_surr_expr, input_labels, output_labels, input_bounds
+    )
+    surrogate_scatter2D(
+        pysmo_surr,
+        min_st_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/min_st_scatter2D.pdf",
+        show=show,
+    )
+    surrogate_scatter2D(
+        pysmo_surr,
+        min_st_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/min_st_scatter3D.pdf",
+        show=show,
+    )
+    surrogate_parity(
+        pysmo_surr,
+        min_st_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/min_st_parity.pdf",
+        show=show,
+    )
+    surrogate_residual(
+        pysmo_surr,
+        min_st_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/min_st_residual.pdf",
+        show=show,
+    )
 
-input_labels = trainer._input_labels
-output_labels = trainer._output_labels
-xmin, xmax = [0, 0.5], [1, 100]
-input_bounds = {input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))}
+    # To save a model
+    model = pysmo_surr.save_to_file("min_st_surrogate.json", overwrite=True)
 
-pysmo_surr = surrogate.PysmoSurrogate(
-    pysmo_surr_expr, input_labels, output_labels, input_bounds
-)
 
-surrogate_scatter2D(
-    pysmo_surr, throughput_param_df, filename="scatter2D.pdf", show=True
-)
-surrogate_scatter2D(
-    pysmo_surr, throughput_param_df, filename="scatter3D.pdf", show=True
-)
-surrogate_parity(pysmo_surr, throughput_param_df, filename="parity.pdf", show=True)
-surrogate_residual(pysmo_surr, throughput_param_df, filename="residual.pdf", show=True)
+def throughput_regression():
+    # ---------------------------------------------------------------------
+    # throughput equation parameter lookup
 
-# trainer throughput
-throughput_param_df = pd.read_csv(
-    "watertap/examples/flowsheets/pfas_treatment/throughput_parameters.csv",
-)
-# after reading or generating a DataFrame object called `data_training`
-trainer = surrogate.PysmoKrigingTrainer(
-    input_labels=["1/n", "Bi"],
-    output_labels=["B0", "B1", "B2", "B3", "B4"],
-    training_dataframe=throughput_param_df,
-)
-pysmo_surr_expr = trainer.train_surrogate()
+    throughput_df = pd.read_csv(
+        "watertap/examples/flowsheets/pfas_treatment/throughput_parameters.csv",
+    )
+    throughput_ninv_bi = throughput_df.loc[:, ["1/n", "Bi"]]
+    throughput_conc_ratio = list(np.linspace(0.05, 0.95, 19))
+    throughput_conc_ratio.insert(0, 0.01)
+    throughput_conc_ratio.insert(-1, 0.99)
 
-input_labels = trainer._input_labels
-output_labels = trainer._output_labels
-xmin, xmax = [0, 0.5], [1, 100]
-input_bounds = {input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))}
+    ninv_list, bi_list, conc_ratio_list, throughput_list = [], [], [], []
 
-pysmo_surr = surrogate.PysmoSurrogate(
-    pysmo_surr_expr, input_labels, output_labels, input_bounds
-)
+    throughput_param_df = pd.read_csv(
+        "watertap/examples/flowsheets/pfas_treatment/throughput_parameters.csv",
+        index_col=["1/n", "Bi"],
+    )
+    ninv_param_list = throughput_param_df.index.get_level_values(0).values
 
-surrogate_scatter2D(
-    pysmo_surr, throughput_param_df, filename="scatter2D.pdf", show=True
-)
-surrogate_scatter2D(
-    pysmo_surr, throughput_param_df, filename="scatter3D.pdf", show=True
-)
-surrogate_parity(pysmo_surr, throughput_param_df, filename="parity.pdf", show=True)
-surrogate_residual(pysmo_surr, throughput_param_df, filename="residual.pdf", show=True)
+    for ind, row in throughput_ninv_bi.iterrows():
+        ninv = row["1/n"]
+        bi = row["Bi"]
+        for conc_ratio in throughput_conc_ratio:
+
+            ninv_param_closest = ninv_param_list[
+                min(
+                    range(len(ninv_param_list)),
+                    key=lambda i: abs(ninv_param_list[i] - ninv),
+                )
+            ]
+            N_Bi_param_list = throughput_param_df.loc[
+                ninv_param_closest, :
+            ].index.values
+            N_Bi_param_closest = N_Bi_param_list[
+                min(
+                    range(len(N_Bi_param_list)),
+                    key=lambda i: abs(N_Bi_param_list[i] - bi),
+                )
+            ]
+            lookup = throughput_param_df.loc[ninv_param_closest, N_Bi_param_closest]
+            b0_lookup = lookup[0]
+            b1_lookup = lookup[1]
+            b2_lookup = lookup[2]
+            b3_lookup = lookup[3]
+            b4_lookup = lookup[4]
+
+            ninv_list.append(ninv)
+            bi_list.append(bi)
+            conc_ratio_list.append(conc_ratio)
+            throughput = (
+                b0_lookup
+                + b1_lookup * (conc_ratio**b2_lookup)
+                + b3_lookup / (1.01 - (conc_ratio**b4_lookup))
+            )
+            throughput_list.append(throughput)
+
+    throughput_df = pd.DataFrame(
+        {
+            "ninv": ninv_list,
+            "bi": bi_list,
+            "conc_ratio": conc_ratio_list,
+            "throughput": throughput_list,
+        }
+    )
+
+    # ---------------------------------------------------------------------
+    # throughput equation surrogate training
+
+    trainer = surrogate.PysmoKrigingTrainer(
+        input_labels=["ninv", "bi", "conc_ratio"],
+        output_labels=["throughput"],
+        training_dataframe=throughput_df,
+    )
+    pysmo_surr_expr = trainer.train_surrogate()
+
+    input_labels = trainer._input_labels
+    output_labels = trainer._output_labels
+    xmin, xmax = [0, 0.5, 0.01], [1, 100, 0.99]
+    input_bounds = {
+        input_labels[i]: (xmin[i], xmax[i]) for i in range(len(input_labels))
+    }
+
+    show = False
+    pysmo_surr = surrogate.PysmoSurrogate(
+        pysmo_surr_expr, input_labels, output_labels, input_bounds
+    )
+    surrogate_scatter2D(
+        pysmo_surr,
+        throughput_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/throughput_scatter2D.pdf",
+        show=show,
+    )
+    surrogate_scatter2D(
+        pysmo_surr,
+        throughput_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/throughput_scatter3D.pdf",
+        show=show,
+    )
+    surrogate_parity(
+        pysmo_surr,
+        throughput_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/throughput_parity.pdf",
+        show=show,
+    )
+    surrogate_residual(
+        pysmo_surr,
+        throughput_df,
+        filename="watertap/examples/flowsheets/pfas_treatment/throughput_residual.pdf",
+        show=show,
+    )
+
+    # To save a model
+    model = pysmo_surr.save_to_file("throughput_surrogate.json", overwrite=True)
+
+
+if __name__ == "__main__":
+    main()
