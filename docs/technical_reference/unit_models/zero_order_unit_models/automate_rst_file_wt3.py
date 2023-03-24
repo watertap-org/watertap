d03362ebfe86ce94c145ef472941682c2f1a41ec
adam-a-a <aatia@keylogic.com>
2023-03-23 23:05:07 -0400

    revive discalimer with slight modification

--- docs/technical_reference/unit_models/zero_order_unit_models/automate_rst_file_wt3.py
+++ docs/technical_reference/unit_models/zero_order_unit_models/automate_rst_file_wt3.py
@@ -278,6 +278,15 @@ with open("index.rst", "w") as f:
     f.write("Zero-Order Unit Models\n")
     f.write("=" * len("Zero-Order Unit Models"))
     f.write("\n")
+    f.write(
+        "The zero-order models rely on default model parameter values specified in YAML files, "
+        "but these data are generally only meant to help initiate model usage for new users. Users "
+        "should supply their own values, if possible, instead of relying on the default parameter values. "
+        "The YAML database files for the zero-order models are located in :code:`watertap/data/techno_economic/`. "
+        "The name of the YAML file should match the associated model name without the ZO letters and use snake "
+        "case style. For example, the YAML file for the :code:`DualMediaFiltrationZO()` zero-order model is named "
+        ":code:`dual_media_filtration.yaml`.\n"
+    )
     f.write(".. toctree::\n")
     f.write("   :maxdepth: 1\n\n")
 
