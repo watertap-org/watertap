cd2ec4dce42f01e5e171d8352518a0df9c2cf8f0
luohezhiming <98901358+luohezhiming@users.noreply.github.com>
2023-05-11 17:45:49 -0400

    ADM1 ASM2D translator block part 1 (#935)
    * ADM1 ASM2D translator block
    
    * Minor edits/comments
    
    * Update header and intro
    
    * Revise copyright
    
    * add test
    
    * initial draft of documentation
    
    * Update index
    
    * Fix documentation syntax error
    
    * add tests
    
    * revise details
    
    * Inherit from the IDAES translator block class
    
    * Update initialize_build
    
    * Update copyright headers
    
    * Gets rid of bound_push in test file
    
    * Update documentation
    
    * Replace absolute tolerance with relative tolerance
    
    * Update import
    
    * Fix test failure
    
    * add
    
    * delete redundant files
    
    * update adm1/asm2d interface
    
    * resolve problem for X_AUT
    
    * Revise translator
    
    * Update documentation
    
    * Remove unused imports
    
    * Add S_K and S_Mg into test_solution
    
    ---------
    
    Co-authored-by: MarcusHolly <marcus.holly@keylogic.com>
    Co-authored-by: Adam Atia <aatia@keylogic.com>

--- watertap/property_models/activated_sludge/modified_asm2d_reactions.py
+++ watertap/property_models/activated_sludge/modified_asm2d_reactions.py
@@ -58,36 +58,37 @@ class ModifiedASM2dReactionParameterData(ASM2dReactionParameterData):
 
         self._reaction_block_class = ModifiedASM2dReactionBlock
 
-        for j in self.config.property_package.config.additional_solute_list:
+        if self.config.property_package.config.additional_solute_list is not None:
+            for j in self.config.property_package.config.additional_solute_list:
 
-            rate_reaction_stoichiometry_additional = {
-                ("R1", "Liq", j): 0,
-                ("R2", "Liq", j): 0,
-                ("R3", "Liq", j): 0,
-                ("R4", "Liq", j): 0,
-                ("R5", "Liq", j): 0,
-                ("R6", "Liq", j): 0,
-                ("R7", "Liq", j): 0,
-                ("R8", "Liq", j): 0,
-                ("R9", "Liq", j): 0,
-                ("R10", "Liq", j): 0,
-                ("R11", "Liq", j): 0,
-                ("R12", "Liq", j): 0,
-                ("R13", "Liq", j): 0,
-                ("R14", "Liq", j): 0,
-                ("R15", "Liq", j): 0,
-                ("R16", "Liq", j): 0,
-                ("R17", "Liq", j): 0,
-                ("R18", "Liq", j): 0,
-                ("R19", "Liq", j): 0,
-                ("R20", "Liq", j): 0,
-                ("R21", "Liq", j): 0,
-            }
-            new_rate_reaction_stoichiometry = {
-                **self.rate_reaction_stoichiometry,
-                **rate_reaction_stoichiometry_additional,
-            }
-            self.rate_reaction_stoichiometry = new_rate_reaction_stoichiometry
+                rate_reaction_stoichiometry_additional = {
+                    ("R1", "Liq", j): 0,
+                    ("R2", "Liq", j): 0,
+                    ("R3", "Liq", j): 0,
+                    ("R4", "Liq", j): 0,
+                    ("R5", "Liq", j): 0,
+                    ("R6", "Liq", j): 0,
+                    ("R7", "Liq", j): 0,
+                    ("R8", "Liq", j): 0,
+                    ("R9", "Liq", j): 0,
+                    ("R10", "Liq", j): 0,
+                    ("R11", "Liq", j): 0,
+                    ("R12", "Liq", j): 0,
+                    ("R13", "Liq", j): 0,
+                    ("R14", "Liq", j): 0,
+                    ("R15", "Liq", j): 0,
+                    ("R16", "Liq", j): 0,
+                    ("R17", "Liq", j): 0,
+                    ("R18", "Liq", j): 0,
+                    ("R19", "Liq", j): 0,
+                    ("R20", "Liq", j): 0,
+                    ("R21", "Liq", j): 0,
+                }
+                new_rate_reaction_stoichiometry = {
+                    **self.rate_reaction_stoichiometry,
+                    **rate_reaction_stoichiometry_additional,
+                }
+                self.rate_reaction_stoichiometry = new_rate_reaction_stoichiometry
 
 
 class _ModifiedASM2dReactionBlock(_ASM2dReactionBlock):
