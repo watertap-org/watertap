ee77b52a91dd5717967e16ef41253000e601845b
savannahsakhai <123977561+savannahsakhai@users.noreply.github.com>
2023-06-12 10:10:45 -0400

    Update watertap/property_models/NaCl_T_dep_prop_pack.py
    Co-authored-by: Adam Atia <aatia@keylogic.com>

--- watertap/property_models/NaCl_T_dep_prop_pack.py
+++ watertap/property_models/NaCl_T_dep_prop_pack.py
@@ -10,7 +10,7 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 """
-Initial property package for H2O-NaCl system
+Initial property package for H2O-NaCl system with temperature dependence
 """
 
 # Import Python libraries
