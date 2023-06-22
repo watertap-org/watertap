c689d02ef6228b1b4ef468c69303685856f22a2c
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2023-06-22 14:31:18 -0700

    updated bounds (hope fix pt1)

--- watertap/examples/flowsheets/nf_dspmde/tests/test_nf_with_bypass.py
+++ watertap/examples/flowsheets/nf_dspmde/tests/test_nf_with_bypass.py
@@ -19,17 +19,17 @@ from watertap.examples.flowsheets.nf_dspmde.nf_with_bypass import main
 def test_main():
     m = main()
     test_dict = {
-        "lcow": [m.fs.costing.LCOW, 0.1701],
-        "pressure": [m.fs.NF.pump.outlet.pressure[0] / 1e5, 6.414],
-        "area": [m.fs.NF.nfUnit.area, 274.409],
+        "lcow": [m.fs.costing.LCOW, 0.1376],
+        "pressure": [m.fs.NF.pump.outlet.pressure[0] / 1e5, 6.702781588429595],
+        "area": [m.fs.NF.nfUnit.area, 419.7559215919783],
         "recovery": [
             m.fs.NF.nfUnit.recovery_vol_phase[0.0, "Liq"] * 100,
-            71.2768,
+            94.9999989788293,
         ],
         "bypass": [
             m.fs.by_pass_splitter.split_fraction[0, "bypass"] * 100,
-            1.00,
+            10.592027658361907,
         ],
     }
-    for (model_result, testval) in test_dict.values():
+    for model_result, testval in test_dict.values():
         assert pytest.approx(testval, rel=1e-3) == value(model_result)
