839db0a8eb8537d69633022677e313d89ff8e194
agarciadiego <alejandro.garciadieg@keylogic.com>
2023-07-27 13:26:20 -0400

    changing unit model to separator

--- watertap/unit_models/tests/test_electroNP_ZO.py
+++ watertap/unit_models/tests/test_electroNP_ZO.py
@@ -272,7 +272,7 @@ class TestElectroNP:
 
         m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
         m.fs.costing.cost_process()
-        m.fs.costing.add_LCOW(m.fs.unit.properties_treated[0].flow_vol)
+        m.fs.costing.add_LCOW(m.fs.unit.treated_state[0].flow_vol)
         results = solver.solve(m)
 
         assert_optimal_termination(results)
