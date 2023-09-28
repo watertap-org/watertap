db34137d50ff33d82fb5f14ecab63abc1733dbd2
Zach Binger <zbinger@nrel.gov>
2023-09-28 16:35:14 -0600

    fixes to changed implementation

--- watertap/unit_models/reverse_osmosis_base.py
+++ watertap/unit_models/reverse_osmosis_base.py
@@ -798,11 +798,12 @@ class ReverseOsmosisBaseData(InitializationMixin, UnitModelBlockData):
         if iscale.get_scaling_factor(self.recovery_vol_phase) is None:
             iscale.set_scaling_factor(self.recovery_vol_phase, 1)
 
-        if iscale.get_scaling_factor(self.alpha) is None:
-            iscale.set_scaling_factor(self.alpha, 1e-8)
+        if self.config.transport_model == TransportModel.SKK:
+            if iscale.get_scaling_factor(self.alpha) is None:
+                iscale.set_scaling_factor(self.alpha, 1e-8)
 
-        if iscale.get_scaling_factor(self.reflect_coeff) is None:
-            iscale.set_scaling_factor(self.reflect_coeff, 1)
+            if iscale.get_scaling_factor(self.reflect_coeff) is None:
+                iscale.set_scaling_factor(self.reflect_coeff, 1)
 
         for (t, p, j), v in self.recovery_mass_phase_comp.items():
             if j in self.config.property_package.solvent_set:
