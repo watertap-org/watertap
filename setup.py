4b2c793e57a69e329b0e0c2636b1f9d0c0d68641
Ludovico Bianchi <lbianchi@lbl.gov>
2023-09-29 11:58:24 -0500

    0.11.dev0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ SPECIAL_DEPENDENCIES_FOR_PRERELEASE = [
 setup(
     name="watertap",
     url="https://github.com/watertap-org/watertap",
-    version="0.10.dev0",
+    version="0.11.dev0",
     description="WaterTAP modeling library",
     long_description=long_description,
     long_description_content_type="text/markdown",
