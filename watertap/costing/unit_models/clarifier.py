04065cfc5834250dad2a30066bf9069d91799075
luohezhiming <chenyu.wang@keylogic.com>
2023-11-09 10:52:25 -0500

    add clarifier

---
+++ watertap/costing/unit_models/clarifier.py
@@ -0,0 +1,227 @@
+#################################################################################
+# WaterTAP Copyright (c) 2020-2023, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
+# National Renewable Energy Laboratory, and National Energy Technology
+# Laboratory (subject to receipt of any required approvals from the U.S. Dept.
+# of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#################################################################################
+
+import math
+import pyomo.environ as pyo
+from idaes.core.util.exceptions import ConfigurationError
+from idaes.core.util.misc import StrEnum
+from ..util import (
+    make_capital_cost_var,
+    make_fixed_operating_cost_var,
+    register_costing_parameter_block,
+)
+
+
+class ClarifierType(StrEnum):
+    circular = "circular"
+    rectangular = "rectangular"
+
+
+def cost_clarifier(blk, clarifier_type=ClarifierType.circular, **kwargs):
+    """
+    Clarifier costing method
+
+    Args:
+        clarifier_type: ClarifierType Enum indicating clarifier type,
+            default = ClarifierType.circular
+
+        # `**kwargs`: Additional keywords for the ClarifierType, e.g., NaOCl
+        #     and CaOH2 mixers expect the `dosing_rate` keyword
+        #     argument.
+    """
+    if clarifier_type == clarifier_type.circular:
+        cost_circular_clarifier(blk, **kwargs)
+    elif clarifier_type == clarifier_type.rectangular:
+        cost_rectangular_clarifier(blk, **kwargs)
+    else:
+        raise ConfigurationError(
+            f"{blk.unit_model.name} received invalid argument for clarifier_type:"
+            f" {clarifier_type}. Argument must be a member of the ClarifierType Enum."
+        )
+
+
+def build_circular_clarifier_cost_param_block(blk):
+
+    blk.concstruction_a_parameter = pyo.Param(
+        initialize=-6e-4,
+        doc="A parameter for construction cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**4,
+    )
+
+    blk.concstruction_b_parameter = pyo.Param(
+        initialize=98.952,
+        doc="B parameter for construction cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**2,
+    )
+
+    blk.concstruction_c_parameter = pyo.Param(
+        initialize=191806,
+        doc="C parameter for construction cost",
+        units=pyo.units.USD_2011,
+    )
+
+    blk.O_and_M_a_parameter = pyo.Param(
+        initialize=8e-10,
+        doc="A parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**6 / pyo.units.year,
+    )
+
+    blk.O_and_M_b_parameter = pyo.Param(
+        initialize=-5e-5,
+        doc="B parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**4 / pyo.units.year,
+    )
+
+    blk.O_and_M_c_parameter = pyo.Param(
+        initialize=1.6945,
+        doc="C parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**2 / pyo.units.year,
+    )
+
+    blk.O_and_M_d_parameter = pyo.Param(
+        initialize=7207,
+        doc="D parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.year,
+    )
+
+
+@register_costing_parameter_block(
+    build_rule=build_circular_clarifier_cost_param_block,
+    parameter_block_name="circular",
+)
+def cost_circular_clarifier(blk):
+    """
+    Circular clarifier costing method
+    """
+    make_capital_cost_var(blk)
+    make_fixed_operating_cost_var(blk)
+
+    blk.capital_cost_constraint = pyo.Constraint(
+        expr=blk.capital_cost
+        == pyo.units.convert(
+            blk.costing_package.circular.concstruction_a_parameter
+            * blk.unit_model.surface_area**2
+            + blk.costing_package.circular.concstruction_b_parameter
+            * blk.unit_model.surface_area
+            + blk.costing_package.circular.concstruction_c_parameter,
+            to_units=blk.costing_package.base_currency,
+        )
+    )
+
+    surface_area = pyo.units.convert(
+        blk.unit_model.surface_area, to_units=pyo.units.ft**2
+    )
+    max_surface_area_limit = 200 * pyo.units.ft**2
+    if pyo.value(surface_area) > pyo.value(max_surface_area_limit):
+        num_O_and_M = math.ceil(
+            pyo.value(surface_area) / pyo.value(max_surface_area_limit)
+        )
+        blk.fixed_operating_cost_constraint = pyo.Constraint(
+            expr=blk.fixed_operating_cost
+            == pyo.units.convert(
+                num_O_and_M
+                * (
+                    blk.costing_package.circular.O_and_M_a_parameter
+                    * max_surface_area_limit**3
+                    + blk.costing_package.circular.O_and_M_b_parameter
+                    * max_surface_area_limit**2
+                    + blk.costing_package.circular.O_and_M_c_parameter
+                    * max_surface_area_limit
+                    + blk.costing_package.circular.O_and_M_d_parameter
+                ),
+                to_units=blk.costing_package.base_currency
+                / blk.costing_package.base_period,
+            )
+        )
+    else:
+        blk.fixed_operating_cost_constraint = pyo.Constraint(
+            expr=blk.fixed_operating_cost
+            == pyo.units.convert(
+                blk.costing_package.circular.O_and_M_a_parameter
+                * blk.unit_model.surface_area**3
+                + blk.costing_package.circular.O_and_M_b_parameter
+                * blk.unit_model.surface_area**2
+                + blk.costing_package.circular.O_and_M_c_parameter
+                * blk.unit_model.surface_area
+                + blk.costing_package.circular.O_and_M_d_parameter,
+                to_units=blk.costing_package.base_currency
+                / blk.costing_package.base_period,
+            )
+        )
+
+
+def build_rectangular_clarifier_cost_param_block(blk):
+
+    blk.concstruction_a_parameter = pyo.Param(
+        initialize=-2.9e-3,
+        doc="A parameter for construction cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**4,
+    )
+
+    blk.concstruction_b_parameter = pyo.Param(
+        initialize=169.19,
+        doc="B parameter for construction cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**2,
+    )
+
+    blk.concstruction_c_parameter = pyo.Param(
+        initialize=94365,
+        doc="C parameter for construction cost",
+        units=pyo.units.USD_2011,
+    )
+
+    blk.O_and_M_a_parameter = pyo.Param(
+        initialize=4.2948,
+        doc="A parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.ft**2 / pyo.units.year,
+    )
+
+    blk.O_and_M_b_parameter = pyo.Param(
+        initialize=8283,
+        doc="B parameter for operation and maintenance cost",
+        units=pyo.units.USD_2011 / pyo.units.year,
+    )
+
+
+@register_costing_parameter_block(
+    build_rule=build_rectangular_clarifier_cost_param_block,
+    parameter_block_name="rectangular",
+)
+def cost_rectangular_clarifier(blk):
+    """
+    Rectangular clarifier costing method
+    """
+    make_capital_cost_var(blk)
+    make_fixed_operating_cost_var(blk)
+
+    blk.capital_cost_constraint = pyo.Constraint(
+        expr=blk.capital_cost
+        == pyo.units.convert(
+            blk.costing_package.rectangular.concstruction_a_parameter
+            * blk.unit_model.surface_area**2
+            + blk.costing_package.rectangular.concstruction_b_parameter
+            * blk.unit_model.surface_area
+            + blk.costing_package.rectangular.concstruction_c_parameter,
+            to_units=blk.costing_package.base_currency,
+        )
+    )
+
+    blk.fixed_operating_cost_constraint = pyo.Constraint(
+        expr=blk.fixed_operating_cost
+        == pyo.units.convert(
+            blk.costing_package.rectangular.O_and_M_a_parameter
+            * blk.unit_model.surface_area
+            + blk.costing_package.rectangular.O_and_M_b_parameter,
+            to_units=blk.costing_package.base_currency
+            / blk.costing_package.base_period,
+        )
+    )
---
+++ watertap/unit_models/clarifier.py
@@ -0,0 +1,198 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+"""
+Clarifier unit model for BSM2 and plant-wide wastewater treatment modeling.
+This unit inherits from the IDAES separator unit.
+"""
+from enum import Enum, auto
+
+# Import IDAES cores
+from idaes.core import (
+    declare_process_block_class,
+)
+from idaes.models.unit_models.separator import SeparatorData, SplittingType
+
+from idaes.core.util.tables import create_stream_table_dataframe
+import idaes.logger as idaeslog
+
+from pyomo.environ import (
+    Var,
+    units as pyunits,
+)
+from pyomo.common.config import ConfigValue, In
+
+from idaes.core.util.exceptions import (
+    ConfigurationError,
+)
+
+__author__ = "Chenyu Wang"
+
+
+# Set up logger
+_log = idaeslog.getLogger(__name__)
+
+
+class ActivatedSludgeModelType(Enum):
+    """
+    ASM1: ASM1 model
+    ASM2D: ASM2D model
+    modified_ASM2D: modified ASM2D model for ADM1 compatibility
+    """
+
+    ASM1 = auto()
+    ASM2D = auto()
+    modified_ASM2D = auto()
+
+
+@declare_process_block_class("Clarifier")
+class ClarifierData(SeparatorData):
+    """
+    Thickener unit model for BSM2
+    """
+
+    CONFIG = SeparatorData.CONFIG()
+    CONFIG.outlet_list = ["underflow", "overflow"]
+    CONFIG.split_basis = SplittingType.componentFlow
+
+    # CONFIG.declare(
+    #     "activated_sludge_model",
+    #     ConfigValue(
+    #         default=ActivatedSludgeModelType.ASM1,
+    #         domain=In(ActivatedSludgeModelType),
+    #         description="Activated Sludge Model used with unit",
+    #         doc="""
+    #     Options to account for version of activated sludge model property package.
+    #
+    #     **default** - ``ActivatedSludgeModelType.ASM1``
+    #
+    # .. csv-table::
+    #     :header: "Configuration Options", "Description"
+    #
+    #     "``ActivatedSludgeModelType.ASM1``", "ASM1 model"
+    #     "``ActivatedSludgeModelType.ASM2D``", "ASM2D model"
+    #     "``ActivatedSludgeModelType.modified_ASM2D``", "modified ASM2D model for ADM1 compatibility"
+    # """,
+    #     ),
+    # )
+
+    def build(self):
+        """
+        Begin building model.
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        # Call UnitModel.build to set up dynamics
+        super(ClarifierData, self).build()
+
+        if "underflow" and "effluent" not in self.config.outlet_list:
+            raise ConfigurationError(
+                "{} encountered unrecognised "
+                "outlet_list. This should not "
+                "occur - please use underflow "
+                "and effluent as outlets.".format(self.name)
+            )
+
+        self.surface_area = Var(
+            initialize=5000,
+            doc="Surface area of the clarifier",
+            units=pyunits.ft**2,
+            bounds=(0, 5e4),
+        )
+
+        # self.p_thick = Param(
+        #     initialize=0.07,
+        #     units=pyunits.dimensionless,
+        #     mutable=True,
+        #     doc="Fraction of suspended solids in the underflow",
+        # )
+        #
+        # self.TSS_rem = Param(
+        #     initialize=0.98,
+        #     units=pyunits.dimensionless,
+        #     mutable=True,
+        #     doc="Fraction of suspended solids removed",
+        # )
+        #
+        # @self.Expression(self.flowsheet().time, doc="Suspended solids concentration")
+        # def TSS_in(blk, t):
+        #     if blk.config.activated_sludge_model == ActivatedSludgeModelType.ASM1:
+        #         return 0.75 * (
+        #             sum(
+        #                 blk.inlet.conc_mass_comp[t, i]
+        #                 for i in blk.config.property_package.tss_component_set
+        #             )
+        #         )
+        #     elif blk.config.activated_sludge_model == ActivatedSludgeModelType.ASM2D:
+        #         return blk.inlet.conc_mass_comp[
+        #             t, blk.config.property_package.tss_component_set.first()
+        #         ]
+        #     elif (
+        #         blk.config.activated_sludge_model
+        #         == ActivatedSludgeModelType.modified_ASM2D
+        #     ):
+        #         return blk.mixed_state[t].TSS
+        #     else:
+        #         raise ConfigurationError(
+        #             "The activated_sludge_model was not specified properly in configuration options."
+        #         )
+        #
+        # @self.Expression(self.flowsheet().time, doc="Thickening factor")
+        # def f_thick(blk, t):
+        #     return blk.p_thick * (10 / (blk.TSS_in[t]))
+        #
+        # @self.Expression(self.flowsheet().time, doc="Remove factor")
+        # def f_q_du(blk, t):
+        #     return blk.TSS_rem / (pyunits.kg / pyunits.m**3) / 100 / blk.f_thick[t]
+        #
+        # @self.Constraint(
+        #     self.flowsheet().time,
+        #     self.config.property_package.particulate_component_set,
+        #     doc="particulate fraction",
+        # )
+        # def overflow_particulate_fraction(blk, t, i):
+        #     return blk.split_fraction[t, "overflow", i] == 1 - blk.TSS_rem
+        #
+        # @self.Constraint(
+        #     self.flowsheet().time,
+        #     self.config.property_package.non_particulate_component_set,
+        #     doc="soluble fraction",
+        # )
+        # def non_particulate_components(blk, t, i):
+        #     return blk.split_fraction[t, "overflow", i] == 1 - blk.f_q_du[t]
+
+    def _get_performance_contents(self, time_point=0):
+        if hasattr(self, "split_fraction"):
+            var_dict = {}
+            for k in self.split_fraction.keys():
+                if k[0] == time_point:
+                    var_dict[f"Split Fraction [{str(k[1:])}]"] = self.split_fraction[k]
+            return {"vars": var_dict}
+        else:
+            return None
+
+    def _get_stream_table_contents(self, time_point=0):
+        outlet_list = self.create_outlet_list()
+
+        io_dict = {}
+        if self.config.mixed_state_block is None:
+            io_dict["Inlet"] = self.mixed_state
+        else:
+            io_dict["Inlet"] = self.config.mixed_state_block
+
+        for o in outlet_list:
+            io_dict[o] = getattr(self, o + "_state")
+
+        return create_stream_table_dataframe(io_dict, time_point=time_point)
