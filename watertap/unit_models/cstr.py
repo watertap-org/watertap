6658fa9b6db27ece1375ab892f7dd38cc891b024
MarcusHolly <marcus.holly@keylogic.com>
2023-11-17 22:12:33 -0500

    Add CSTR unit model based on IDAES CSTR

---
+++ watertap/unit_models/cstr.py
@@ -0,0 +1,71 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+"""
+Anoxic CSTR unit model for BSM2 and plant-wide wastewater treatment modeling.
+This unit inherits from the IDAES CSTR unit.
+"""
+from enum import Enum, auto
+
+# Import IDAES cores
+from idaes.core import (
+    declare_process_block_class,
+)
+from idaes.models.unit_models.cstr import CSTRData
+
+from idaes.core.util.tables import create_stream_table_dataframe
+import idaes.logger as idaeslog
+
+from pyomo.environ import (
+    Param,
+    units as pyunits,
+    NonNegativeReals,
+)
+from pyomo.common.config import ConfigValue, In
+
+from idaes.core.util.exceptions import (
+    ConfigurationError,
+)
+
+__author__ = "Marcus Holly"
+
+
+# Set up logger
+_log = idaeslog.getLogger(__name__)
+
+
+@declare_process_block_class("AnoxicCSTR")
+class AnoxicCSTRData(CSTRData):
+    """
+    Anoxic CSTR unit block for BSM2
+    """
+
+    CONFIG = CSTRData.CONFIG()
+
+    def build(self):
+        """
+        Begin building model.
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        # Call UnitModel.build to set up dynamics
+        super(AnoxicCSTRData, self).build()
+
+        self.HRT = Param(
+            initialize=4,
+            units=pyunits.hr,
+            mutable=True,
+            doc="Hydraulic retention time",
+        )
