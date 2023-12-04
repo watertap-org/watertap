3d83b6097e5780eec6102cb921cc5857ca02cfbb
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-12-04 15:02:30 -0700

    remove mutable=False from Params with units

--- watertap/property_models/activated_sludge/modified_asm2d_properties.py
+++ watertap/property_models/activated_sludge/modified_asm2d_properties.py
@@ -144,14 +144,12 @@ class ModifiedASM2dParameterData(PhysicalParameterBlock):
 
         # Heat capacity of water
         self.cp_mass = pyo.Param(
-            mutable=False,
             initialize=4182,
             doc="Specific heat capacity of water",
             units=pyo.units.J / pyo.units.kg / pyo.units.K,
         )
         # Density of water
         self.dens_mass = pyo.Param(
-            mutable=False,
             initialize=997,
             doc="Density of water",
             units=pyo.units.kg / pyo.units.m**3,
