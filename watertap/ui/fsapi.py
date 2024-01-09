db3868ec9d66eeff15c0b472481eec89f953d926
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2024-01-08 22:46:46 -0800

    Update fsapi.py

--- watertap/ui/fsapi.py
+++ watertap/ui/fsapi.py
@@ -821,8 +821,7 @@ class FlowsheetInterface:
                     f"'{Actions.build}') before flowsheet is built"
                 )
             else:
-                kwargs["model"] = self.fs_exp.m
-                result = action_func(flowsheet=self.fs_exp.obj, **kwargs)
+                result = action_func(flowsheet=self.fs_exp.m, **kwargs)
                 # Issue 755: Report optimization errors
                 if action_name == Actions.solve:
                     _log.debug(f"Solve result: {result}")
