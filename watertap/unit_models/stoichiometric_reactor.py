a8191b4ad7aedd1961c97457ecf5c1f91214e67a
Adam Atia <aatia@keylogic.com>
2024-01-15 14:09:22 -0500

    add indent

--- watertap/unit_models/stoichiometric_reactor.py
+++ watertap/unit_models/stoichiometric_reactor.py
@@ -220,30 +220,31 @@ class StoichiometricReactorData(UnitModelBlockData):
             default={},
             domain=dict,
             description="Specification of precipitates formed in StoichiometricReactor process",
-            doc="""A dict of precipitates formed in the StoichiometricReactor process
-    including their molecular weights, and precipitation stoichiometric coefficients for
-    the components defined in the property package in the following format:
-        .. code-block:: python
-            {
-            "precipitate_name_1":
+            doc="""
+            A dict of precipitates formed in the StoichiometricReactor process
+            including their molecular weights, and precipitation stoichiometric coefficients for
+            the components defined in the property package in the following format:
+            .. code-block:: python
                 {
-                "mw": value,  # in kg/mol
-                "precipitation_stoichiometric":
+                "precipitate_name_1":
                     {
-                        "component_name_1": stoichiometric_coeff,
-                        "component_name_2": stoichiometric_coeff
-                    }
-                },
-            "precipitate_name_2":
-                {
-                "mw": value,  # in kg/mol
-                "precipitation_stoichiometric":
+                    "mw": value,  # in kg/mol
+                    "precipitation_stoichiometric":
+                        {
+                            "component_name_1": stoichiometric_coeff,
+                            "component_name_2": stoichiometric_coeff
+                        }
+                    },
+                "precipitate_name_2":
                     {
-                        "component_name_1": stoichiometric_coeff,
-                        "component_name_2": stoichiometric_coeff
-                    }
-                },
-            }
+                    "mw": value,  # in kg/mol
+                    "precipitation_stoichiometric":
+                        {
+                            "component_name_1": stoichiometric_coeff,
+                            "component_name_2": stoichiometric_coeff
+                        }
+                    },
+                }
    
     """,
         ),
