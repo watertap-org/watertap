5f5115359ff9dd2ff5b3a276ea188a4276749116
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2024-01-15 00:04:29 -0800

    Update stoichiometric_reactor.py

--- watertap/unit_models/stoichiometric_reactor.py
+++ watertap/unit_models/stoichiometric_reactor.py
@@ -193,23 +193,23 @@ class StoichiometricReactorData(UnitModelBlockData):
                 "reagent_name_1":
                     {
                     "mw": (value, units),
+                    "density_reagent": (value, units),
                     "dissolution_stoichiometric":
                         {
-                        ("component_name_1", stoichiometric_coeff),
-                        ("component_name_2", stoichiometric_coeff)
+                        "component_name_1": stoichiometric_coeff,
+                        "component_name_2": stoichiometric_coeff
                         }
                     },
-                    "density_reagent": (value, units),
                 "reagent_name_2":
                     {
                     "mw": (value, units),
+                    "density_reagent": (value, units),
                     "dissolution_stoichiometric":
                         {
-                        ("component_name_1", stoichiometric_coeff),
-                        ("component_name_2", stoichiometric_coeff)
+                        "component_name_1": stoichiometric_coeff,
+                        "component_name_2": stoichiometric_coeff
                         }
                     },
-                    "density_reagent": (value, units),
                 }
             """,
         ),
@@ -222,30 +222,30 @@ class StoichiometricReactorData(UnitModelBlockData):
             domain=dict,
             description="Specification of precipitates formed in StoichiometricReactor process",
             doc="""
-               A dict of precipitates formed in the StoichiometricReactor process
-               including their molecular weights, and precipitation stoichiometric coefficients for
-               the components defined in the property package in the following format:
-                   {
-                   "precipitate_name_1":
-                       {
-                       "mw": value  # in kg/mol
-                       "precipitation_stoichiometric":
-                           {
-                           "component_name_1": stoichiometric_coeff,
-                           "component_name_2": stoichiometric_coeff
-                           }
-                       },
-                   "precipitate_name_2":
-                       {
-                       "mw": value  # in kg/mol
-                       "precipitation_stoichiometric":
-                           {
-                           "component_name_1": stoichiometric_coeff,
-                           "component_name_2": stoichiometric_coeff
-                           }
-                       },
-                   }
-               """,
+            A dict of precipitates formed in the StoichiometricReactor process
+            including their molecular weights, and precipitation stoichiometric coefficients for
+            the components defined in the property package in the following format:
+                {
+                "precipitate_name_1":
+                    {
+                    "mw": value  # in kg/mol
+                    "precipitation_stoichiometric":
+                        {
+                        "component_name_1": stoichiometric_coeff,
+                        "component_name_2": stoichiometric_coeff
+                        }
+                    },
+                "precipitate_name_2":
+                    {
+                    "mw": value  # in kg/mol
+                    "precipitation_stoichiometric":
+                        {
+                        "component_name_1": stoichiometric_coeff,
+                        "component_name_2": stoichiometric_coeff
+                        }
+                    },
+                }
+            """,
         ),
     )
 
