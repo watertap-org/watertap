bb5110fedcb5d631d66af4e96eb64a059b06af87
Ludovico Bianchi <lbianchi@lbl.gov>
2024-01-25 19:06:09 -0600

    0.11.0rc3

--- docs/conf.py
+++ docs/conf.py
@@ -35,9 +35,9 @@ copyright = "2020-2023, NAWI"
 author = "NAWI"
 
 # The full version, including alpha/beta/rc tags
-release = "0.11.0rc2"
+release = "0.11.0rc3"
 # The short X.Y version
-version = "0.11.0rc2"
+version = "0.11.0rc3"
 # -- General configuration ---------------------------------------------------
 
 
