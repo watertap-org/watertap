ae8204e4457fbdca495ca32100df44aceab72f94
Zach Binger <zbinger@nrel.gov>
2024-02-09 15:08:26 -0700

    Corrected Flux Eqn for RO1D

--- watertap/unit_models/reverse_osmosis_1D.py
+++ watertap/unit_models/reverse_osmosis_1D.py
@@ -174,7 +174,7 @@ class ReverseOsmosis1DData(ReverseOsmosisBaseData):
         def eq_connect_mass_transfer(b, t, x, p, j):
             return (
                 b.permeate_side[t, x].get_material_flow_terms(p, j)
-                == -b.feed_side.mass_transfer_term[t, x, p, j] * b.length / b.nfe
+                == -b.feed_side.mass_transfer_term[t, x, p, j]
             )
 
         # ==========================================================================
@@ -188,7 +188,7 @@ class ReverseOsmosis1DData(ReverseOsmosisBaseData):
         )
         def eq_mass_flux_equal_mass_transfer(b, t, x, p, j):
             return (
-                b.flux_mass_phase_comp[t, x, p, j] * b.width
+                b.flux_mass_phase_comp[t, x, p, j] * (b.area / b.nfe)
                 == -b.feed_side.mass_transfer_term[t, x, p, j]
             )
 
