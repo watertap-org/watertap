ad034a62de189128fb256bb9f868f72d45b4584f
Bernard Knueven <Bernard.Knueven@nrel.gov>
2024-02-14 12:28:20 -0700

    add test for WaterTAPCostingDetailed

--- watertap/costing/tests/test_costing_interop.py
+++ watertap/costing/tests/test_costing_interop.py
@@ -17,8 +17,7 @@ import os
 import pyomo.environ as pyo
 from idaes.core import UnitModelCostingBlock
 
-from watertap.costing import WaterTAPCosting, ZeroOrderCosting
-from watertap.costing.watertap_costing_package import WaterTAPCostingDetailed
+from watertap.costing import WaterTAPCosting, WaterTAPCostingDetailed, ZeroOrderCosting
 import watertap.examples.flowsheets.case_studies.wastewater_resource_recovery.amo_1575_hrcs.hrcs as hrcs
 
 hrcs_original_costing = hrcs.add_costing
@@ -46,6 +45,55 @@ def simple_main():
 
 
 def add_wt_costing(m):
+    m.fs.costing = WaterTAPCosting()
+
+    m.fs.costing.register_flow_type(
+        "ferric_chloride", 0.6 * pyo.units.USD_2020 / pyo.units.kg
+    )
+
+    m.fs.mixer.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+
+    m.fs.HRCS.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+    m.fs.clarifier.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+    m.fs.sep.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+
+    m.fs.costing.cost_process()
+
+    m.fs.costing.add_electricity_intensity(m.fs.feed.properties[0].flow_vol)
+    m.fs.costing.add_LCOW(m.fs.feed.properties[0].flow_vol)
+
+    m.fs.costing.add_annual_water_production(m.fs.product.properties[0].flow_vol)
+
+    m.fs.costing.add_annual_water_production(
+        m.fs.feed.properties[0].flow_vol, "annual_water_inlet"
+    )
+
+    m.fs.costing.LCOW_with_revenue = pyo.Expression(
+        expr=(
+            (
+                m.fs.costing.total_annualized_cost
+                + m.fs.costing.aggregate_flow_costs["ferric_chloride"]
+            )
+            / m.fs.costing.annual_water_production
+        ),
+        doc="Levelized Cost of Water With Revenue",
+    )
+
+    m.fs.costing.add_LCOW(m.fs.feed.properties[0].flow_vol, "LCOT")
+
+    m.fs.costing.LCOT_with_revenue = pyo.Expression(
+        expr=(
+            (
+                m.fs.costing.total_annualized_cost
+                + m.fs.costing.aggregate_flow_costs["ferric_chloride"]
+            )
+            / m.fs.costing.annual_water_inlet
+        ),
+        doc="Levelized Cost of Treatment With Revenue",
+    )
+
+
+def add_wt_costing_detailed(m):
     m.fs.costing = WaterTAPCostingDetailed()
 
     m.fs.costing.register_flow_type(
@@ -102,6 +150,24 @@ def test_hrcs_case_1575_wtcosting():
         pyo.assert_optimal_termination(results)
 
         # check costing -- baseline is 0.02003276
+        assert pyo.value(m.fs.costing.LCOW) == pytest.approx(
+            0.02419106, rel=1e-3
+        )  # in $/m**3
+    except:
+        raise
+    finally:
+        hrcs.add_costing = hrcs_original_costing
+
+
+def test_hrcs_case_1575_wtcosting_detailed():
+    hrcs.add_costing = add_wt_costing_detailed
+
+    try:
+        m, results = simple_main()
+        pyo.assert_optimal_termination(results)
+
+        # check costing -- baseline is 0.02003276
+        # LCOW should match between WaterTAPCosting and WaterTAPCostingDetailed
         assert pyo.value(m.fs.costing.LCOW) == pytest.approx(
             0.02419106, rel=1e-3
         )  # in $/m**3
