f90f46e0ae8a114f61f64ecf59dd7321d4e922cb
hunterbarber <bjh0005@mix.wvu.edu>
2024-03-07 16:20:17 -0500

    first working implementation

--- watertap/costing/unit_models/tests/test_gac.py
+++ watertap/costing/unit_models/tests/test_gac.py
@@ -14,27 +14,57 @@ import pytest
 import pyomo.environ as pyo
 import idaes.core.util.model_statistics as istat
 
+from pyomo.environ import (
+    Block,
+    Var,
+    units as pyunits,
+)
 from pyomo.util.check_units import assert_units_consistent
 from idaes.core import (
+    FlowsheetBlock,
     UnitModelCostingBlock,
 )
 from idaes.core.solvers import get_solver
 from idaes.core.util.testing import initialization_tester
 from watertap.costing import WaterTAPCosting
 from watertap.unit_models.tests.test_gac import build_crittenden
-
+from watertap.costing.unit_models.tests.unit_costing_test_harness import (
+    UnitCostingTestHarness,
+)
+from idaes.core import (
+    declare_process_block_class,
+    UnitModelBlockData,
+)
+from watertap.costing.unit_models.gac import cost_gac
 
 __author__ = "Hunter Barber"
 
 solver = get_solver()
 
 
+@declare_process_block_class("DummyUnitModel")
+class DummyUnitModelData(UnitModelBlockData):
+    def build(self):
+        super().build()
+
+    @property
+    def default_costing_method(self):
+        return cost_gac
+
+
 class TestGACCosting:
     @pytest.fixture(scope="class")
     def build(self):
-        m = build_crittenden()
-        initialization_tester(m)
-        solver.solve(m)
+        m = pyo.ConcreteModel()
+        m.fs = FlowsheetBlock(dynamic=False)
+
+        m.fs.unit = DummyUnitModel()
+        m.fs.unit.bed_volume = Var(units=pyunits.meter**3)
+        m.fs.unit.bed_volume.fix(8.900)
+        m.fs.unit.bed_mass_gac = Var(units=pyunits.kg)
+        m.fs.unit.bed_mass_gac.fix(4004)
+        m.fs.unit.gac_usage_rate = Var(units=pyunits.kg / pyunits.second)
+        m.fs.unit.gac_usage_rate.fix(0.0002925)
 
         return m
 
@@ -73,90 +103,156 @@ class TestGACCosting:
         assert pytest.approx(0.01631, rel=1e-3) == pyo.value(cost.energy_consumption)
         assert pytest.approx(40370, rel=1e-3) == pyo.value(cost.fixed_operating_cost)
 
-    @pytest.mark.component
-    def test_robust_costing_gravity(self, build):
-        mr_grav = build
 
-        mr_grav.fs.costing = WaterTAPCosting()
-        mr_grav.fs.costing.base_currency = pyo.units.USD_2020
-
-        mr_grav.fs.unit.costing = UnitModelCostingBlock(
-            flowsheet_costing_block=mr_grav.fs.costing,
-            costing_method_arguments={"contactor_type": "gravity"},
-        )
-        mr_grav.fs.costing.cost_process()
-
-        # testing gac costing block dof and initialization
-        assert assert_units_consistent(mr_grav) is None
-        assert istat.degrees_of_freedom(mr_grav) == 0
-        mr_grav.fs.unit.costing.initialize()
-
-        # solve
-        results = solver.solve(mr_grav)
-
-        # Check for optimal solution
-        assert pyo.check_optimal_termination(results)
-
-        cost = mr_grav.fs.unit.costing
-        # Check for known cost solution of default twin alternating contactors
-        assert pyo.value(mr_grav.fs.costing.gac_gravity.num_contactors_op) == 1
-        assert pyo.value(mr_grav.fs.costing.gac_gravity.num_contactors_redundant) == 1
-        assert pytest.approx(163200, rel=1e-3) == pyo.value(cost.contactor_cost)
-        assert pytest.approx(4.359, rel=1e-3) == pyo.value(cost.adsorbent_unit_cost)
-        assert pytest.approx(17450, rel=1e-3) == pyo.value(cost.adsorbent_cost)
-        assert pytest.approx(159500, rel=1e-3) == pyo.value(cost.other_process_cost)
-        assert pytest.approx(2.0 * 340200, rel=1e-3) == pyo.value(cost.capital_cost)
-        assert pytest.approx(12680, rel=1e-3) == pyo.value(cost.gac_makeup_cost)
-        assert pytest.approx(27660, rel=1e-3) == pyo.value(cost.gac_regen_cost)
-        assert pytest.approx(2.476, rel=1e-3) == pyo.value(cost.energy_consumption)
-        assert pytest.approx(40370, rel=1e-3) == pyo.value(cost.fixed_operating_cost)
-
-    @pytest.mark.component
-    def test_robust_costing_modular_contactors(self, build):
-        m = build
-
-        m.fs.costing = WaterTAPCosting()
-        m.fs.costing.base_currency = pyo.units.USD_2020
-
-        m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
-        m.fs.costing.cost_process()
-
-        m.fs.costing.gac_pressure.num_contactors_op.fix(4)
-        m.fs.costing.gac_pressure.num_contactors_redundant.fix(2)
-
-        results = solver.solve(m)
-
-        cost = m.fs.unit.costing
-        # Check for known cost solution when changing volume scale of vessels in parallel
-        assert pyo.value(m.fs.costing.gac_pressure.num_contactors_op) == 4
-        assert pyo.value(m.fs.costing.gac_pressure.num_contactors_redundant) == 2
-        assert pytest.approx(89040, rel=1e-3) == pyo.value(cost.contactor_cost)
-        assert pytest.approx(69690, rel=1e-3) == pyo.value(cost.other_process_cost)
-        assert pytest.approx(2.0 * 176200, rel=1e-3) == pyo.value(cost.capital_cost)
-
-    @pytest.mark.component
-    def test_robust_costing_max_gac_ref(self, build):
-        m = build
-
-        # scale flow up 10x
-        unit_feed = m.fs.unit.process_flow.properties_in[0]
-        unit_feed.flow_mol_phase_comp["Liq", "H2O"].fix(10 * 824.0736620370348)
-        unit_feed.flow_mol_phase_comp["Liq", "TCE"].fix(10 * 5.644342973110135e-05)
-
-        m.fs.costing = WaterTAPCosting()
-        m.fs.costing.base_currency = pyo.units.USD_2020
-
-        m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
-        m.fs.costing.cost_process()
-        # not necessarily an optimum solution because poor scaling
-        # but just checking the conditional
-        results = solver.solve(m)
-
-        # Check for bed_mass_gac_cost_ref to be overwritten
-        # if bed_mass_gac is greater than bed_mass_gac_cost_max_ref
-        assert pyo.value(m.fs.unit.bed_mass_gac) > pyo.value(
-            m.fs.costing.gac_pressure.bed_mass_max_ref
-        )
-        assert pyo.value(m.fs.unit.costing.bed_mass_gac_ref) == (
-            pytest.approx(pyo.value(m.fs.costing.gac_pressure.bed_mass_max_ref), 1e-5)
-        )
+# class TestGACHand(UnitCostingTestHarness):
+#     def configure(self):
+#
+#
+#         # Approx data pulled from graph in Hand, 1984 at ~30 days
+#         # 30 days adjusted to actual solution to account for web plot data extraction error within reason
+#         # values calculated by hand and match those reported in Hand, 1984
+#         self.unit_solutions[m.fs.unit.equil_conc] = 0.0005178
+#         self.unit_solutions[m.fs.unit.dg] = 19780
+#         self.unit_solutions[m.fs.unit.N_Bi] = 6.113
+#         self.unit_solutions[m.fs.unit.min_N_St] = 35.68
+#         self.unit_solutions[m.fs.unit.throughput] = 0.9882
+#         self.unit_solutions[m.fs.unit.min_residence_time] = 468.4
+#         self.unit_solutions[m.fs.unit.residence_time] = 134.7
+#         self.unit_solutions[m.fs.unit.min_operational_time] = 9153000
+#         self.unit_solutions[m.fs.unit.operational_time] = 2554000
+#         self.unit_solutions[m.fs.unit.bed_volumes_treated] = 8514
+#
+#         return m
+#
+#
+# class TestGACCosting:
+#     @pytest.fixture(scope="class")
+#     def build(self):
+#         m = build_crittenden()
+#         initialization_tester(m)
+#         solver.solve(m)
+#
+#         return m
+#
+#     @pytest.mark.component
+#     def test_robust_costing_pressure(self, build):
+#         m = build
+#
+#         m.fs.costing = WaterTAPCosting()
+#         m.fs.costing.base_currency = pyo.units.USD_2020
+#
+#         m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+#         m.fs.costing.cost_process()
+#
+#         # testing gac costing block dof and initialization
+#         assert assert_units_consistent(m) is None
+#         assert istat.degrees_of_freedom(m) == 0
+#         m.fs.unit.costing.initialize()
+#
+#         # solve
+#         results = solver.solve(m)
+#
+#         # Check for optimal solution
+#         assert pyo.check_optimal_termination(results)
+#
+#         cost = m.fs.unit.costing
+#         # Check for known cost solution of default twin alternating contactors
+#         assert pyo.value(m.fs.costing.gac_pressure.num_contactors_op) == 1
+#         assert pyo.value(m.fs.costing.gac_pressure.num_contactors_redundant) == 1
+#         assert pytest.approx(56900, rel=1e-3) == pyo.value(cost.contactor_cost)
+#         assert pytest.approx(4.359, rel=1e-3) == pyo.value(cost.adsorbent_unit_cost)
+#         assert pytest.approx(17450, rel=1e-3) == pyo.value(cost.adsorbent_cost)
+#         assert pytest.approx(81690, rel=1e-3) == pyo.value(cost.other_process_cost)
+#         assert pytest.approx(2.0 * 156000, rel=1e-3) == pyo.value(cost.capital_cost)
+#         assert pytest.approx(12680, rel=1e-3) == pyo.value(cost.gac_makeup_cost)
+#         assert pytest.approx(27660, rel=1e-3) == pyo.value(cost.gac_regen_cost)
+#         assert pytest.approx(0.01631, rel=1e-3) == pyo.value(cost.energy_consumption)
+#         assert pytest.approx(40370, rel=1e-3) == pyo.value(cost.fixed_operating_cost)
+#
+#     @pytest.mark.component
+#     def test_robust_costing_gravity(self, build):
+#         mr_grav = build
+#
+#         mr_grav.fs.costing = WaterTAPCosting()
+#         mr_grav.fs.costing.base_currency = pyo.units.USD_2020
+#
+#         mr_grav.fs.unit.costing = UnitModelCostingBlock(
+#             flowsheet_costing_block=mr_grav.fs.costing,
+#             costing_method_arguments={"contactor_type": "gravity"},
+#         )
+#         mr_grav.fs.costing.cost_process()
+#
+#         # testing gac costing block dof and initialization
+#         assert assert_units_consistent(mr_grav) is None
+#         assert istat.degrees_of_freedom(mr_grav) == 0
+#         mr_grav.fs.unit.costing.initialize()
+#
+#         # solve
+#         results = solver.solve(mr_grav)
+#
+#         # Check for optimal solution
+#         assert pyo.check_optimal_termination(results)
+#
+#         cost = mr_grav.fs.unit.costing
+#         # Check for known cost solution of default twin alternating contactors
+#         assert pyo.value(mr_grav.fs.costing.gac_gravity.num_contactors_op) == 1
+#         assert pyo.value(mr_grav.fs.costing.gac_gravity.num_contactors_redundant) == 1
+#         assert pytest.approx(163200, rel=1e-3) == pyo.value(cost.contactor_cost)
+#         assert pytest.approx(4.359, rel=1e-3) == pyo.value(cost.adsorbent_unit_cost)
+#         assert pytest.approx(17450, rel=1e-3) == pyo.value(cost.adsorbent_cost)
+#         assert pytest.approx(159500, rel=1e-3) == pyo.value(cost.other_process_cost)
+#         assert pytest.approx(2.0 * 340200, rel=1e-3) == pyo.value(cost.capital_cost)
+#         assert pytest.approx(12680, rel=1e-3) == pyo.value(cost.gac_makeup_cost)
+#         assert pytest.approx(27660, rel=1e-3) == pyo.value(cost.gac_regen_cost)
+#         assert pytest.approx(2.476, rel=1e-3) == pyo.value(cost.energy_consumption)
+#         assert pytest.approx(40370, rel=1e-3) == pyo.value(cost.fixed_operating_cost)
+#
+#     @pytest.mark.component
+#     def test_robust_costing_modular_contactors(self, build):
+#         m = build
+#
+#         m.fs.costing = WaterTAPCosting()
+#         m.fs.costing.base_currency = pyo.units.USD_2020
+#
+#         m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+#         m.fs.costing.cost_process()
+#
+#         m.fs.costing.gac_pressure.num_contactors_op.fix(4)
+#         m.fs.costing.gac_pressure.num_contactors_redundant.fix(2)
+#
+#         results = solver.solve(m)
+#
+#         cost = m.fs.unit.costing
+#         # Check for known cost solution when changing volume scale of vessels in parallel
+#         assert pyo.value(m.fs.costing.gac_pressure.num_contactors_op) == 4
+#         assert pyo.value(m.fs.costing.gac_pressure.num_contactors_redundant) == 2
+#         assert pytest.approx(89040, rel=1e-3) == pyo.value(cost.contactor_cost)
+#         assert pytest.approx(69690, rel=1e-3) == pyo.value(cost.other_process_cost)
+#         assert pytest.approx(2.0 * 176200, rel=1e-3) == pyo.value(cost.capital_cost)
+#
+#     @pytest.mark.component
+#     def test_robust_costing_max_gac_ref(self, build):
+#         m = build
+#
+#         # scale flow up 10x
+#         unit_feed = m.fs.unit.process_flow.properties_in[0]
+#         unit_feed.flow_mol_phase_comp["Liq", "H2O"].fix(10 * 824.0736620370348)
+#         unit_feed.flow_mol_phase_comp["Liq", "TCE"].fix(10 * 5.644342973110135e-05)
+#
+#         m.fs.costing = WaterTAPCosting()
+#         m.fs.costing.base_currency = pyo.units.USD_2020
+#
+#         m.fs.unit.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+#         m.fs.costing.cost_process()
+#         # not necessarily an optimum solution because poor scaling
+#         # but just checking the conditional
+#         results = solver.solve(m)
+#
+#         # Check for bed_mass_gac_cost_ref to be overwritten
+#         # if bed_mass_gac is greater than bed_mass_gac_cost_max_ref
+#         assert pyo.value(m.fs.unit.bed_mass_gac) > pyo.value(
+#             m.fs.costing.gac_pressure.bed_mass_max_ref
+#         )
+#         assert pyo.value(m.fs.unit.costing.bed_mass_gac_ref) == (
+#             pytest.approx(pyo.value(m.fs.costing.gac_pressure.bed_mass_max_ref), 1e-5)
+#         )
---
+++ watertap/costing/unit_models/tests/unit_costing_test_harness.py
@@ -0,0 +1,188 @@
+#################################################################################
+# WaterTAP Copyright (c) 2020-2023, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
+# National Renewable Energy Laboratory, and National Energy Technology
+# Laboratory (subject to receipt of any required approvals from the U.S. Dept.
+# of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#################################################################################
+
+import pytest
+import abc
+
+from pyomo.environ import Block, assert_optimal_termination, ComponentMap, value
+from pyomo.util.check_units import assert_units_consistent
+from idaes.core.util.model_statistics import (
+    degrees_of_freedom,
+)
+from idaes.core.solvers import get_solver
+from idaes.core.util.testing import initialization_tester
+import idaes.core.util.scaling as iscale
+
+
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+class UnitAttributeError(AttributeError):
+    """
+    WaterTAP exception for generic attribute errors arising from unit model testing.
+    """
+
+
+class UnitValueError(ValueError):
+    """
+    WaterTAP exception for generic value errors arising from unit model testing.
+    """
+
+
+class UnitRuntimeError(RuntimeError):
+    """
+    WaterTAP exception for generic runtime errors arising from unit model testing.
+    """
+
+
+class UnitCostingTestHarness(abc.ABC):
+    def configure_class(self):
+        # string for solver, if None use WaterTAP default
+        self.solver = None
+        # dictionary for solver options, if None use WaterTAP default
+        self.optarg = None
+
+        # solution map from var to value
+        self.unit_solutions = ComponentMap()
+
+        # arguments for badly scaled variables
+        self.default_large = 1e4
+        self.default_small = 1e-3
+        self.default_zero = 1e-10
+
+        # arguments for solver tolerance
+        self.default_absolute_tolerance = 1e-12
+        self.default_relative_tolerance = 1e-6
+
+        model = self.configure()
+        if not hasattr(self, "unit_model_block"):
+            self.unit_model_block = model.find_component("fs.unit")
+            if self.unit_model_block is None:
+                raise RuntimeError(
+                    f"The {self.__class__.__name__}.configure method should either "
+                    "set the attribute `unit_model_block` or name it `fs.unit`."
+                )
+        # keep the model so it does not get garbage collected
+        self._model = model
+        blk = self.unit_model_block
+
+        # attaching objects to model to carry through in pytest frame
+        # TODO: Consider removing these objects and directly calling self
+        assert not hasattr(blk, "_test_objs")
+        blk._test_objs = Block()
+        blk._test_objs.solver = self.solver
+        blk._test_objs.optarg = self.optarg
+        blk._test_objs.unit_solutions = self.unit_solutions
+
+    @abc.abstractmethod
+    def configure(self):
+        """
+        Placeholder method to allow user to setup test harness.
+
+        The configure method must set the attributes:
+        unit_solutions: ComponentMap of values for the specified variables
+
+        The unit model tested should be named `fs.unit`, or this method
+        should set the attribute `unit_model_block`.
+
+        Returns:
+            model: the top-level Pyomo model
+        """
+
+    @pytest.fixture(scope="class")
+    def frame(self):
+        self.configure_class()
+        return self._model, self.unit_model_block
+
+    @pytest.mark.unit
+    def test_units_consistent(self, frame):
+        m, unit_model = frame
+        assert_units_consistent(unit_model)
+
+    @pytest.mark.unit
+    def test_dof(self, frame):
+        m, unit_model = frame
+        if degrees_of_freedom(unit_model) != 0:
+            raise UnitAttributeError(
+                "The unit has {dof} degrees of freedom when 0 is required."
+                "".format(dof=degrees_of_freedom(unit_model))
+            )
+
+    @pytest.mark.component
+    def test_initialization(self, frame):
+        m, blk = frame
+        initialization_tester(
+            m,
+            unit=blk,
+            solver=blk._test_objs.solver,
+            optarg=blk._test_objs.optarg,
+        )
+
+    @pytest.mark.component
+    def test_unit_solutions(self, frame):
+        self.configure_class()
+        m, blk = frame
+        solutions = blk._test_objs.unit_solutions
+
+        # solve unit
+        if blk._test_objs.solver is None:
+            opt = get_solver()
+        else:
+            opt = get_solver(
+                solver=blk._test_objs.solver, options=blk._test_objs.optarg
+            )
+        results = opt.solve(blk)
+
+        # check solve
+        badly_scaled_vars = list(
+            iscale.badly_scaled_var_generator(
+                blk,
+                large=self.default_large,
+                small=self.default_small,
+                zero=self.default_zero,
+            )
+        )
+        if badly_scaled_vars:
+            lines = [
+                f"{x[0].name}\t{x[0].value}\tsf: {iscale.get_scaling_factor(x[0])}"
+                for x in badly_scaled_vars
+            ]
+            msg = "One or more badly scaled variables found:\n"
+            msg += "\n".join(lines)
+            raise AssertionError(msg)
+
+        assert_optimal_termination(results)
+
+        # check results
+
+        for var, val in solutions.items():
+            comp_obj = None
+            try:
+                val = float(val)
+            except:
+                # expect the same API as pytest.approx
+                comp_obj = val
+                val = comp_obj.expected
+            if comp_obj is None:
+                comp_obj = pytest.approx(
+                    val,
+                    abs=self.default_absolute_tolerance,
+                    rel=self.default_relative_tolerance,
+                )
+            if not comp_obj == value(var):
+                raise AssertionError(f"{var}: Expected {val}, got {value(var)} instead")
+
+    @pytest.mark.component
+    def test_reporting(self, frame):
+        m, blk = frame
+        blk.report()
