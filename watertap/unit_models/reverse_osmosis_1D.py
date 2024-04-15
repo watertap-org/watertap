6711bf1351c867592b0911c774a6574c4634ae3c
Zach Binger <zbinger@nrel.gov>
2024-04-15 16:20:02 -0600

    debug

--- watertap/unit_models/reverse_osmosis_1D.py
+++ watertap/unit_models/reverse_osmosis_1D.py
@@ -188,7 +188,7 @@ class ReverseOsmosis1DData(ReverseOsmosisBaseData):
         )
         def eq_mass_flux_equal_mass_transfer(b, t, x, p, j):
             return (
-                b.flux_mass_phase_comp[t, x, p, j] * b.width
+                b.flux_mass_phase_comp[t, x, p, j] * (b.area / b.length)
                 == -b.feed_side.mass_transfer_term[t, x, p, j]
             )
 
