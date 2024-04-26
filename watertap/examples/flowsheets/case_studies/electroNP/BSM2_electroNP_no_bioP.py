c5e1b6f32d3027b92df20a4ba477defc5b98d588
luohezhiming <98901358+luohezhiming@users.noreply.github.com>
2024-04-26 10:44:33 -0400

    Update watertap/examples/flowsheets/case_studies/electroNP/BSM2_electroNP_no_bioP.py
    Co-authored-by: MarcusHolly <96305519+MarcusHolly@users.noreply.github.com>

--- watertap/examples/flowsheets/case_studies/electroNP/BSM2_electroNP_no_bioP.py
+++ watertap/examples/flowsheets/case_studies/electroNP/BSM2_electroNP_no_bioP.py
@@ -35,7 +35,7 @@ from idaes.models.unit_models import (
     PressureChanger,
 )
 from idaes.models.unit_models.separator import SplittingType
-from idaes.core.solvers import get_solver
+from watertap.core.solvers import get_solver
 from idaes.core.util.model_statistics import degrees_of_freedom
 import idaes.logger as idaeslog
 import idaes.core.util.scaling as iscale
