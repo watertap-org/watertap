093de689032c4348c48d42a4ee90eeabded08067
Kinshuk <kinshuk.panda@nrel.gov>
2024-05-28 15:32:22 -0600

    added my changes to BSM2_P_extension.py

--- watertap/examples/flowsheets/case_studies/full_water_resource_recovery_facility/BSM2_P_extension.py
+++ watertap/examples/flowsheets/case_studies/full_water_resource_recovery_facility/BSM2_P_extension.py
@@ -76,7 +76,11 @@ from watertap.unit_models.thickener import (
     ActivatedSludgeModelType as thickener_type,
 )
 
-from watertap.core.util.initialization import check_solve, assert_degrees_of_freedom
+from watertap.core.util.initialization import (
+    check_solve,
+    assert_degrees_of_freedom,
+    interval_improve_initial,
+)
 from watertap.costing import WaterTAPCosting
 from watertap.costing.unit_models.clarifier import (
     cost_circular_clarifier,
@@ -673,6 +677,10 @@ def initialize_system(m, bio_P=False):
 def solve(m, solver=None):
     if solver is None:
         solver = get_solver()
+<<<<<<< HEAD
+=======
+    # interval_improve_initial(m)
+>>>>>>> c8cdd915 (resolve merge conflicts.)
     results = solver.solve(m, tee=True)
     check_solve(results, checkpoint="closing recycle", logger=_log, fail_flag=True)
     pyo.assert_optimal_termination(results)
