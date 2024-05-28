da5c9f63e37ce293182c258cefff560c19dae896
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-28 16:28:06 -0600

    add scaling to demin

--- watertap/unit_models/ion_exchange/ion_exchange_demin.py
+++ watertap/unit_models/ion_exchange/ion_exchange_demin.py
@@ -14,26 +14,16 @@ from copy import deepcopy
 
 # Import Pyomo libraries
 from pyomo.environ import (
-    Set,
     Var,
-    check_optimal_termination,
     Param,
-    Suffix,
-    log,
     value,
     units as pyunits,
 )
 
 # Import IDAES cores
 from idaes.core import declare_process_block_class
-from watertap.core.solvers import get_solver
-from idaes.core.util.tables import create_stream_table_dataframe
-from idaes.core.util.constants import Constants
-from idaes.core.util.exceptions import InitializationError, ConfigurationError
 
 import idaes.core.util.scaling as iscale
-import idaes.logger as idaeslog
-
 
 from watertap.unit_models.ion_exchange.ion_exchange_base import (
     IonExchangeBaseData,
@@ -48,14 +38,25 @@ class IonExchangeDeminData(IonExchangeBaseData):
     """
     Ion exchange demineralization
     """
+    # For comparison to EPA-WBS
+    # self.bv = BV_regen
+    # self.breakthrough_time = regen_days
+    # self.bed_volume_total = resin_volume_total
 
     def build(self):
         super().build()
 
         prop_in = self.process_flow.properties_in[0]
+        regen = self.regeneration_stream[0]
         solutes = [
             x for x in self.config.property_package.config.solute_list if x != "H2O"
         ]
+        inerts = self.config.property_package.neutral_set
+        for j in inerts:
+            self.process_flow.mass_transfer_term[:, "Liq", j].fix(0)
+            regen.get_material_flow_terms("Liq", j).fix(0)
+
+        # Set EPA-WBS bounds for EBCT
         self.ebct.setlb(60)
         self.ebct.setub(480)
 
@@ -70,17 +71,7 @@ class IonExchangeDeminData(IonExchangeBaseData):
         self.flow_equiv_total = self.flow_equiv_cation + self.flow_equiv_anion
 
         self.ion_exchange_type = IonExchangeType.demineralize
-        # if self.flow_equiv_cation == self.flow_equiv_total:
-        #     self.charge_ratio_cx = 1
-        #     self.charge_ratio_ax = 0
-        #     # self.ion_exchange_type = IonExchangeType.cation
 
-        # elif self.flow_equiv_anion == self.flow_equiv_total:
-        #     self.charge_ratio_cx = 0
-        #     self.charge_ratio_ax = 1
-        #     # self.ion_exchange_type = IonExchangeType.anion
-
-        # else:
         assert (
             self.flow_equiv_cation + self.flow_equiv_anion
         ) / self.flow_equiv_total == 1
@@ -101,24 +92,27 @@ class IonExchangeDeminData(IonExchangeBaseData):
             doc="Removal efficiency",
         )
 
-        # typical operating capacity for resin is between 30-60 kg/m3; Wachinski, chap. 3
+        # typical total capacity for synthetic resin is between 0.14-2.3 eq/L
+        # typical operating/usable capacity for synthetic resin is between 0.69-1.15 eq/L
+        # Wachinski, chap. 3
+
         self.resin_capacity_ax = Var(
             initialize=0.75,
-            bounds=(0.687, 2.1),
+            bounds=(0.14, 2.3),
             units=pyunits.mol / pyunits.liter,
             doc="Operating capacity of the anion exchange resin in equivalents per liter resin",
         )
 
         self.resin_capacity_cx = Var(
             initialize=0.75,
-            bounds=(0.687, 2.1),
+            bounds=(0.14, 2.3),
             units=pyunits.mol / pyunits.liter,
             doc="Operating capacity of the cation exchange resin in equivalents per liter resin",
         )
 
         self.resin_capacity_op = Var(
             initialize=0.75,
-            bounds=(0.687, 2.1),
+            bounds=(0.14, 2.3),
             units=pyunits.mol / pyunits.liter,
             doc="Operating capacity of the mixed bed resin",
         )
@@ -155,19 +149,34 @@ class IonExchangeDeminData(IonExchangeBaseData):
                 (b.bed_depth * b.bv) / b.loading_rate, to_units=pyunits.s
             )
 
-        # @self.Constraint()
-        # def eq_bed_vol_tot(b):
-        #     return b.bed_volume_total == b.bed_volume * b.number_columns
-
-        @self.Constraint(solutes)
+        @self.Constraint(solutes, doc="Mass transfer term for control volume")
         def eq_mass_transfer_term(b, j):
             return (
                 b.process_flow.mass_transfer_term[0, "Liq", j]
                 == -1 * prop_in.flow_mass_phase_comp["Liq", j] * b.removal_efficiency[j]
             )
 
-        @self.Constraint()
-        def eq_bed_vol(b):
+        @self.Constraint(solutes, doc="Regeneration stream mass flow")
+        def eq_mass_transfer_regen(b, j):
+            return (
+                regen.get_material_flow_terms("Liq", j)
+                == -b.process_flow.mass_transfer_term[0, "Liq", j]
+            )
+
+        @self.Constraint(doc="Total bed volume required based on volumetric flow")
+        def eq_bed_volume_required(b):
             return b.bed_volume_total == pyunits.convert(
                 prop_in.flow_vol_phase["Liq"] * b.ebct, to_units=pyunits.m**3
             )
+
+    def calculate_scaling_factors(self):
+        super().calculate_scaling_factors()
+
+        if iscale.get_scaling_factor(self.resin_capacity_ax) is None:
+            iscale.set_scaling_factor(self.resin_capacity_ax, 1)
+
+        if iscale.get_scaling_factor(self.resin_capacity_cx) is None:
+            iscale.set_scaling_factor(self.resin_capacity_cx, 1)
+
+        if iscale.get_scaling_factor(self.resin_capacity_op) is None:
+            iscale.set_scaling_factor(self.resin_capacity_op, 1)
