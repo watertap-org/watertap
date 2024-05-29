ec28926919aae4df12ba4ae5ced3358b28129068
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-29 17:20:03 -0600

    fix class name in init

--- watertap/unit_models/ion_exchange/__init__.py
+++ watertap/unit_models/ion_exchange/__init__.py
@@ -2,5 +2,5 @@ from .ion_exchange_base import IonExchangeBaseData
 from .ion_exchange_demin import IonExchangeDemin
 from .ion_exchange_clark import IonExchangeClark
 from .ion_exchange_constant_pattern import IonExchangeCP
-from .ion_exchange_multi_component import IonExchageMultiComponent
+from .ion_exchange_multi_component import IonExchageMultiComp
 from .ion_exchange_parmest import IXParmest
