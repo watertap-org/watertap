2d79077f6926962d9340d4fc5e71edf534d808b8
Adam Atia <aatia@keylogic.com>
2024-05-29 12:02:23 -0400

    fix string expected

--- watertap/tools/oli_api/conftest.py
+++ watertap/tools/oli_api/conftest.py
@@ -123,7 +123,7 @@ def oliapi_instance_with_invalid_phase(
         oliapi.upload_dbs_file(str(local_dbs_file))
         with pytest.raises(
             RuntimeError,
-            match="Failed DBS file generation. Unexpected phase(s): invalid_phase",
+            match="Failed DBS file generation. Unexpected phase(s): ['invalid_phase']",
         ):
             oliapi.generate_dbs_file(source_water, phases=["invalid_phase"])
         yield oliapi
