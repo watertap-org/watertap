ae103167511f673a7449d021e1cb4a67061e2f63
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-30 10:10:30 -0600

    better demo for IX multi component

--- watertap/unit_models/ion_exchange/tests/test_ion_exchange_multi_component.py
+++ watertap/unit_models/ion_exchange/tests/test_ion_exchange_multi_component.py
@@ -43,69 +43,7 @@ from idaes.core.util.constants import Constants
 from watertap.core.solvers import get_solver
 
 solver = get_solver()
-target_component = "comp1"
 
-solute_list = ["comp1", "comp2", "inert"]
-reactive_ions = ["comp1", "comp2"]
-nontarget = [s for s in solute_list if s != target_component]
-
-c0_dict = {"comp1": 1e-6, "comp2": 8e-6, "inert": 1e-3}
-diff_data_dict = {
-    ("Liq", "comp1"): 10e-10,
-    ("Liq", "comp2"): 5e-9,
-    # ("Liq", "inert"): 3e-9,
-}
-charge_dict = {"comp1": -1, "comp2": -1}
-solute_list = ["comp1", "comp2"]
-mass_transfer_coeff_dict = {"comp1": 0.75, "comp2": 0.6}
-freundlich_n_dict = {"comp1": 1.1, "comp2": 1.2}
-bv_50_dict = {"comp1": 75000, "comp2": 90000}
-mw_dict = {"comp1": 0.21404, "comp2": 0.164031, "inert": 0.04}
-fix_vars = {
-    "ebct": 210,
-    "resin_density": 0.72,
-    "resin_diam": 700e-6,
-    "bed_porosity": 0.44,
-    "loading_rate": 0.006,
-    "number_columns": 6,
-    "c_norm": 0.25,
-}
-calc_from_constr = dict()
-set_vars = {"c_trap_min": 1e-5}
-
-flow_in = 0.04381
-number_traps = 5
-
-def _calc_from_constr(ix, calc_from_constr):
-    if isinstance(calc_from_constr, dict):
-        for k, v in calc_from_constr.items():
-            print(k, v)
-            ixv = getattr(ix, k)
-            ixc = getattr(ix, v)
-            if all(c.is_indexed() for c in [ixv, ixc]):
-                idx = [*ixv.index_set()]
-                idx_c = [*ixc.index_set()]
-                if not len(idx) == len(idx_c):
-                    idx = [
-                        i for i in idx_c if i in idx
-                    ]  # only calculate variables we have a constraint for
-                for i in idx:
-                    calculate_variable_from_constraint(ixv[i], ixc[i])
-                    set_scaling_factor(ixv[i], 1 / value(ixv[i]))
-            elif ixv.is_indexed():
-                idx = [*ixv.index_set()]
-                for i in idx:
-                    calculate_variable_from_constraint(ixv[i], ixc)
-                    set_scaling_factor(ixv[i], 1 / value(ixv[i]))
-            elif ixc.is_indexed():
-                idx = [*ixc.index_set()]
-                for i in idx:
-                    calculate_variable_from_constraint(ixv, ixc[i])
-                    set_scaling_factor(ixv, 1 / value(ixv))
-
-            else:
-                calculate_variable_from_constraint(ixv, ixc)
-                set_scaling_factor(ixv, 1 / value(ixv))
 
 def build_ix(
     flow_in=0.04381,
@@ -128,6 +66,10 @@ def build_ix(
 
     global c0_mol_flow_sf_dict
     assert target_component in solute_list
+
+    mw_dict = {"comp1": 0.21404, "comp2": 0.164031, "inert": 0.04}
+
+    # assert all(d.keys() == solute_list for d in [c0_dict, diff_data_dict, charge_dict])
 
     flow_in = flow_in * pyunits.m**3 / pyunits.s
     rho = 1000 * pyunits.kg / pyunits.m**3
@@ -194,12 +136,8 @@ def build_ix(
         else:
             ixv.fix(v)
 
-    if mass_transfer_coeff_dict == {}:
-        for k, v in kinetic_param_dict.items():
-            ix.kinetic_param[k].fix(v)
-    else:
-        for k, v in mass_transfer_coeff_dict.items():
-            ix.mass_transfer_coeff[k].fix(v)
+    for k, v in mass_transfer_coeff_dict.items():
+        ix.mass_transfer_coeff[k].fix(v)
 
     for k, v in freundlich_n_dict.items():
         ix.freundlich_n[k].fix(v)
@@ -216,7 +154,9 @@ def build_ix(
         ixv = getattr(ix, k)
         ixv.set_value(v)
 
-    ix.bv.set_value(value(ix.bv_50[target_component]) / value(ix.c_norm[target_component]))
+    ix.bv.set_value(
+        value(ix.bv_50[target_component]) / value(ix.c_norm[target_component])
+    )
     if dont_calc_effluent:
         ix.eq_tb_traps.deactivate()
         ix.eq_c_traps.deactivate()
@@ -230,14 +170,15 @@ def build_ix(
             v.fix()
         ix.c_norm_avg.fix()
         if ix.config.regenerant != "single_use":
-            ix.eq_mass_transfer_target_fr.deactivate()
+            # ix.eq_mass_transfer_target_fr.deactivate()
             ix.eq_mass_transfer_regen.deactivate()
         pf.mass_transfer_term[0, "Liq", target_component].fix()
-        ix.eq_mass_transfer_target_fr.deactivate()
+        ix.eq_mass_transfer_reactive_ions.deactivate()
 
     print(f"DOF: {degrees_of_freedom(m)}")
 
     return m
+
 
 def plot_traps(m, x_plot=None, run_curve=None, cmap="viridis"):
     global tb_traps_dict, bv_traps_dict, c_norm_dict
@@ -274,7 +215,9 @@ def plot_traps(m, x_plot=None, run_curve=None, cmap="viridis"):
         )
     else:
         ax.scatter(
-            ((ix.bv_50[target_component]() * ix.bed_depth()) / ix.loading_rate()) / 3600 / 24,
+            ((ix.bv_50[target_component]() * ix.bed_depth()) / ix.loading_rate())
+            / 3600
+            / 24,
             0.5,
             marker="X",
             color="red",
@@ -349,70 +292,66 @@ def plot_traps(m, x_plot=None, run_curve=None, cmap="viridis"):
         ax.set_ylim([0, 1.02])
         # plt.tight_layout()
     #     return results_dict
-def _get_comp_list(blk, comp=Var, skip_list=[]):
-    cs = []
-    split_name = blk.name + "."
-    skip_list += ["ref", "process_flow", "regeneration"]
-    for c in blk.component_objects(comp):
-        if any(s in c.name for s in skip_list):
-            continue
-        cs.append(c.name.split(split_name)[1])
-    return cs
 
-def scale_it(m, autoscale_fixed=True, scale_from_value=list(), custom_scaling=dict()):
+
+def scale_model(
+    m, autoscale_fixed=True, scale_from_value=list(), custom_scaling=dict()
+):
     """
     Scale the model
 
     """
+
+    def _get_comp_list(blk, comp=Var, skip_list=[]):
+        cs = []
+        split_name = blk.name + "."
+        skip_list += ["ref", "process_flow", "regeneration"]
+        for c in blk.component_objects(comp):
+            if any(s in c.name for s in skip_list):
+                continue
+            cs.append(c.name.split(split_name)[1])
+        return cs
+
     ix = m.fs.ix
-    target_component = ix.config.target_component
-    prop_out = ix.process_flow.properties_out[0]
-    # prop_regen = ix.regeneration_stream[0]
-    # set_scaling_factor(
-    #     prop_out.flow_mol_phase_comp["Liq", target_component], c0_mol_flow_sf
-    # )
-    # set_scaling_factor(
-    #     prop_out.flow_mol_phase_comp["Liq", "H2O"], water_mol_flow_sf
-    # )
-    # set_scaling_factor(prop_regen.flow_mol_phase_comp["Liq", target_component], 1e15)
 
     calculate_scaling_factors(m)
 
     if autoscale_fixed:
         print("autoscale_fixed")
-        for v in _get_comp_list(ix, skip_list=["traps"]):
+        for v in _get_comp_list(ix):
             # print(v)
             ixv = getattr(ix, v)
             if ixv.is_indexed():
-                idx = [*ixv.index_set()]
-                for i in idx:
-                    if ixv[i].is_fixed():
-                        if ixv[i]() == 0:
+                for i, vv in ixv.items():
+                    if vv.is_fixed():
+                        if value(vv) == 0:
                             continue
-                        # print(i, ixv.name, ixv[i](), 1 / ixv[i]())
-                        set_scaling_factor(ixv[i], 1 / ixv[i]())
+                        sf = 1 / value(vv)
+                        set_scaling_factor(vv, sf)
             else:
                 if ixv.is_fixed():
-                    if ixv() == 0:
+                    if value(ixv) == 0:
                         continue
-                    set_scaling_factor(ixv, 1 / ixv())
-                    # print(v, "fixed")
+                    sf = 1 / value(ixv)
+                    set_scaling_factor(ixv, sf)
 
     if isinstance(scale_from_value, list):
-        print("scale_from_value")
         for v in scale_from_value:
-            # print(v)
             ixv = getattr(ix, v)
             if ixv.is_indexed():
-                idx = [*ixv.index_set()]
-                for i in idx:
-                    if ixv[i]() == 0:
-                        continue
-                    set_scaling_factor(ixv[i], 1 / ixv[i]())
+                for i, vv in ixv.items():
+                    if vv.is_fixed():
+                        if value(vv) == 0:
+                            continue
+                        sf = 1 / value(vv)
+                        if get_scaling_factor(vv) is None:
+                            set_scaling_factor(vv, sf)
             else:
-                if ixv() == 0:
+                if value(ixv) == 0:
                     continue
-                set_scaling_factor(ixv, 1 / ixv())
+                sf = 1 / value(ixv)
+                if get_scaling_factor(ixv) is None:
+                    set_scaling_factor(ixv, sf)
 
     for k, v in custom_scaling.items():
         print(f"{k, v} custom_scaling")
@@ -422,38 +361,76 @@ def scale_it(m, autoscale_fixed=True, scale_from_value=list(), custom_scaling=di
     for i, c in ix.eq_clark.items():
 
         constraint_scaling_transform(c, 1e-2)
-m = build_ix(
-    flow_in=flow_in,
-    target_component=target_component,
-    solute_list=solute_list,
-    reactive_ions=reactive_ions,
-    fix_vars=fix_vars,
-    c0_dict=c0_dict,
-    mass_transfer_coeff_dict=mass_transfer_coeff_dict,
-    freundlich_n_dict=freundlich_n_dict,
-    bv_50_dict=bv_50_dict,
-    diff_data_dict=diff_data_dict,
-    number_traps=number_traps,
-    charge_dict=charge_dict,
-    # deactivate_constr=deactivate_constr,
-    dont_calc_effluent=False,
-)
-ix = m.fs.ix
-_calc_from_constr(ix, calc_from_constr)
-scale_it(m, scale_from_value=list())
-ix.c_trap_min['comp2'].set_value(1e-10)
-print(f"DOF: {degrees_of_freedom(m)}")
-set_scaling_factor(ix.c_traps, 1e3)
-set_scaling_factor(ix.traps, 1e6)
-set_scaling_factor(ix.c_norm_avg["comp2"], 1e6)
-try:
-    ix.initialize()
-except:
-    print_infeasible_constraints(m)
-    print_variables_close_to_bounds(m)
 
-solver = get_solver()
-results = solver.solve(m)
-print(f"\ntermination = {results.solver.termination_condition.swapcase()}\n")
 
-plot_traps(m, x_plot="bv", run_curve=True, cmap="turbo")
+## Dummy data for model
+def main():
+    solute_list = ["comp1", "comp2", "inert"]
+    reactive_ions = ["comp1", "comp2"]
+
+    c0_dict = {"comp1": 1e-6, "comp2": 8e-6, "inert": 1e-3}
+    diff_data_dict = {
+        ("Liq", "comp1"): 10e-10,
+        ("Liq", "comp2"): 5e-9,
+        ("Liq", "inert"): 3e-9,
+    }
+    charge_dict = {"comp1": -1, "comp2": -1}
+
+    mass_transfer_coeff_dict = {"comp1": 0.75, "comp2": 0.6}
+    freundlich_n_dict = {"comp1": 5.1, "comp2": 1.2}
+    bv_50_dict = {"comp1": 75000, "comp2": 90000}
+    fix_vars = {
+        "ebct": 210,
+        "resin_density": 0.72,
+        "resin_diam": 700e-6,
+        "bed_porosity": 0.44,
+        "loading_rate": 0.006,
+        "number_columns": 6,
+        "c_norm": 0.75,
+        "number_columns_redundant": 2,
+    }
+
+    target_component = "comp1"
+    flow_in = 0.04381
+    number_traps = 5
+
+    m = build_ix(
+        flow_in=flow_in,
+        target_component=target_component,
+        solute_list=solute_list,
+        reactive_ions=reactive_ions,
+        fix_vars=fix_vars,
+        c0_dict=c0_dict,
+        mass_transfer_coeff_dict=mass_transfer_coeff_dict,
+        freundlich_n_dict=freundlich_n_dict,
+        bv_50_dict=bv_50_dict,
+        diff_data_dict=diff_data_dict,
+        number_traps=number_traps,
+        charge_dict=charge_dict,
+        dont_calc_effluent=False,
+    )
+
+    ix = m.fs.ix
+
+    scale_model(m, scale_from_value=list())
+
+    ix.c_trap_min["comp2"].set_value(1e-10)
+    print(f"DOF: {degrees_of_freedom(m)}")
+    set_scaling_factor(ix.c_traps, 1e3)
+    set_scaling_factor(ix.traps, 1e6)
+    set_scaling_factor(ix.c_norm_avg["comp2"], 1e6)
+    try:
+        ix.initialize()
+    except:
+        print_infeasible_constraints(m)
+        print_variables_close_to_bounds(m)
+
+    solver = get_solver()
+    results = solver.solve(m)
+    print(f"\ntermination = {results.solver.termination_condition.swapcase()}\n")
+
+    plot_traps(m, x_plot="bv", run_curve=True, cmap="turbo")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
