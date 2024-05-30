06c9888e29c3f29a7a6c0750dea54856c1ffec7d
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-30 15:38:41 -0600

    abs value of charge in eq_mass_removed

--- watertap/unit_models/ion_exchange_0D.py
+++ watertap/unit_models/ion_exchange_0D.py
@@ -20,6 +20,7 @@ from pyomo.environ import (
     Param,
     Suffix,
     log,
+    value,
     units as pyunits,
 )
 from pyomo.common.config import ConfigBlock, ConfigValue, In
@@ -1053,7 +1054,7 @@ class IonExchangeODData(InitializationMixin, UnitModelBlockData):
                 self.target_ion_set, doc="Removed total mass of ion in equivalents"
             )
             def eq_mass_removed(b, j):
-                charge = prop_in.charge_comp[j]
+                charge = abs(value(prop_in.charge_comp[j]))
                 return b.mass_removed[j] * charge == pyunits.convert(
                     b.resin_eq_capacity * b.resin_bulk_dens * b.bed_vol_tot,
                     to_units=pyunits.mol,
