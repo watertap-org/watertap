a2ad37772493a4fe98d10b9ed4695289092165db
Ludovico Bianchi <lbianchi@lbl.gov>
2024-06-14 10:26:52 -0500

    Update parameter-sweep requirement to 0.1.dev2 from PyPI

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         "pyomo>=6.6.1",
         "pyyaml",  # watertap.core.wt_database
         # for parameter_sweep
-        "parameter-sweep[ray] @ git+https://github.com/watertap-org/parameter-sweep@main",
+        "parameter-sweep[ray]==0.1.dev2",
         # for watertap.ui.api_model (though may be generally useful)
         "pydantic >= 2, <3",
         "numpy",
