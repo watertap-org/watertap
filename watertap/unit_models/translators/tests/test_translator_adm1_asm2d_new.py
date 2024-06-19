49ce16ce6237e8fcbef712b753cf2699706e893b
MarcusHolly <marcus.holly@keylogic.com>
2024-06-19 16:22:38 -0400

    Correct imports in test file

--- watertap/unit_models/translators/tests/test_translator_adm1_asm2d_new.py
+++ watertap/unit_models/translators/tests/test_translator_adm1_asm2d_new.py
@@ -45,19 +45,19 @@ from idaes.core.util.testing import initialization_tester
 from watertap.unit_models.translators.translator_adm1_asm2d_new import (
     Translator_ADM1_ASM2D,
 )
-from watertap.property_models.anaerobic_digestion.modified_adm1_properties import (
+from watertap.property_models.unit_specific.anaerobic_digestion.modified_adm1_properties import (
     ModifiedADM1ParameterBlock,
 )
 
-from watertap.property_models.activated_sludge.modified_asm2d_properties import (
+from watertap.property_models.unit_specific.activated_sludge.modified_asm2d_properties import (
     ModifiedASM2dParameterBlock,
 )
 
-from watertap.property_models.anaerobic_digestion.modified_adm1_reactions import (
+from watertap.property_models.unit_specific.anaerobic_digestion.modified_adm1_reactions import (
     ModifiedADM1ReactionParameterBlock,
 )
 
-from watertap.property_models.activated_sludge.modified_asm2d_reactions import (
+from watertap.property_models.unit_specific.activated_sludge.modified_asm2d_reactions import (
     ModifiedASM2dReactionParameterBlock,
 )
 
