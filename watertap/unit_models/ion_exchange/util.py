5cc2a218b54a6de3251fa4433f8fd69d4daa5024
kurbansitterley <kurban.sitterley@nrel.gov>
2024-07-15 10:40:24 -0600

    add saving funcs, results_dict funts

--- watertap/unit_models/ion_exchange/util.py
+++ watertap/unit_models/ion_exchange/util.py
@@ -21,10 +21,7 @@ from scipy.optimize import curve_fit
 
 from .ion_exchange_clark import IonExchangeClark
 
-theta_title_dict = {
-    "thomas_constant": "k$_{Th}$",
-    "resin_max_capacity": "q$_{max}$"
-}
+theta_title_dict = {"thomas_constant": "k$_{Th}$", "resin_max_capacity": "q$_{max}$"}
 
 
 def plot_theta(blk):
@@ -33,7 +30,7 @@ def plot_theta(blk):
 
     if not all(hasattr(blk, attr) for attr in ["test_cnorms", "test_bvs", "R_sq"]):
         blk.test_theta()
-    
+
     stat_str = f"Theta (R$^2$: {blk.R_sq:.3f})"
     theta_str = [f"{theta_title_dict[k]}: {v:.2e}" for k, v in blk.theta_dict.items()]
     textstr = "\n".join([stat_str] + theta_str)
@@ -215,6 +212,7 @@ def plot_theta(blk):
 
 def build_results_dict(
     blk,
+    ix_blk=None,
     split_str="fs.ix.",
     components=[Var, Expression],
     skips=[
@@ -226,7 +224,6 @@ def build_results_dict(
         "H2O",
     ],
     keeps=["flow_vol_phase", "conc_mass_phase_comp"],
-    check_keys=False,
 ):
     blk.results_dict = {}
     blk.v_name_map = {}
@@ -235,15 +232,15 @@ def build_results_dict(
         "ref",
         "compound",
         "resin",
-        "resin_type",
-        "resin_func_group",
+        # "resin_type",
+        # "resin_func_group",
         "target_component",
         "conc_units",
         "ebct_min",
         "flow_in",
         "charge",
         "c0",
-        # "expr_sf",
+        "expr_sf",
         "c0_min_thresh",
         "c0_max_thresh",
         "c_next_thresh",
@@ -252,28 +249,29 @@ def build_results_dict(
         "tc",
     ]
 
-    if blk is None:
-        blk = blk._get_ix_blk()
-    elif isinstance(blk, ConcreteModel):
-        blk = blk._get_ix_blk(m=blk)
+    if ix_blk is None:
+        ix_blk = blk._get_ix_blk()
+    # elif isinstance(blk, ConcreteModel):
+    #     ix_blk = blk._get_ix_blk(m=blk)
 
-    assert isinstance(blk, blk.ix_model)
+    # assert isinstance(blk, IonExchange0D)
 
     for deet in blk.curve_deets:
         blk.results_dict[deet] = []
 
     for c in components:
-        for v in blk.component_objects(c):
+        for v in ix_blk.component_objects(c):
             # print(v.name)
             v_name = v.name.split(split_str)[1]
             if any(True if s in v.name else False for s in keeps):
                 # print(f"KEEP {v.name}")
                 if v.is_indexed():
-                    idx = [*v._index_set]
-                    for i in idx:
+                    # idx = [*v._index_set]
+                    for i, b in v.items():
+                        # for i in idx:
                         v_name_i = v_name + f"_{i}"
-                        blk.results_dict[v[i].name] = []
-                        blk.v_name_map[v[i].name] = v_name_i
+                        blk.results_dict[b.name] = []
+                        blk.v_name_map[b.name] = v_name_i
                 else:
                     blk.results_dict[v.name] = []
                     blk.v_name_map[v.name] = v_name
@@ -296,74 +294,69 @@ def build_results_dict(
     for k in blk.initial_guess_dict.keys():
         blk.results_dict[f"{k}_ig"] = []
 
-    for k in blk.initial_guess_theta_dict.keys():
-        blk.results_dict[f"{k}_ig_theta"] = []
+    for theta in blk.thetas:
+        blk.results_dict[f"{theta}_theta"] = []
 
-    for k in blk.calc_from_constr_dict.keys():
-        blk.results_dict[f"{k}_cfc"] = []
+    blk.results_dict["obj"] = []
+
+    # for k in blk.theta_initial_guess.keys():
+    #     blk.results_dict[f"{k}_ig_theta"] = []
+
+    # for k in blk.calc_from_constr_dict.keys():
+    #     blk.results_dict[f"{k}_cfc"] = []
 
     blk.results_dict["flag"] = []
 
-    if check_keys:
-        for k in blk.results_dict.keys():
-            print(k)
 
+def results_dict_append(blk, ix_blk=None, components=[Var, Expression]):
+    if ix_blk is None:
+        ix_blk = blk._get_ix_blk()
+    # elif isinstance(blk, ConcreteModel):
+    #     blk = blk._get_ix_blk(m=blk)
+    # else:
+    #     pass
 
-def results_dict_append(
-    blk=None,
-    components=[Var, Expression],
-    tmp_results_dict=None,
-):
-
-    if blk is None:
-        blk = blk.ix
-    elif isinstance(blk, ConcreteModel):
-        blk = blk._get_ix_blk(m=blk)
-    else:
-        pass
-
-    assert isinstance(blk, blk.ix_model)
+    # assert isinstance(blk, IonExchange0D)
 
     for deet in blk.curve_deets:
         # print(deet)
         blk.results_dict[deet].append(getattr(blk, deet))
 
-    if tmp_results_dict is None:
-        for c in components:
-            # if c is Var:
-            for v in blk.component_objects(c):
-                if v.is_indexed():
-                    idx = [*v._index_set]
-                    for i in idx:
-                        if v[i].name in blk.results_dict.keys():
-                            blk.results_dict[v[i].name].append(v[i]())
-                else:
-                    if v.name in blk.results_dict.keys():
-                        # print(v.name, v())
-                        blk.results_dict[v.name].append(v())
+    for c in components:
+        # if c is Var:
+        for v in ix_blk.component_objects(c):
+            if v.is_indexed():
+                # idx = [*v._index_set]
+                for _, b in v.items():
+                    # for i in idx:
+                    if b.name in blk.results_dict.keys():
+                        blk.results_dict[b.name].append(value(b))
+            else:
+                if v.name in blk.results_dict.keys():
+                    # print(v.name, v())
+                    blk.results_dict[v.name].append(value(v))
 
-        for k, v in blk.initial_guess_dict.items():
-            blk.results_dict[f"{k}_ig"].append(v)
+    for k, v in blk.initial_guess_dict.items():
+        blk.results_dict[f"{k}_ig"].append(v)
 
-        for k, v in blk.initial_guess_theta_dict.items():
-            blk.results_dict[f"{k}_ig_theta"].append(v)
+    # for k, v in blk.theta_initial_guess.items():
+    #     blk.results_dict[f"{k}_ig_theta"].append(v)
 
-        for k, v in blk.calc_from_constr_dict.items():
-            blk.results_dict[f"{k}_cfc"].append(v)
+    # for k, v in blk.calc_from_constr.items():
+    #     blk.results_dict[f"{k}_cfc"].append(v)
 
-        if hasattr(blk, "theta_dict"):
-            for k, v in blk.theta_dict.items():
-                blk.results_dict[f"{k}_theta"].append(v)
-            blk.results_dict["obj"].append(blk.obj)
+    if hasattr(blk, "theta_dict"):
+        for k, v in blk.theta_dict.items():
+            blk.results_dict[f"{k}_theta"].append(v)
+        blk.results_dict["obj"].append(blk.obj)
 
-        blk.results_dict["flag"].append(blk.flag)
-
-    else:
-        for k, v in tmp_results_dict.items():
-            blk.results_dict[k].append(v)
+    blk.results_dict["flag"].append(blk.flag)
 
 
-def save_results(blk, overwrite=False, results_filename=None):
+def save_results(blk, overwrite=True, results_filename=None):
+
+    if blk.save_directory is None:
+        raise ValueError("Must provide save directory to save results.")
 
     blk.results_dict_save = deepcopy(blk.results_dict)
 
@@ -371,31 +364,44 @@ def save_results(blk, overwrite=False, results_filename=None):
         blk.results_dict_save[new_key] = blk.results_dict_save.pop(old_key)
 
     if results_filename is None:
-        blk.results_filename = f"results/curve_id{blk.curve_id}_results.csv"
+        blk.results_filename = (
+            f"{blk.save_directory}/results/curve{blk.curve_id}_results.csv"
+        )
     else:
         blk.results_filename = results_filename
 
-    blk.df_results = pd.DataFrame.from_dict(blk.results_dict_save)
+    try:
+        blk.df_results = pd.DataFrame.from_dict(blk.results_dict_save)
+    except ValueError:  # there are some empty lists in results_dict
+        col_length = len(
+            blk.results_dict_save["curve_id"]
+        )  # curve_id will always be populated
+        for k, v in blk.results_dict_save.items():
+            if len(v) != col_length:
+                blk.results_dict_save[k] = list(None for _ in range(col_length))
+        blk.df_results = pd.DataFrame.from_dict(blk.results_dict_save)
 
     if overwrite:
         blk.df_results.to_csv(blk.results_filename, index=False)
     elif not overwrite:
-        x = blk.results_filename.split(".csv")[0]
+        file_base = blk.results_filename.split(".csv")[0]
         append = 2
         while os.path.exists(blk.results_filename):
-            blk.results_filename = x + f"_{append}.csv"
+            blk.results_filename = file_base + f"_{append}.csv"
             append += 1
         blk.df_results.to_csv(blk.results_filename, index=False)
 
 
-def save_figs(blk, overwrite=False, extension=None):
+def save_figs(blk, overwrite=True, extension=None):
 
-    # fig_file_base = f"figs/curve_id{blk.curve_id}_"
+    if blk.save_directory is None:
+        raise ValueError("Must provide save directory to save figures.")
 
     for figname, d in blk.all_figs.items():
-        # fig_file_base = f"figs/{figname}/curve_id{blk.curve_id}_"
         fig = d["fig"]
-        fig_file = f"figs/{figname}/curve_id{blk.curve_id}_{figname}.png"
+        fig_file = (
+            f"{blk.save_directory}/figs/{figname}/curve{blk.curve_id}_{figname}.png"
+        )
         if overwrite:
             fig.savefig(fig_file, bbox_inches="tight")
         elif not overwrite:
@@ -407,7 +413,10 @@ def save_figs(blk, overwrite=False, extension=None):
             fig.savefig(fig_file, bbox_inches="tight")
 
 
-def save_output(blk, overwrite=False):
+def save_output(blk, overwrite=True):
+
+    if blk.save_directory is None:
+        raise ValueError("Must provide save directory to save output.")
 
     datas = [
         "bv50_bvs",
@@ -439,8 +448,8 @@ def save_output(blk, overwrite=False):
         "bv_rel_error",
     ]
 
-    data_file_base = f"output/curve_id{blk.curve_id}_output.csv"
-    theta_file_base = f"theta/curve_id{blk.curve_id}_theta.csv"
+    data_file_base = f"{blk.save_directory}/output/curve{blk.curve_id}_output.csv"
+    theta_file_base = f"{blk.save_directory}/theta/curve{blk.curve_id}_theta.csv"
 
     if hasattr(blk, "theta_dict"):
         tmp_dict = dict()
@@ -466,11 +475,14 @@ def save_output(blk, overwrite=False):
 
     blk.df_data["c0_max_thresh"] = blk.c0_max_thresh
     blk.df_data["c0_min_thresh"] = blk.c0_min_thresh
+    blk.df_data["use_all_data"] = blk.use_all_data
+    blk.df_data["use_this_data"] = blk.use_this_data
 
     for deet in blk.curve_deets:
         if hasattr(blk, deet):
             blk.df_data[deet] = getattr(blk, deet)
 
+    if hasattr(blk, "theta_dict"):
         for k, v in blk.theta_dict.items():
             blk.df_data[f"{k}_theta"] = v
         blk.df_data["obj"] = blk.obj
@@ -543,8 +555,6 @@ def plot_initial_guess(blk):
         alpha=0.25,
         # label="Influent",
     )
-
-
 
     title = (
         f"Initial Guess - Curve {blk.curve_id}:\n"
@@ -749,8 +759,11 @@ def plot_estimate_bv50(blk):
 
 def plot_curve(blk):
 
+    modified_dropped = blk.dropped_points + blk.modified_points
+
     fig, ax = plt.subplots(figsize=blk.figsize)
-    ax.scatter(blk.excl_bvs, blk.excl_cnorms, marker="x", color="k", label="Excluded data")
+
+    # ax.scatter(blk.)
     ax.plot(
         blk.keep_bvs,
         blk.keep_cnorms,
@@ -765,15 +778,36 @@ def plot_curve(blk):
         color="purple",
         marker=".",
         alpha=0.25,
-        label="All Data",
+        label="Filtered Data",
     )
     ax.plot(
         [0, max(blk.all_bvs)],
         [1, 1],
         linestyle="-.",
         alpha=0.25,
-        label="Influent",
+        color="navy",
+        # label="Influent",
     )
+    ax.scatter(
+        blk.excl_bvs, blk.excl_cnorms, marker="x", color="k", label="Excluded Data"
+    )
+
+    # if len(modified_dropped) > 0:
+    #     md = blk.input_data.loc[modified_dropped]
+    #     ax.plot(md.bv, md.c_norm, marker="^", color="c", alpha=0.25, label="Modified/Dropped Data")
+    #     for p in modified_dropped:
+    #         if p in blk.dropped_points:
+    #             print(f"Input data with point id {p} was dropped.")
+    #         if p in blk.modified_points:
+    #             print(f"Input data with point id {p} was modified:")
+    #             s = blk.input_data.loc[p]
+    #             f = blk.filtered_data.loc[p]
+    #             e = s.eq(f)
+    #             for i, b in zip(s.index, e):
+    #                 if not b:
+    #                     print(f"\t{i}: input {s[i]} --> filtered {f[i]}")
+
+    # for c in i.
 
     title = (
         f"Data Used for Curve {blk.curve_id}\n"
@@ -790,7 +824,7 @@ def plot_curve(blk):
     ax.set_ylabel(ylabe)
     ax.set_title(title)
     # ax.set_ylim([-0.05, 1.02])
-    ax.set_xlim([-5, max(blk.all_bvs) * 1.05])
+    # ax.set_xlim([-5, max(blk.all_bvs) * 1.05])
     ax.legend()
     plt.tight_layout()
 
@@ -799,23 +833,23 @@ def plot_curve(blk):
     blk.all_figs["raw_curve"] = {"fig": fig, "ax": ax}
 
 
-# def _just_plot_curve(self):
-#     tmp = self.df_curve.reset_index()
-#     _, ax = plt.subplots(figsize=self.figsize)
+# def _just_plot_curve(blk):
+#     tmp = blk.df_curve.reset_index()
+#     _, ax = plt.subplots(figsize=blk.figsize)
 
 #     ax.plot(tmp.bv, tmp.c_norm, marker=".")
 #     ax.plot([0, tmp.bv.max()], [1, 1], linestyle=":")
 #     ax.set_title(
-#         f"Curve {self.curve_id}: {self.ref.replace('_', ' ').title()}, {self.compound.upper()}"
+#         f"Curve {blk.curve_id}: {blk.ref.replace('_', ' ').title()}, {blk.compound.upper()}"
 #     )
 
 #     # ax.set_ylim([-0.01, 1.02])
 #     ax.set_xlabel("BV")
-#     ax.set_ylabel(f"C/C$_0$ [{self.compound.upper()}]")
-#     self.idx_labels = zip(
+#     ax.set_ylabel(f"C/C$_0$ [{blk.compound.upper()}]")
+#     blk.idx_labels = zip(
 #         tmp.bv.to_list(), tmp.c_norm.to_list(), tmp.index.to_list()
 #     )
-#     for x, y, i in self.idx_labels:
+#     for x, y, i in blk.idx_labels:
 #         # ax.annotate(f"{i}\n{y}", (x, y), fontsize="small", rotation=-45)
 #         ax.annotate(i, (x, y), fontsize="small")
 #     plt.tight_layout()
