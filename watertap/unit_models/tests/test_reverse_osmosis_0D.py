f59b598125537ed0eaad065f290f0bf76300ccf3
Adam Atia <aatia@keylogic.com>
2024-07-25 17:36:57 -0400

    raise config error for simple RO

--- watertap/unit_models/tests/test_reverse_osmosis_0D.py
+++ watertap/unit_models/tests/test_reverse_osmosis_0D.py
@@ -985,7 +985,7 @@ class TestReverseOsmosis0D_friction_factor_spiral_wound(UnitTestHarness):
 
 
 @pytest.mark.unit
-def test_RO_dynamic():
+def test_RO_dynamic_instantiation():
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=True, time_set=[0, 1], time_units=pyunits.minute)
 
