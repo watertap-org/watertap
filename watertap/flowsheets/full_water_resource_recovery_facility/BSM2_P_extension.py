c2ee4ac4fad0f760870a4bf0aa49dd631e46d3c6
MarcusHolly <marcus.holly@keylogic.com>
2024-09-17 22:17:47 -0400

    Touch effluent metrics before scaling

--- watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension.py
+++ watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension.py
@@ -532,6 +532,14 @@ def set_operating_conditions(m):
     m.fs.thickener.hydraulic_retention_time.fix(86400 * pyo.units.s)
     m.fs.thickener.diameter.fix(10 * pyo.units.m)
 
+    # Touch treated properties
+    m.fs.Treated.properties[0].TSS
+    m.fs.Treated.properties[0].COD
+    m.fs.Treated.properties[0].BOD5
+    m.fs.Treated.properties[0].SNKj
+    m.fs.Treated.properties[0].SP_organic
+    m.fs.Treated.properties[0].SP_inorganic
+
     def scale_variables(m):
         for var in m.fs.component_data_objects(pyo.Var, descend_into=True):
             if "flow_vol" in var.name:
@@ -557,14 +565,6 @@ def set_operating_conditions(m):
     # Apply scaling
     scale_variables(m)
     iscale.calculate_scaling_factors(m)
-
-    # Touch treated properties
-    m.fs.Treated.properties[0].TSS
-    m.fs.Treated.properties[0].COD
-    m.fs.Treated.properties[0].BOD5
-    m.fs.Treated.properties[0].SNKj
-    m.fs.Treated.properties[0].SP_organic
-    m.fs.Treated.properties[0].SP_inorganic
 
 
 def initialize_system(m, bio_P=False, solver=None):
