0f3a41df7c61cd8ac9130f8ef66b3127e92e9a6c
Adam Atia <aatia@keylogic.com>
2024-10-03 16:59:27 -0400

    revert hydraulic retention time constraint

--- watertap/unit_models/cstr.py
+++ watertap/unit_models/cstr.py
@@ -69,8 +69,7 @@ class CSTRData(CSTRIDAESData):
         def CSTR_retention_time_rule(self, t):
             return (
                 self.hydraulic_retention_time[t]
-                * self.control_volume.properties_in[t].flow_vol
-                == self.volume[t]
+                == self.volume[t] / self.control_volume.properties_in[t].flow_vol
             )
 
         self.CSTR_retention_time = Constraint(
