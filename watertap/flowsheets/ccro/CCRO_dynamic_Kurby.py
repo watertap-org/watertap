889f15a0fd2470d14dcdce164efa049f66093d21
fahim831 <44279032+fahim831@users.noreply.github.com>
2024-10-22 16:26:57 -0400

    Scaling issues

--- watertap/flowsheets/ccro/CCRO_dynamic_Kurby.py
+++ watertap/flowsheets/ccro/CCRO_dynamic_Kurby.py
@@ -20,9 +20,10 @@ from pyomo.environ import TransformationFactory
 from pyomo.network import Arc
 
 import idaes.core.util.scaling as iscale
+from idaes.core.util import DiagnosticsToolbox
 from idaes.core.util.initialization import propagate_state
 from idaes.core import FlowsheetBlock, UnitModelCostingBlock
-from idaes.core.util.scaling import calculate_scaling_factors, set_scaling_factor
+from idaes.core.util.scaling import calculate_scaling_factors, set_scaling_factor, constraint_scaling_transform
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.models.unit_models import Product, Feed
 from idaes.models.unit_models.mixer import (
@@ -149,7 +150,40 @@ class CCRO:
         self.m = self.build_system()
         self.set_operating_conditions()
         # self.scale_system()
-        self.initialize_system()
+        # self.initialize()
+
+    def check_jac(self, m, print_extreme_jacobian_values=True):
+        jac, jac_scaled, nlp = iscale.constraint_autoscale_large_jac(m, min_scale=1e-8)
+        try:
+            cond_number = iscale.jacobian_cond(m, jac=jac_scaled) / 1e10
+            print("--------------------------")
+            print("COND NUMBER:", cond_number)
+        except:
+            print("Cond number failed")
+            cond_number = None
+        if print_extreme_jacobian_values:
+            print("--------------------------")
+            print("Extreme Jacobian entries:")
+            extreme_entries = iscale.extreme_jacobian_entries(
+                m, jac=jac_scaled, nlp=nlp, zero=1e-20, large=100
+            )
+            for val, var, con in extreme_entries:
+                print(val, var.name, con.name)
+            print("--------------------------")
+            print("Extreme Jacobian columns:")
+            extreme_cols = iscale.extreme_jacobian_columns(
+                m, jac=jac_scaled, nlp=nlp, small=1e-3
+            )
+            for val, var in extreme_cols:
+                print(val, var.name)
+            print("------------------------")
+            print("Extreme Jacobian rows:")
+            extreme_rows = iscale.extreme_jacobian_rows(
+                m, jac=jac_scaled, nlp=nlp, small=1e-3
+            )
+            for val, con in extreme_rows:
+                print(val, con.name)
+        return cond_number
 
     def build_system(self, time_blk=None):
         """
@@ -263,13 +297,24 @@ class CCRO:
             "flow_mass_phase_comp", 1e2, index=("Liq", "NaCl")
         )
 
-        set_scaling_factor(m.fs.P1.control_volume.work, 1e-3)
-        set_scaling_factor(m.fs.P2.control_volume.work, 1e-3)
-        set_scaling_factor(m.fs.RO.area, 1e-2)
+        set_scaling_factor(m.fs.P1.control_volume.work, 1e-2)
+        set_scaling_factor(m.fs.P2.control_volume.work, 1e-2)
+        set_scaling_factor(m.fs.RO.area, 1)
 
-        set_scaling_factor(m.fs.water_recovery, 10)
-        set_scaling_factor(m.fs.feed_flow_mass_water, 1)
+        set_scaling_factor(m.fs.water_recovery, 1e2)
+        set_scaling_factor(m.fs.feed_flow_mass_water, 1e1)
         set_scaling_factor(m.fs.feed_salinity, 1)
+
+        constraint_scaling_transform(m.fs.RO.feed_side.eq_K[0.0,0.0,'NaCl'], 1e7)
+        constraint_scaling_transform(m.fs.RO.feed_side.eq_K[0.0,1.0,'NaCl'], 1e7)
+        constraint_scaling_transform(m.fs.RO.eq_flux_mass[0.0,0.0,'Liq','NaCl'], 1e7)
+        constraint_scaling_transform(m.fs.RO.eq_flux_mass[0.0,1.0,'Liq','NaCl'], 1e7)
+        constraint_scaling_transform(m.fs.RO.eq_flux_mass[0.0,0.0,'Liq','H2O'], 1e4)
+        constraint_scaling_transform(m.fs.RO.eq_flux_mass[0.0,1.0,'Liq','H2O'], 1e4)
+        constraint_scaling_transform(m.fs.RO.eq_recovery_mass_phase_comp[0.0,'NaCl'], 1e4)
+        constraint_scaling_transform(m.fs.RO.eq_mass_frac_permeate[0.0,0.0,'NaCl'], 1e5)
+        constraint_scaling_transform(m.fs.RO.eq_mass_frac_permeate[0.0,1.0,'NaCl'], 1e5)
+        constraint_scaling_transform(m.fs.RO.eq_permeate_production[0.0,'Liq','NaCl'], 1e4)
 
         calculate_scaling_factors(m)
 
@@ -320,21 +365,23 @@ class CCRO:
         """
 
         m.fs.P2.efficiency_pump.fix(self.p2_eff)
-        # m.fs.P2.control_volume.properties_out[0].pressure.set_value(self.p2_pressure_start)
+        m.fs.P2.control_volume.properties_out[0].pressure.fix(self.p1_pressure_start)
         m.fs.P2.control_volume.properties_out[0].pressure.setub(6e6)
 
         """
         Mixer operating conditions
         """
 
-        m.fs.M1_constraint_1 = Constraint(
-            expr=m.fs.M1.inlet_2_state[0].pressure
-            == m.fs.M1.inlet_1_state[0].pressure
-        )
+        # m.fs.M1_constraint_1 = Constraint(
+        #     expr=m.fs.M1.inlet_2_state[0].pressure
+        #     == m.fs.M1.inlet_1_state[0].pressure
+        # )
         m.fs.M1_constraint_2 = Constraint(
             expr=m.fs.M1.inlet_1_state[0].pressure
             == m.fs.M1.mixed_state[0].pressure
         )
+
+        # m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "NaCl"].setub(5)
 
         """
         RO operating conditions
@@ -347,6 +394,7 @@ class CCRO:
         m.fs.RO.length.fix(self.membrane_length)
         m.fs.RO.feed_side.channel_height.fix(self.channel_height)
         m.fs.RO.feed_side.spacer_porosity.fix(self.spacer_porosity)
+        m.fs.RO.RO_constraint_1 = Constraint(expr=m.fs.RO.mixed_permeate[0].conc_mass_phase_comp["Liq", "NaCl"] <= 0.035) # Activating this reduces condition number by 8 OoM
 
         print("DOF =", degrees_of_freedom(m))
         print("DOF FEED =", degrees_of_freedom(m.fs.feed))
@@ -356,7 +404,16 @@ class CCRO:
         print("DOF RO =", degrees_of_freedom(m.fs.RO))
         assert_no_degrees_of_freedom(m)
 
-    def initialize_system(self, m=None):
+    def initialize_mixer(self, m=None, guess=False):
+        m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "H2O"].set_value(0.75465)
+        m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "NaCl"].set_value(0.0029261)
+        m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "H2O"].set_value(0.80332)
+        m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "NaCl"].set_value(0.0030916)
+        m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "NaCl"].set_value(4.0)
+        m.fs.M1.inlet_2_state[0].pressure.set_value(2.1098e+06)
+        m.fs.M1.initialize()
+
+    def do_forward_initialization_pass(self, m=None, pass_num=1):
         """
         Initialize steady-state model
         """
@@ -369,21 +426,17 @@ class CCRO:
         m.fs.P1.initialize()
 
         propagate_state(m.fs.P1_to_M1)
-        m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "H2O"].set_value(0.75465)
-        m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "NaCl"].set_value(0.0029261)
-        m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "H2O"].set_value(0.80332)
-        m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "NaCl"].set_value(0.0030916)
-        m.fs.M1.inlet_2_state[0].pressure.set_value(2.1098e+06)
-
+       
         print('M1 first initialization')
         m.fs.M1.report()
-        m.fs.M1.initialize()
+        if pass_num > 0:
+            m.fs.M1.initialize()
+        else:
+            self.initialize_mixer(m)
         m.fs.M1.report()
 
         propagate_state(m.fs.M1_to_RO)
         
-        
-
         print('RO first initialization')
         m.fs.RO.feed_side.properties_in[0].pressure_osm_phase
         m.fs.RO.feed_side.properties_in[0].temperature = value(
@@ -408,27 +461,23 @@ class CCRO:
         m.fs.P2.report()
 
         propagate_state(m.fs.P2_to_M1)
+    
+    def initialize(self, m=None):
+        # initialize unit by unit
+        dt = DiagnosticsToolbox(self.m)
+        dt.report_structural_issues()
         
-        print('M1 second initialization')
-        m.fs.M1.report()
-        m.fs.M1.inlet_2_state[0].pressure.unfix()
-        m.fs.M1.initialize()
-        m.fs.M1.report()
-
-        propagate_state(m.fs.M1_to_RO)
-
-        print('RO second initialization')
-        m.fs.RO.report()
-        m.fs.RO.initialize()
-        m.fs.RO.report()
-
-        print(value(m.fs.RO.feed_side.properties_out[0].pressure))
-        m.fs.RO.feed_side.properties_in[0].pressure_osm_phase.display()
-
-        propagate_state(m.fs.RO_retentate_to_P2)
-        m.fs.P2.report()
-        m.fs.P2.initialize()
-        m.fs.P2.report()
+        # dt.display_constraints_with_extreme_jacobians()
+        for idx, init_pass in enumerate(range(1)):
+            print(f"\n\nINITIALIZATION PASS {idx+1}\n\n")
+            self.do_forward_initialization_pass(m, pass_num=idx)
+            self.print_results()
+        self.check_jac(self.m)
+        dt.report_numerical_issues()
+        dt.display_constraints_with_large_residuals()
+        # dt.display_variables_with_extreme_jacobians()
+        # self.m.fs.P1.control_volume.properties_out[0].pressure.unfix()
+        # self.m.fs.M1_constraint_1.deactivate()
 
     def fix_dof_and_initialize(self, m=None):
         """
@@ -449,6 +498,8 @@ class CCRO:
 
         print("\n--------- SOLVING ---------\n")
         print(f"Degrees of Freedom: {degrees_of_freedom(model)}")
+        optarg = {"tol": 1e-5, "linear_solver": "ma27", "max_iter": 1000}
+        solver.options = optarg
         results = solver.solve(model, tee=tee)
 
         if check_optimal_termination(results):
@@ -472,7 +523,53 @@ class CCRO:
             print(msg)
             # debug(model, solver=solver, automate_rescale=False, resolve=False)
             # check_jac(model)
-            assert False
+            # assert False
+    
+    def print_results(self, m=None):
+        print("\n\n")
+        print(
+            f'MIXER INLET 1: {value(self.m.fs.M1.inlet_1_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'MIXER INLET 2: {value(self.m.fs.M1.inlet_2_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'MIXER OUTLET: {value(self.m.fs.M1.mixed_state[0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'MIXER CONC: {value(pyunits.convert(self.m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "NaCl"], to_units=pyunits.g/pyunits.L)):<5.2f} {pyunits.get_units(pyunits.convert(self.m.fs.M1.mixed_state[0].conc_mass_phase_comp["Liq", "NaCl"], to_units=pyunits.g/pyunits.L))}'
+        )
+        print("\n")
+        print(
+            f'PUMP 1 INLET: {value(self.m.fs.P1.control_volume.properties_in[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'PUMP 1 OUTLET: {value(self.m.fs.P1.control_volume.properties_out[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f"PUMP 1 PRESSURE: {value(pyunits.convert(self.m.fs.P1.control_volume.properties_out[0.0].pressure, to_units=pyunits.bar)):<5.2f}"
+        )
+        print("\n")
+        print(
+            f'PUMP 2 INLET: {value(self.m.fs.P2.control_volume.properties_in[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'PUMP 2 OUTLET: {value(self.m.fs.P2.control_volume.properties_out[0.0].flow_mass_phase_comp["Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f"PUMP 2 PRESSURE: {value(pyunits.convert(self.m.fs.P2.control_volume.properties_out[0.0].pressure, to_units=pyunits.bar)):<5.2f}"
+        )
+        print("\n")
+        print(f'RO FEED: {value(self.m.fs.RO.inlet.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}')
+        print(
+            f'RO PRODUCT: {value(self.m.fs.RO.permeate.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}'
+        )
+        print(
+            f'RO BRINE: {value(self.m.fs.RO.retentate.flow_mass_phase_comp[0,"Liq", "H2O"]):<5.2f}'
+        )
+        print("\n\n")
+        print(self.m.fs.M1.report())
+        print(self.m.fs.RO.report())
 
 if __name__ == "__main__":
 
@@ -492,6 +589,8 @@ if __name__ == "__main__":
     }
 
     ccro = CCRO(**initial_conditions)
-
-    ccro.solve()
+    ccro.initialize()
+    ccro.solve(tee=False)
+    dt = DiagnosticsToolbox(ccro.m)
+    dt.compute_infeasibility_explanation()
     ccro.mp_df
