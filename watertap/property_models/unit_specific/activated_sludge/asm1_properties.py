248b82dd0d8a44b3eaee25a2fbd416ed86f8e5c3
MarcusHolly <marcus.holly@keylogic.com>
2024-11-22 15:57:24 -0500

    Have new scaling factors match old scaling factors

--- watertap/property_models/unit_specific/activated_sludge/asm1_properties.py
+++ watertap/property_models/unit_specific/activated_sludge/asm1_properties.py
@@ -202,12 +202,12 @@ class ASM1PropertiesScaler(CustomScalerBase):
 
     UNIT_SCALING_FACTORS = {
         # "QuantityName: (reference units, scaling factor)
-        "Pressure": (pyo.units.Pa, 1e-5),
+        "Pressure": (pyo.units.Pa, 1e-6),
     }
 
     DEFAULT_SCALING_FACTORS = {
         "flow_vol": 1e1,
-        "temperature": 1e-2,
+        "temperature": 1e-1,
     }
 
     def variable_scaling_routine(
