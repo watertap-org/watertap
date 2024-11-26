a96890740ce3809b03ee7be3f755a4aa006565f7
Ludovico Bianchi <lbianchi@lbl.gov>
2024-11-26 08:34:28 -0600

    Update idaes-flowsheet-requirement to 0.1.dev1

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
             # treebeardtech/nbmake#121
             "nbmake != 1.5.1",
             "nbconvert",
-            "idaes-flowsheet-processor @ git+https://github.com/watertap-org/idaes-flowsheet-processor@refs/pull/6/merge",
+            "idaes-flowsheet-processor @ git+https://github.com/watertap-org/idaes-flowsheet-processor@0.1.dev1",
         ],
         "notebooks": [
             "jupyter",
