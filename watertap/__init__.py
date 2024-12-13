581b728f9a22f27aee88d166504799251d0b18f4
Ludovico Bianchi <lbianchi@lbl.gov>
2024-12-13 09:23:54 -0600

    Remove watertap.core.plugins subpackage

--- watertap/__init__.py
+++ watertap/__init__.py
@@ -12,7 +12,3 @@
 import pathlib
 
 _ROOT = pathlib.Path(__file__).parent.absolute()
-
-# create the ipopt-watertap solver and register
-# it as the default IDAES solver
-import watertap.core.plugins
