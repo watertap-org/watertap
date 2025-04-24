fcbd6f16bc5b53e025799af04e6663674e1c4fa0
MarcusHolly <marcus.holly@keylogic.com>
2025-04-24 19:19:20 -0400

    Address pylint issue

--- watertap/flowsheets/full_water_resource_recovery_facility/test/test_BSM2_P_extension.py
+++ watertap/flowsheets/full_water_resource_recovery_facility/test/test_BSM2_P_extension.py
@@ -27,12 +27,6 @@ from pyomo.util.check_units import assert_units_consistent
 
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.core.util.model_diagnostics import IpoptConvergenceAnalysis
-from idaes.core.util.parameter_sweep import (
-    ParameterSweepSpecification,
-)
-from idaes.core.surrogate.pysmo.sampling import (
-    UniformSampling,
-)
 
 from watertap.flowsheets.full_water_resource_recovery_facility.BSM2_P_extension import (
     main,
