c0e657eb4d2eeeb338bb72caacf4745ce7137fd2
kurbansitterley <kurban.sitterley@nrel.gov>
2025-05-08 13:18:14 -0600

    carbon steel as default reactor material and fix USD yr; floc cost to power eq; add references

--- watertap/costing/unit_models/electrocoagulation.py
+++ watertap/costing/unit_models/electrocoagulation.py
@@ -17,18 +17,34 @@ from ..util import (
     make_fixed_operating_cost_var,
 )
 
-# Costing equations from:
+# Costing data & equations adapted from:
 
-# Saad et al., 2022 - doi.org/10.1016/j.jksus.2022.102015;
-# Smith, 2005 - doi.org/10.1205/cherd.br.0509
-# Reference for Al cost: Anuf et al., 2022 - https://doi.org/https://doi.org/10.1016/j.jwpe.2022.103074
+# W. McGivney and S. Kawamura (2008)
+# Cost Estimating Manual for Water Treatment Facilities
+# DOI: 10.1002/9780470260036
+
+# Power supply cost estimation from magna-power.com
+# Linear equation fit to SL and MT series cost data
+
+# R. Smith (2005)
+# Chemical Process: Design and Integration, 1st ed.
+# ISBN: 978-0471486817
+
+# For material cost ratios (carbon steel = 1):
+# https://www.engineeringtoolbox.com/piping-materials-cost-ratios-d_864.html
+# https://web.mit.edu/course/3/3.11/www/modules/props.pdf
+
+# A. R. Anuf, K. Ramaraj, V. S. Sivasankarapillai, R. Dhanusuraman, J. P. Maran, G. Rajeshkumar (2022)
+# Optimization of electrocoagulation process for treatment of
+#   rice mill effluent using response surface methodology
+# DOI: 10.1016/j.jwpe.2022.103074
 
 
 def build_aluminum_cost_param_block(blk):
     costing = blk.parent_block()
     blk.cost = pyo.Param(
         initialize=2.23,
-        units=pyo.units.USD_2021 / pyo.units.kg,  # Anuf et al., 2022
+        units=pyo.units.USD_2021 / pyo.units.kg,  # Anuf et al. (2022)
         mutable=True,
         doc="Cost of aluminum plate per kg",
     )
@@ -39,7 +55,7 @@ def build_iron_cost_param_block(blk):
     costing = blk.parent_block()
     blk.cost = pyo.Param(
         initialize=3.41,
-        units=pyo.units.USD_2021 / pyo.units.kg,  # Anuf et al., 2022
+        units=pyo.units.USD_2021 / pyo.units.kg,  # Anuf et al. (2022)
         mutable=True,
         doc="Cost of iron plate per kg",
     )
@@ -50,10 +66,11 @@ def build_electrocoagulation_cost_param_block(blk):
 
     costing = blk.parent_block()
 
+    # Parameters from Table 2.1 for "Agitated Reactor" in Smith (2005)
     blk.reactor_capital_cost_base = pyo.Var(
         initialize=1.15e4,
-        units=pyo.units.USD_2020,
-        doc="Reactor capital cost base cost",
+        units=pyo.units.USD_2000,
+        doc="Reactor capital cost base parameter",
     )
 
     blk.reactor_capital_cost_exponent = pyo.Var(
@@ -62,10 +79,14 @@ def build_electrocoagulation_cost_param_block(blk):
         doc="Reactor capital cost exponent",
     )
 
+    # Material cost ratios can be found at:
+    # https://www.engineeringtoolbox.com/piping-materials-cost-ratios-d_864.html
+    # https://web.mit.edu/course/3/3.11/www/modules/props.pdf
+    # Table 2.2 and Table 2.3 in Smith (2005)
     blk.reactor_material_coeff = pyo.Var(
-        initialize=3.4,
+        initialize=1,  # carbon steel = 1 is default
         units=pyo.units.dimensionless,
-        doc="Reactor capital cost material coeff (3.4 for stainless steel; 0.062 for PVC)",
+        doc="Reactor capital cost material coefficient",
     )
 
     blk.reactor_capital_safety_factor = pyo.Var(
@@ -74,22 +95,25 @@ def build_electrocoagulation_cost_param_block(blk):
         doc="Reactor capital cost safety factor",
     )
 
+    # see: https://magna-power.com/products/magnadc
     blk.power_supply_capital_slope = pyo.Var(
         initialize=0.51972,
         units=pyo.units.USD_2020 / pyo.units.watt,
         doc="DC power supply + transformer + electrical connection base cost",
     )
 
-    blk.floc_capital_cost_slope = pyo.Var(
-        initialize=952902,
-        units=pyo.units.USD_2007 / pyo.units.Mgallons,
-        doc="Flocculator capital cost equation slope",
+    # Flocculator costs rom Figure 5.5.22 in McGivney & Kawamura (2008)
+    # linear equation refit to power equation (C = A*x**b)
+    blk.floc_capital_cost_base = pyo.Var(
+        initialize=1.0757e06,
+        units=pyo.units.USD_2007,
+        doc="Flocculator capital cost equation base parameter",
     )
 
-    blk.floc_capital_cost_intercept = pyo.Var(
-        initialize=177335,
-        units=pyo.units.USD_2007,
-        doc="Flocculator capital cost equation intercept",
+    blk.floc_capital_cost_exponent = pyo.Var(
+        initialize=9.5139e-01,
+        units=pyo.units.dimensionless,
+        doc="Flocculator capital cost equation exponent",
     )
 
     blk.sludge_handling_cost = pyo.Var(
@@ -108,12 +132,6 @@ def build_electrocoagulation_cost_param_block(blk):
         initialize=2,
         units=pyo.units.dimensionless,
         doc="Electrode material cost safety factor",
-    )
-
-    blk.number_redundant_reactors = pyo.Var(
-        initialize=2,
-        units=pyo.units.dimensionless,
-        doc="Number of redundant EC reactors",
     )
 
     blk.fix_all_vars()
@@ -139,10 +157,6 @@ def cost_electrocoagulation(blk):
     make_fixed_operating_cost_var(blk)
 
     ec = blk.unit_model
-
-    flow_m3_yr = pyo.units.convert(
-        ec.properties_in[0].flow_vol, to_units=pyo.units.m**3 / pyo.units.year
-    )
 
     blk.annual_sludge_flow = pyo.units.convert(
         sum(
@@ -190,8 +204,11 @@ def cost_electrocoagulation(blk):
         doc="Annual sludge management cost",
     )
 
-    if ec.config.reactor_material == "pvc":
-        ec_params.reactor_material_coeff.fix(0.062)
+    if ec.config.reactor_material == "carbon_steel":
+        ec_params.reactor_material_coeff.fix(1)
+
+    elif ec.config.reactor_material == "pvc":
+        ec_params.reactor_material_coeff.fix(0.56)
 
     elif ec.config.reactor_material == "stainless_steel":
         ec_params.reactor_material_coeff.fix(3.4)
@@ -205,7 +222,7 @@ def cost_electrocoagulation(blk):
     capital_cost_expr = 0
 
     ec_reactor_dim = pyo.units.convert(
-        ec.cell_volume * pyo.units.m**-3, to_units=pyo.units.dimensionless
+        ec.reactor_volume * pyo.units.m**-3, to_units=pyo.units.dimensionless
     )
     blk.capital_cost_reactor_constraint = pyo.Constraint(
         expr=blk.capital_cost_reactor
@@ -248,9 +265,12 @@ def cost_electrocoagulation(blk):
     blk.capital_cost_floc_constraint = pyo.Constraint(
         expr=blk.capital_cost_floc_reactor
         == pyo.units.convert(
-            ec_params.floc_capital_cost_slope
-            * pyo.units.convert(ec.floc_basin_vol, to_units=pyo.units.Mgallons)
-            + ec_params.floc_capital_cost_intercept,
+            ec_params.floc_capital_cost_base
+            * pyo.units.convert(
+                ec.floc_basin_vol * pyo.units.Mgallons**-1,
+                to_units=pyo.units.dimensionless,
+            )
+            ** ec_params.floc_capital_cost_exponent,
             to_units=base_currency,
         )
     )
@@ -275,7 +295,8 @@ def cost_electrocoagulation(blk):
 
     blk.annual_electrode_replacement_mass_flow = pyo.Expression(
         expr=pyo.units.convert(
-            ec.coagulant_dose * flow_m3_yr, to_units=pyo.units.kg / pyo.units.year
+            ec.coagulant_dose * ec.properties_in[0].flow_vol_phase["Liq"],
+            to_units=pyo.units.kg / pyo.units.year,
         )
     )
 
