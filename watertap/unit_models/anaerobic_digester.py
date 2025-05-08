6dab85513cf63779dee3415fc6d11e288c04d9d8
MarcusHolly <marcus.holly@keylogic.com>
2025-05-08 00:01:35 -0400

    Restructure HRT equations

--- watertap/unit_models/anaerobic_digester.py
+++ watertap/unit_models/anaerobic_digester.py
@@ -875,8 +875,9 @@ see reaction package for documentation.}""",
 
         def AD_retention_time_rule(self, t):
             return (
-                self.hydraulic_retention_time[t]
-                == self.volume_AD[t] / self.liquid_phase.properties_in[t].flow_vol
+                self.volume_AD[t]
+                == self.hydraulic_retention_time[t]
+                * self.liquid_phase.properties_in[t].flow_vol
             )
 
         self.AD_retention_time = Constraint(
