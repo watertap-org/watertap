3be6102357d3e1f0e1379bbfd841c919e3212c98
kurbansitterley <kurban.sitterley@nrel.gov>
2025-05-13 18:09:40 -0600

    add costing to model

--- watertap/unit_models/pseudo_steady_state/CCRO.py
+++ watertap/unit_models/pseudo_steady_state/CCRO.py
@@ -38,6 +38,7 @@ from watertap.unit_models.pressure_changer import Pump
 from watertap.unit_models.reverse_osmosis_1D import ReverseOsmosis1D, ReverseOsmosis1DData
 from watertap.unit_models.reverse_osmosis_base import _add_has_full_reporting
 from watertap.costing.unit_models.ccro import cost_ccro
+from watertap.costing.unit_models.reverse_osmosis import cost_reverse_osmosis
 
 __author__ = "Adam Atia, Bernard Knueven"
 
@@ -52,63 +53,14 @@ class CCRO1DData(ReverseOsmosis1DData):
     """
     # this model is meant to be identical to RO1D but with different costing
 
-    CONFIG = CONFIG_Template()
+    CONFIG = ReverseOsmosis1DData.CONFIG()
 
-
-    # CONFIG.declare(
-    #     "multi",
-    #     ConfigValue(
-    #         # domain=Pump,
-    #         default=None,
-    #         description="",
-    #         doc="""""",
-    #     ),
-    # )
-
-    _add_has_full_reporting(CONFIG)
+    # _add_has_full_reporting(CONFIG)
     
     def build(self):
         super().build()
 
-        # if self.config.feed_pump is not None:
-        #     self.feed_pump = add_object_reference(self, "feed_pump", self.config.feed_pump)
-        
-    # def calculate_scaling_factors(self):
-    #     if iscale.get_scaling_factor(self.dens_solvent) is None:
-    #         sf = iscale.get_scaling_factor(
-    #             self.feed_side.properties[0, 0].dens_mass_phase["Liq"]
-    #         )
-    #         iscale.set_scaling_factor(self.dens_solvent, sf)
-
-    #     super().calculate_scaling_factors()
-
-    #     # these variables should have user input, if not there will be a warning
-    #     if iscale.get_scaling_factor(self.width) is None:
-    #         sf = iscale.get_scaling_factor(self.width, default=1, warning=True)
-    #         iscale.set_scaling_factor(self.width, sf)
-
-    #     if iscale.get_scaling_factor(self.length) is None:
-    #         sf = iscale.get_scaling_factor(self.length, default=10, warning=True)
-    #         iscale.set_scaling_factor(self.length, sf)
-
-    #     for (t, x, p, j), v in self.mass_transfer_phase_comp.items():
-    #         sf = (
-    #             iscale.get_scaling_factor(
-    #                 self.feed_side.properties[t, x].get_material_flow_terms(p, j)
-    #             )
-    #             / iscale.get_scaling_factor(self.feed_side.length)
-    #         ) * value(self.nfe)
-    #         if iscale.get_scaling_factor(v) is None:
-    #             iscale.set_scaling_factor(v, sf)
-    #         v = self.feed_side.mass_transfer_term[t, x, p, j]
-    #         if iscale.get_scaling_factor(v) is None:
-    #             iscale.set_scaling_factor(v, sf)
-
-    #     if hasattr(self, "deltaP"):
-    #         for v in self.deltaP.values():
-    #             if iscale.get_scaling_factor(v) is None:
-    #                 iscale.set_scaling_factor(v, 1e-4)
-
     @property
     def default_costing_method(self):
         return cost_ccro
+        # return cost_reverse_osmosis
