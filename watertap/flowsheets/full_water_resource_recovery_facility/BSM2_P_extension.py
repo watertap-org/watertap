b8e2ec737fb1b8e8d90e71b9b29baf4f157d2b31
MarcusHolly <marcus.holly@keylogic.com>
2025-05-30 05:17:08 -0400

    Clean up flowsheet

--- watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension.py
+++ watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension.py
@@ -95,9 +95,6 @@ from watertap.costing.unit_models.clarifier import (
     cost_primary_clarifier,
 )
 
-from idaes.core.util import DiagnosticsToolbox
-from idaes.core.scaling import report_scaling_factors
-
 # Set up logger
 _log = idaeslog.getLogger(__name__)
 
@@ -153,15 +150,6 @@ def main(bio_P=False):
 
     display_costing(m)
     display_performance_metrics(m)
-
-    print("--- Scaling Factors ---")
-    report_scaling_factors(m, descend_into=True)
-
-    dt = DiagnosticsToolbox(m)
-    print("Numerical Issues")
-    dt.report_numerical_issues()
-    dt.display_variables_with_extreme_jacobians()
-    dt.display_constraints_with_extreme_jacobians()
 
     return m, results
 
@@ -598,11 +586,6 @@ def set_operating_conditions(m, bio_P=False):
     scale_variables(m)
     iscale.calculate_scaling_factors(m)
 
-    if bio_P:
-        iscale.constraint_scaling_transform(
-            m.fs.AD.liquid_phase.reactions[0].rate_expression["R24"], 1e6
-        )
-
 
 def initialize_system(m, bio_P=False, solver=None):
     # Initialize flowsheet
@@ -621,7 +604,7 @@ def initialize_system(m, bio_P=False, solver=None):
 
     if bio_P:
         tear_guesses = {
-            "flow_vol": {0: 1.2368},
+            "flow_vol": {0: 1.23682},
             "conc_mass_comp": {
                 (0, "S_A"): 0.0006,
                 (0, "S_F"): 0.0004,
@@ -640,7 +623,7 @@ def initialize_system(m, bio_P=False, solver=None):
                 (0, "X_PAO"): 3.6,
                 (0, "X_PHA"): 0.094,
                 (0, "X_PP"): 1.16,
-                (0, "X_S"): 0.059,
+                (0, "X_S"): 0.0594,
             },
             "temperature": {0: 308.15},
             "pressure": {0: 101325},
@@ -1026,4 +1009,3 @@ if __name__ == "__main__":
         time_point=0,
     )
     print(stream_table_dataframe_to_string(stream_table))
-    m.fs.Treated.display()
