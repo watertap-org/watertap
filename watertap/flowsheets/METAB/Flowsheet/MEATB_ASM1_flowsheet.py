56137c2d3890ce5fff317cc5cdcd67a835d7e9a5
Maojian Wang <maojian.wang@netl.doe.gov>
2025-06-18 11:24:56 -0400

    update METAB plus ASM flowsheet with correct influent. and start adding workflow demo

--- watertap/flowsheets/METAB/Flowsheet/MEATB_ASM1_flowsheet.py
+++ watertap/flowsheets/METAB/Flowsheet/MEATB_ASM1_flowsheet.py
@@ -19,14 +19,15 @@ __author__ = "Maojian Wang"
 import pyomo.environ as pyo
 import re
 from pyomo.network import Arc, SequentialDecomposition
-from watertap.unit_models.anaerobic_digester import AD
-from watertap.unit_models.thickener import Thickener
-from watertap.unit_models.dewatering import DewateringUnit
-from watertap.unit_models.cstr import CSTR
-from watertap.unit_models.clarifier import Clarifier
 
-from watertap.unit_models.translators.translator_asm1_adm1 import Translator_ASM1_ADM1
-from watertap.unit_models.translators.translator_adm1_asm1 import Translator_ADM1_ASM1
+# from watertap.unit_models.anaerobic_digester import AD
+# from watertap.unit_models.thickener import Thickener
+# from watertap.unit_models.dewatering import DewateringUnit
+# from watertap.unit_models.cstr import CSTR
+# from watertap.unit_models.clarifier import Clarifier
+
+# from watertap.unit_models.translators.translator_asm1_adm1 import Translator_ASM1_ADM1
+# from watertap.unit_models.translators.translator_adm1_asm1 import Translator_ADM1_ASM1
 
 import idaes.logger as idaeslog
 from watertap.core.solvers import get_solver
@@ -52,19 +53,20 @@ from idaes.models.unit_models import (
     Product,
 )
 
-from watertap.unit_models.aeration_tank import AerationTank, ElectricityConsumption
+# from watertap.unit_models.aeration_tank import AerationTank, ElectricityConsumption
 from watertap.property_models.unit_specific.activated_sludge.asm1_properties import (
     ASM1ParameterBlock,
 )
 from watertap.property_models.unit_specific.activated_sludge.asm1_reactions import (
     ASM1ReactionParameterBlock,
 )
-from watertap.core.util.initialization import assert_degrees_of_freedom
-from watertap.costing import WaterTAPCosting
-from watertap.costing.unit_models.clarifier import (
-    cost_circular_clarifier,
-    cost_primary_clarifier,
-)
+
+# from watertap.core.util.initialization import assert_degrees_of_freedom
+# from watertap.costing import WaterTAPCosting
+# from watertap.costing.unit_models.clarifier import (
+#     cost_circular_clarifier,
+#     cost_primary_clarifier,
+# )
 from pyomo.util.check_units import assert_units_consistent
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.core.util.initialization import propagate_state
@@ -118,6 +120,51 @@ def add_metab(m):
     return m
 
 
+def set_port_values(m):
+    # TODO feedwater block no inlet, need to fix
+    m.fs.metab_to_asm1 = Arc(
+        source=m.fs.metab_effluent.outlet, destination=m.fs.FeedWater.outlet
+    )
+    propagate_state(m.fs.metab_to_asm1)
+    # set_port_values(m.fs.FeedWater.outlet,m.fs.metab_effluent.outlet)
+    return m
+
+
+def reset_influent(m):
+    # TODO: Need to update
+    m.fs.FeedWater.flow_vol.fix(18446 * pyo.units.m**3 / pyo.units.day)
+    # TODO: Need to update
+    m.fs.FeedWater.temperature.fix(298.15 * pyo.units.K)
+    m.fs.FeedWater.pressure.fix(1 * pyo.units.atm)
+    m.fs.FeedWater.conc_mass_comp[0, "S_I"].fix(86.312 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "S_S"].fix(
+        0.039661 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.conc_mass_comp[0, "X_I"].fix(
+        0.11152 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.conc_mass_comp[0, "X_S"].fix(
+        0.74911 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.conc_mass_comp[0, "X_BH"].fix(0 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "X_BA"].fix(0 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "X_P"].fix(0 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "S_O"].fix(0 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "S_NO"].fix(0 * pyo.units.g / pyo.units.m**3)
+    m.fs.FeedWater.conc_mass_comp[0, "S_NH"].fix(
+        0.20578 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.conc_mass_comp[0, "S_ND"].fix(
+        0.0052525 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.conc_mass_comp[0, "X_ND"].fix(
+        0.058108 * 1000 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.FeedWater.alkalinity.fix(7 * pyo.units.mol / pyo.units.m**3)  # 46.6
+
+    return m
+
+
 def report_st(m):
 
     stream_table = create_stream_table_dataframe(
@@ -138,7 +185,12 @@ def report_st(m):
 
 if __name__ == "__main__":
 
+    # TODO: NEED to update parameters
     m = build_asm1()
     m = add_metab(m)
+    model_checker(m, True)
+    report_st(m)
+    # m = set_port_values(m)
+    m = reset_influent(m)
     model_checker(m, True)
     report_st(m)
