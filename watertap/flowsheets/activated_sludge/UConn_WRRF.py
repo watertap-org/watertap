11c1762e944fbbfc28e3025c3c244d5d6135783c
Adam Atia <aatia@keylogic.com>
2025-06-26 15:44:30 -0400

    rename file after adding asm3 compatibility

--- watertap/flowsheets/activated_sludge/UConn_WRRF.py
+++ watertap/flowsheets/activated_sludge/UConn_WRRF.py
@@ -10,8 +10,9 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 """
-Example of activated sludge process model.
-Initial layout:
+Example of activated sludge process model, based on 5 CSTR representation of the University of Connecticut's Water Resource Recovery Facility, conceptualized by Stuber's Process Systems and Operations Research Laboratory.
+
+Layout:
     * 5 reactors
         * R1, R3, R5: anoxic
         * R2 and R4: aerobic
@@ -27,27 +28,16 @@ Unit operations are modeled as follows:
     * Anoxic reactors as standard CSTRs (CSTR)
     * Aerobic reactors as AerationTanks
 
-Note that a pressure changer is required in the recycle stream to ensure the
-pressure inside the recycle loop is bounded. As the inlet Mixer uses a pressure
-minimization constraint and there is no pressure drop in the reactors, if pressure
-is not specified at some point within the recycle loop then it becomes unbounded.
-
 """
 
 __author__ = "Adam Atia"
 
+from enum import Enum, auto
 import pyomo.environ as pyo
 from pyomo.network import Arc, SequentialDecomposition
 
 from idaes.core import FlowsheetBlock
-from idaes.models.unit_models import (
-    Feed,
-    Mixer,
-    Separator,
-    PressureChanger,
-    Product,
-    MomentumMixingType
-)
+from idaes.models.unit_models import Feed, Mixer, Separator, Product, MomentumMixingType
 from idaes.models.unit_models.separator import SplittingType
 from watertap.core.solvers import get_solver
 from idaes.core.util.model_statistics import degrees_of_freedom
@@ -67,63 +57,71 @@ from watertap.property_models.unit_specific.activated_sludge.asm1_properties imp
 from watertap.property_models.unit_specific.activated_sludge.asm1_reactions import (
     ASM1ReactionParameterBlock,
 )
-
+from watertap.property_models.unit_specific.activated_sludge.asm3_properties import (
+    ASM3ParameterBlock,
+)
+from watertap.property_models.unit_specific.activated_sludge.asm3_reactions import (
+    ASM3ReactionParameterBlock,
+)
 from watertap.core.util.initialization import check_solve, interval_initializer
 
 # Set up logger
 _log = idaeslog.getLogger(__name__)
 
 
-def build_flowsheet():
+class ASMModel(auto):
+    asm1 = auto()
+    asm3 = auto()
+
+
+def build_flowsheet(asm_model=ASMModel.asm1):
     m = pyo.ConcreteModel()
 
     m.fs = FlowsheetBlock(dynamic=False)
+    if asm_model == ASMModel.asm1:
+        m.fs.props = ASM1ParameterBlock()
+        m.fs.rxn_props = ASM1ReactionParameterBlock(property_package=m.fs.props)
+    elif asm_model == ASMModel.asm3:
+        m.fs.props = ASM3ParameterBlock()
+        m.fs.rxn_props = ASM3ReactionParameterBlock(property_package=m.fs.props)
 
-    m.fs.props = ASM1ParameterBlock()
-    m.fs.rxn_props = ASM1ReactionParameterBlock(property_package=m.fs.props)
     # Feed water stream
     m.fs.feed = Feed(property_package=m.fs.props)
 
     # Mixer for feed water and recycled sludge
-    m.fs.M1 = Mixer(property_package=m.fs.props, 
-                    inlet_list=["feed", "recycle"],
-                    momentum_mixing_type=MomentumMixingType.none
-                    )
+    m.fs.M1 = Mixer(
+        property_package=m.fs.props,
+        inlet_list=["feed", "recycle"],
+        momentum_mixing_type=MomentumMixingType.none,
+    )
     m.fs.M1.outlet.pressure.fix()
     # m.fs.M1.pressure_equality_constraints[0,2].deactivate()
-    # First reactor (anoxic) 
+    # First reactor (anoxic)
     m.fs.R1 = CSTR(property_package=m.fs.props, reaction_package=m.fs.rxn_props)
-    
-    # Second reactor (aerobic) 
+
+    # Second reactor (aerobic)
     m.fs.R2 = AerationTank(property_package=m.fs.props, reaction_package=m.fs.rxn_props)
-    
-    
+
     # Mixer for R1 and R2 outlets
-    m.fs.M2= Mixer(property_package=m.fs.props, inlet_list=["R1_outlet", "R2_outlet"],
-                   momentum_mixing_type=MomentumMixingType.none
-                    )
+    m.fs.M2 = Mixer(
+        property_package=m.fs.props,
+        inlet_list=["R1_outlet", "R2_outlet"],
+        momentum_mixing_type=MomentumMixingType.none,
+    )
     m.fs.M2.outlet.pressure.fix()
 
     # Third reactor (anoxic)
-    m.fs.R3 = CSTR(
-        property_package=m.fs.props, reaction_package=m.fs.rxn_props
-    )
+    m.fs.R3 = CSTR(property_package=m.fs.props, reaction_package=m.fs.rxn_props)
     # Fourth reactor (aerobic) - CSTR with injection
-    m.fs.R4 = AerationTank(
-        property_package=m.fs.props, reaction_package=m.fs.rxn_props
-    )
-    # Fifth reactor (anoxic) 
-    m.fs.R5 = CSTR(
-        property_package=m.fs.props, reaction_package=m.fs.rxn_props
-    )
+    m.fs.R4 = AerationTank(property_package=m.fs.props, reaction_package=m.fs.rxn_props)
+    # Fifth reactor (anoxic)
+    m.fs.R5 = CSTR(property_package=m.fs.props, reaction_package=m.fs.rxn_props)
     m.fs.S1 = Separator(
-        property_package=m.fs.props, outlet_list=["effluent","M1_inlet", "R2_inlet"]
+        property_package=m.fs.props, outlet_list=["effluent", "M1_inlet", "R2_inlet"]
     )
 
-    
     # Product Blocks
     m.fs.Treated = Product(property_package=m.fs.props)
-
 
     # Link units
     m.fs.feed_to_m1 = Arc(source=m.fs.feed.outlet, destination=m.fs.M1.feed)
@@ -139,11 +137,11 @@ def build_flowsheet():
     m.fs.s1_to_r2 = Arc(source=m.fs.S1.R2_inlet, destination=m.fs.R2.inlet)
 
     pyo.TransformationFactory("network.expand_arcs").apply_to(m)
-    
+
     return m
 
-def set_operating_conditions(m):
-    # Feed Water Conditions
+
+def set_asm1_inlet_conditions(m):
     m.fs.feed.flow_vol.fix(18446 * pyo.units.m**3 / pyo.units.day)
     m.fs.feed.temperature.fix(298.15 * pyo.units.K)
     m.fs.feed.pressure.fix(1 * pyo.units.atm)
@@ -160,6 +158,55 @@ def set_operating_conditions(m):
     m.fs.feed.conc_mass_comp[0, "S_ND"].fix(6.95 * pyo.units.g / pyo.units.m**3)
     m.fs.feed.conc_mass_comp[0, "X_ND"].fix(10.59 * pyo.units.g / pyo.units.m**3)
     m.fs.feed.alkalinity.fix(7 * pyo.units.mol / pyo.units.m**3)
+
+
+def set_asm3_inlet_conditions(m):
+    m.fs.feed.flow_vol.fix(92230 * pyo.units.m**3 / pyo.units.day)
+    m.fs.feed.temperature.fix(288.15 * pyo.units.K)
+    m.fs.feed.pressure.fix(1 * pyo.units.atm)
+    m.fs.feed.conc_mass_comp[0, "S_O"].fix(
+        0.0333140769653528 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "S_I"].fix(30 * pyo.units.g / pyo.units.m**3)
+    m.fs.feed.conc_mass_comp[0, "S_S"].fix(
+        1.79253833233150 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "S_NH4"].fix(
+        7.47840572528914 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "S_N2"].fix(
+        25.0222401125193 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "S_NOX"].fix(
+        4.49343937121928 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.alkalinity.fix(4.95892616814772 * pyo.units.mol / pyo.units.m**3)
+    m.fs.feed.conc_mass_comp[0, "X_I"].fix(
+        1460.88032984731 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "X_S"].fix(
+        239.049918909639 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "X_H"].fix(
+        1624.51533042293 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "X_STO"].fix(
+        316.937373308996 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "X_A"].fix(
+        130.798830163795 * pyo.units.g / pyo.units.m**3
+    )
+    m.fs.feed.conc_mass_comp[0, "X_TSS"].fix(
+        3044.89285508125 * pyo.units.g / pyo.units.m**3
+    )
+
+
+def set_operating_conditions(m, asm_model=ASMModel.asm1):
+    # Feed Water Conditions
+    if asm_model == ASMModel.asm1:
+        set_asm1_inlet_conditions(m)
+    elif asm_model == ASMModel.asm3:
+        set_asm3_inlet_conditions(m)
 
     # Reactor sizing
     m.fs.R1.volume.fix(1000 * pyo.units.m**3)
@@ -184,10 +231,10 @@ def set_operating_conditions(m):
     # Set fraction of outflow from reactor 5 that goes to effluent
     m.fs.S1.split_fraction[:, "effluent"].fix(0.4)
 
-
     # Check degrees of freedom
     print("DOF = ", degrees_of_freedom(m))
     assert degrees_of_freedom(m) == 0
+
 
 def scale_flowsheet(m):
     # Apply scaling
@@ -202,12 +249,13 @@ def scale_flowsheet(m):
             iscale.set_scaling_factor(var, 1e2)
     iscale.calculate_scaling_factors(m.fs)
 
-def init_and_propagate(blk,arc=None,source=None,destination=None):
+
+def init_and_propagate(blk, arc=None, source=None, destination=None):
     blk.initialize()
     if arc is not None:
         propagate_state(arc)
     elif source is not None:
-        propagate_state(source=source,destination=destination)
+        propagate_state(source=source, destination=destination)
 
 
 def initialize_flowsheet(m):
@@ -227,7 +275,7 @@ def initialize_flowsheet(m):
     m.fs.M2.initialize()
     propagate_state(m.fs.m2_to_r3)
 
-    m.fs.R3.initialize() 
+    m.fs.R3.initialize()
     propagate_state(m.fs.r3_to_r4)
 
     m.fs.R4.initialize()
@@ -253,7 +301,7 @@ def initialize_flowsheet(m):
     m.fs.M2.initialize()
     propagate_state(m.fs.m2_to_r3)
 
-    m.fs.R3.initialize() 
+    m.fs.R3.initialize()
     propagate_state(m.fs.r3_to_r4)
 
     m.fs.R4.initialize()
@@ -266,11 +314,10 @@ def initialize_flowsheet(m):
     propagate_state(m.fs.s1_to_effluent)
     propagate_state(m.fs.s1_to_m1)
     propagate_state(m.fs.s1_to_r2)
-    
+
     m.fs.Treated.initialize()
 
     interval_initializer(m)
-
 
     # Apply sequential decomposition - 1 iteration should suffice
     seq = SequentialDecomposition()
@@ -288,11 +335,11 @@ def initialize_flowsheet(m):
     # for o in order:
     #     print(o[0].name)
 
-  
     def function(unit):
         unit.initialize(outlvl=idaeslog.DEBUG)
 
     seq.run(m, function)
+
 
 def solve_flowsheet(m):
     # Solve overall flowsheet to close recycle loop
@@ -307,8 +354,8 @@ if __name__ == "__main__":
     # This method builds and runs a steady state activated sludge
     # flowsheet.
     # m, results = build_flowsheet()
-    m = build_flowsheet()
-    set_operating_conditions(m)
+    m = build_flowsheet(asm_model=ASMModel.asm3)
+    set_operating_conditions(m, asm_model=ASMModel.asm3)
     scale_flowsheet(m)
 
     initialize_flowsheet(m)
@@ -316,7 +363,6 @@ if __name__ == "__main__":
     scale_flowsheet(m)
 
     m, res = solve_flowsheet(m)
-
 
     stream_table = create_stream_table_dataframe(
         {
@@ -330,8 +376,7 @@ if __name__ == "__main__":
             "R5": m.fs.R5.outlet,
             "S1 to M1": m.fs.S1.M1_inlet,
             "S1 to R2": m.fs.S1.R2_inlet,
-            "Effluent": m.fs.Treated.inlet
-
+            "Effluent": m.fs.Treated.inlet,
         },
         time_point=0,
     )
