c52d6611f85c2af37df75679f335ac17f1a241f6
Adam Atia <aatia@keylogic.com>
2025-07-31 12:53:56 -0400

    Remove kla from bsm2 (#1603)
    * remove Kla and constraints from flowsheet since this is an underlying modeling error (they exist on aerationtank units)
    
    * get rid of mixer equality config
    
    * run black and return scaled_model
    
    * bring back testing on unscaled model
    
    * remove mixer deactivations
    
    * test scaled model condition number
    
    * blk
    
    * incorporate fail_flag arg into interval_initializer
    
    * blk
    
    * accept kurby's suggestions
    
    * attempt to fix linux tests apparently with  removal of ipopt-watertap specification during initialization
    
    * blk
    
    * tests for Windows and Linux to check condition number on scaled biop=True model; add logger to BSM2; momentummixingtype=none for mixers on BSM2;fix mixer outlet pressures
    
    * scale constraint that is highest on list on extreme jac reports from diagnostictoolbox

--- watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension_ui.py
+++ watertap/flowsheets/full_water_resource_recovery_facility/BSM2_P_extension_ui.py
@@ -3872,17 +3872,7 @@ def build_flowsheet(build_options=None, **kwargs):
 
         set_operating_conditions(m, bio_P=bioP)
 
-        for mx in m.fs.mixers:
-            mx.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 3].deactivate()
-
         initialize_system(m, bio_P=bioP)
-
-        for mx in m.fs.mixers:
-            mx.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 3].deactivate()
 
         add_costing(m)
         m.fs.costing.initialize()
@@ -3911,17 +3901,7 @@ def build_flowsheet(build_options=None, **kwargs):
 
         set_operating_conditions(m, bio_P=False)
 
-        for mx in m.fs.mixers:
-            mx.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 3].deactivate()
-
         initialize_system(m, bio_P=False)
-
-        for mx in m.fs.mixers:
-            mx.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 2].deactivate()
-        m.fs.MX3.pressure_equality_constraints[0.0, 3].deactivate()
 
         add_costing(m)
         m.fs.costing.initialize()
