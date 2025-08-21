057b4ce80db542175a5531a4f5d8e655bad0bfaa
Adam Atia <aatia@keylogic.com>
2025-08-21 10:32:49 -0400

    limit pyomo version and check tests

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         # allow X.Y.Z stable release(s) and X.Y+1.dev0 (i.e. the main branch after X.Y.Z)
         # disallow X.Y+1.0rc0 (i.e. forcing a manual update to this requirement)
         "idaes-pse >=2.8.0,<2.9.0rc0",
-        "pyomo>=6.6.1",
+        "pyomo>=6.6.1,<6.9.3",
         "watertap-solvers",
         "pyyaml",  # watertap.core.wt_database
         # for parameter_sweep
