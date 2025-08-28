8fadd559ec5a2d5414f009ff29dfc65670d7cd66
kurbansitterley <kurban.sitterley@nrel.gov>
2025-08-27 19:21:09 -0600

    final clark model

--- watertap/unit_models/ion_exchange/ion_exchange_clark.py
+++ watertap/unit_models/ion_exchange/ion_exchange_clark.py
@@ -1,5 +1,5 @@
 #################################################################################
-# WaterTAP Copyright (c) 2020-2024, The Regents of the University of California,
+# WaterTAP Copyright (c) 2020-2025, The Regents of the University of California,
 # through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
 # National Renewable Energy Laboratory, and National Energy Technology
 # Laboratory (subject to receipt of any required approvals from the U.S. Dept.
@@ -10,27 +10,26 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 
-from copy import deepcopy
-
 # Import Pyomo libraries
 from pyomo.environ import (
     Set,
     Var,
-    Param,
-    value,
     log,
+    check_optimal_termination,
     units as pyunits,
 )
 
 # Import IDAES cores
 from idaes.core import declare_process_block_class
-
 import idaes.core.util.scaling as iscale
-
+import idaes.logger as idaeslog
 from idaes.core.util.exceptions import InitializationError, ConfigurationError
+
+from watertap.core.solvers import get_solver
 from watertap.unit_models.ion_exchange.ion_exchange_base import (
     IonExchangeBaseData,
     IonExchangeType,
+    add_ss_approximation,
 )
 
 __author__ = "Kurban Sitterley"
@@ -55,7 +54,7 @@ class IonExchangeClarkData(IonExchangeBaseData):
 
         if len(self.target_component_set) > 1:
             raise ConfigurationError(
-                f"IonExchange0D can only accept a single target ion but {len(self.target_component_set)} were provided."
+                f"IonExchangeClark can only accept a single target ion but {len(self.target_component_set)} were provided."
             )
         if self.config.property_package.charge_comp[target_component].value > 0:
             self.ion_exchange_type = IonExchangeType.cation
@@ -66,31 +65,12 @@ class IonExchangeClarkData(IonExchangeBaseData):
 
         for j in inerts:
             self.process_flow.mass_transfer_term[:, "Liq", j].fix(0)
-            regen.get_material_flow_terms("Liq", j).fix(0)
+            if j != "H2O":
+                regen.get_material_flow_terms("Liq", j).fix(0)
 
-        self.num_traps = 5  # TODO: make CONFIG option
-        self.trap_disc = range(self.num_traps + 1)
-        self.trap_index = self.trap_disc[1:]
-
-        self.c_trap_min = Param(  # TODO: make CONFIG option
-            initialize=0.01,
-            mutable=True,
-            doc="Minimum relative breakthrough concentration for estimating area under curve",
-        )
-
-        self.N_Sc = Var(
-            self.target_component_set,
-            initialize=700,
-            units=pyunits.dimensionless,
-            doc="Schmidt number",
-        )
-
-        self.N_Sh = Var(
-            self.target_component_set,
-            initialize=30,
-            units=pyunits.dimensionless,
-            doc="Sherwood number",
-        )
+        self.flow_basis = self.process_flow.properties_in[
+            self.flowsheet().config.time.first()
+        ].get_material_flow_basis()
 
         self.c_norm = Var(
             self.target_component_set,
@@ -98,41 +78,6 @@ class IonExchangeClarkData(IonExchangeBaseData):
             bounds=(0, 1),
             units=pyunits.dimensionless,
             doc="Dimensionless (relative) concentration [Ct/C0] of target ion",
-        )
-
-        self.c_traps = Var(
-            self.trap_disc,
-            initialize=0.5,
-            bounds=(0, 1),
-            units=pyunits.dimensionless,
-            doc="Normalized breakthrough concentrations for estimating area under breakthrough curve",
-        )
-
-        self.tb_traps = Var(
-            self.trap_disc,
-            initialize=1e6,
-            bounds=(0, None),
-            units=pyunits.second,
-            doc="Breakthrough times for estimating area under breakthrough curve",
-        )
-
-        self.traps = Var(
-            self.trap_index,
-            initialize=0.01,
-            bounds=(0, 1),
-            units=pyunits.dimensionless,
-            doc="Trapezoid areas for estimating area under breakthrough curve",
-        )
-
-        self.c_traps[0].fix(0)
-        self.tb_traps[0].fix(0)
-
-        self.c_norm_avg = Var(
-            self.target_component_set,
-            initialize=0.25,
-            bounds=(0, 2),
-            units=pyunits.dimensionless,
-            doc="Sum of trapezoid areas",
         )
 
         self.freundlich_n = Var(
@@ -156,36 +101,6 @@ class IonExchangeClarkData(IonExchangeBaseData):
             doc="Bed volumes of feed at 50 percent breakthrough",
         )
 
-        @self.Expression(self.target_component_set, doc="Breakthrough concentration")
-        def c_breakthru(b, j):
-            return b.c_norm[j] * prop_in.conc_mass_phase_comp["Liq", j]
-
-        @self.Constraint(self.target_component_set, doc="Schmidt number")
-        def eq_Sc(b, j):  # Eq. 3.359, Inglezakis + Poulopoulos
-            return (
-                b.N_Sc[j]
-                == prop_in.visc_k_phase["Liq"] / prop_in.diffus_phase_comp["Liq", j]
-            )
-
-        @self.Constraint(self.target_component_set, doc="Sherwood number")
-        def eq_Sh(b, j):  # Eq. 3.346, Inglezakis + Poulopoulos
-            return (
-                b.N_Sh[j]
-                == b.Sh_A
-                * b.bed_porosity**b.Sh_exp_A
-                * b.N_Re**b.Sh_exp_B
-                * b.N_Sc[j] ** b.Sh_exp_C
-            )
-
-        @self.Constraint(
-            self.target_component_set, doc="Mass transfer for regeneration stream"
-        )
-        def eq_mass_transfer_regen(b, j):
-            return (
-                regen.get_material_flow_terms("Liq", j)
-                == -b.process_flow.mass_transfer_term[0, "Liq", j]
-            )
-
         @self.Constraint(doc="Bed volumes at breakthrough")
         def eq_bv(b):
             return b.breakthrough_time * b.loading_rate == b.bv * b.bed_depth
@@ -205,56 +120,117 @@ class IonExchangeClarkData(IonExchangeBaseData):
             )
             return left_side - right_side == 0
 
-        @self.Constraint(
-            self.target_component_set,
-            self.trap_index,
-            doc="Evenly spaced c_norm for trapezoids",
-        )
-        def eq_c_traps(b, j, k):
-            return b.c_traps[k] == b.c_trap_min + (b.trap_disc[k] - 1) * (
-                (b.c_norm[j] - b.c_trap_min) / (b.num_traps - 1)
+        @self.Constraint(doc="Total bed volume required based on volumetric flow")
+        def eq_bed_volume_required(b):
+            return b.bed_volume == pyunits.convert(
+                b.flow_per_column * b.ebct, to_units=pyunits.m**3
             )
 
-        @self.Constraint(
-            self.trap_index,
-            doc="Breakthru time calc for trapezoids",
-        )
-        def eq_tb_traps(b, k):
-            bv_traps = (b.tb_traps[k] * b.loading_rate) / b.bed_depth
-            left_side = (
-                (b.mass_transfer_coeff * b.bed_depth * (b.freundlich_n - 1))
-                / (b.bv_50 * b.loading_rate)
-            ) * (b.bv_50 - bv_traps)
+        if self.config.add_steady_state_approximation:
+            add_ss_approximation(self, ix_model_type="clark")
+        else:
+            # If not using steady state approximation, need mass transfer terms;
+            # assume effluent concentration is equal to concentration at breakthrough
 
-            right_side = log(
-                ((1 / b.c_traps[k]) ** (b.freundlich_n - 1) - 1)
-                / (2 ** (b.freundlich_n - 1) - 1)
+            @self.Constraint(
+                self.target_component_set,
+                doc="CV mass transfer term",
             )
-            return left_side - right_side == 0
+            def eq_mass_transfer_term(b, j):
+                return (1 - b.c_norm[j]) * prop_in.get_material_flow_terms(
+                    "Liq", j
+                ) == -b.process_flow.mass_transfer_term[0, "Liq", j]
 
-        @self.Constraint(self.trap_index, doc="Area of trapezoids")
-        def eq_traps(b, k):
-            return b.traps[k] == (b.tb_traps[k] - b.tb_traps[k - 1]) / b.tb_traps[
-                self.num_traps
-            ] * ((b.c_traps[k] + b.c_traps[k - 1]) / 2)
+            @self.Constraint(
+                self.target_component_set,
+                doc="Target component mass transfer for regeneration stream",
+            )
+            def eq_mass_transfer_regen(b, j):
+                return (
+                    regen.get_material_flow_terms("Liq", j)
+                    == -b.process_flow.mass_transfer_term[0, "Liq", j]
+                )
 
-        @self.Constraint(
-            self.target_component_set, doc="Average relative effluent concentration"
+            @self.Constraint(doc="Regeneration stream volumetric flow rate")
+            def eq_regen_flow_rate(b):
+                return regen.flow_vol_phase["Liq"] == pyunits.convert(
+                    b.rinse_flow_rate * (b.rinse_time / b.cycle_time)
+                    + b.backwash_flow_rate * (b.backwash_time / b.cycle_time)
+                    + b.regen_flow_rate * (b.regeneration_time / b.cycle_time),
+                    to_units=pyunits.m**3 / pyunits.s,
+                )
+
+    def initialize_build(
+        self,
+        state_args=None,
+        outlvl=idaeslog.NOTSET,
+        solver=None,
+        optarg=None,
+    ):
+        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="unit")
+        solve_log = idaeslog.getSolveLogger(self.name, outlvl, tag="unit")
+
+        opt = get_solver(solver, optarg)
+
+        flags = self.process_flow.initialize(
+            outlvl=outlvl,
+            optarg=optarg,
+            solver=solver,
+            state_args=state_args,
+            hold_state=True,
         )
-        def eq_c_norm_avg(b, j):
-            return b.c_norm_avg[j] == sum(b.traps[k] for k in b.trap_index)
+        init_log.info("Initialization Step 1a Complete.")
 
-        @self.Constraint(
-            self.target_component_set,
-            doc="CV mass transfer term",
+        # Initialize other state blocks
+        # Set state_args from inlet state
+        if state_args is None:
+            self.state_args = state_args = {}
+            state_dict = self.process_flow.properties_in[
+                self.flowsheet().config.time.first()
+            ].define_port_members()
+
+            for k in state_dict.keys():
+                if state_dict[k].is_indexed():
+                    state_args[k] = {}
+                    for m in state_dict[k].keys():
+                        state_args[k][m] = state_dict[k][m].value
+                else:
+                    state_args[k] = state_dict[k].value
+
+        self.regeneration_stream.initialize(
+            outlvl=outlvl,
+            optarg=optarg,
+            solver=solver,
+            state_args=state_args,
         )
-        def eq_mass_transfer_term(b, j):
-            return (1 - b.c_norm_avg[j]) * prop_in.get_material_flow_terms(
-                "Liq", j
-            ) == -b.process_flow.mass_transfer_term[0, "Liq", j]
+
+        init_log.info("Initialization Step 1b Complete.")
+        # interval_initializer(self)
+
+        # Solve unit
+        with idaeslog.solver_log(solve_log, idaeslog.DEBUG) as slc:
+            res = opt.solve(self, tee=slc.tee)
+            if not check_optimal_termination(res):
+                init_log.warning(
+                    f"Trouble solving unit model {self.name}, trying one more time"
+                )
+                res = opt.solve(self, tee=slc.tee)
+
+        init_log.info("Initialization Step 2 {}.".format(idaeslog.condition(res)))
+
+        # Release Inlet state
+        self.process_flow.properties_in.release_state(flags, outlvl=outlvl)
+        init_log.info("Initialization Complete: {}".format(idaeslog.condition(res)))
+
+        if not check_optimal_termination(res):
+            raise InitializationError(f"Unit model {self.name} failed to initialize.")
 
     def calculate_scaling_factors(self):
         super().calculate_scaling_factors()
+        target_component = self.config.target_component
+
+        if iscale.get_scaling_factor(self.c_norm[target_component]) is None:
+            iscale.set_scaling_factor(self.c_norm[target_component], 10)
 
         if iscale.get_scaling_factor(self.freundlich_n) is None:
             iscale.set_scaling_factor(self.freundlich_n, 0.1)
@@ -264,30 +240,3 @@ class IonExchangeClarkData(IonExchangeBaseData):
 
         if iscale.get_scaling_factor(self.bv_50) is None:
             iscale.set_scaling_factor(self.bv_50, 1e-5)
-
-        if iscale.get_scaling_factor(self.tb_traps) is None:
-            sf = iscale.get_scaling_factor(self.breakthrough_time)
-            iscale.set_scaling_factor(self.tb_traps, sf)
-
-        if iscale.get_scaling_factor(self.c_traps) is None:
-            iscale.set_scaling_factor(self.c_traps, 1)
-
-        if iscale.get_scaling_factor(self.traps) is None:
-            iscale.set_scaling_factor(self.traps, 1e3)
-
-        if iscale.get_scaling_factor(self.c_norm_avg) is None:
-            iscale.set_scaling_factor(self.c_norm_avg, 1e2)
-
-        for ind, c in self.eq_clark.items():
-            if iscale.get_scaling_factor(c) is None:
-                iscale.constraint_scaling_transform(c, 1e-2)
-
-        for ind, c in self.eq_traps.items():
-            if iscale.get_scaling_factor(c) is None:
-                iscale.constraint_scaling_transform(c, 1e2)
-
-        # elif self.config.isotherm == IsothermType.freundlich:
-        #     var_dict[f"BV at Breakthrough"] = self.bv
-        #     var_dict[f"BV at 50% Breakthrough"] = self.bv_50
-        #     var_dict[f"Freundlich n"] = self.freundlich_n
-        #     var_dict[f"Clark Mass Transfer Coeff."] = self.mass_transfer_coeff
