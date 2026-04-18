890d246d1300d21933d68d1a500a9089858c3055
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2026-04-18 15:20:35 -0400

    add costing steam utility

--- watertap/costing/util.py
+++ watertap/costing/util.py
@@ -239,3 +239,121 @@ def cost_by_flow_volume(blk, flow_cost, flow_to_cost):
             blk.flow_cost * flow_to_cost, to_units=blk.costing_package.base_currency
         )
     )
+
+
+def cost_steam_flow(
+    costing_package,
+    steam_cost_type="steam",
+    steam_volume_flow=None,
+    steam_heat_duty=None,
+    steam_mass_flow=None,
+    steam_pressure=None,
+):
+    """
+    Generic function for costing a steam flow.
+
+    Args:
+        steam_cost_type: name of the steam cost to use for costing flow, if not available will be created
+        steam_volume_flow: the steam flow to cost in [volume]/[time]
+        steam_mass_flow (optional): the steam flow to cost in [mass]/[time]
+        steam_pressure (needed if mass flow is provided): the pressure of the steam flow in [pressure]
+    """
+    if steam_volume_flow is None:
+        if steam_mass_flow is not None and steam_heat_duty is not None:
+            raise ValueError(
+                "If steam_volume_flow is not provided, either steam mass_flow or steam_heat_duty should be provided"
+            )
+        assert (
+            steam_pressure is not None
+        ), "steam pressure must be provided if steam mass flow or heat duty is provided"
+        dh_vap, sp_vol = _compute_steam_properties(steam_pressure)
+        if steam_mass_flow is not None:
+            steam_volume_flow = steam_mass_flow * sp_vol
+        elif steam_heat_duty is not None:
+            steam_volume_flow = steam_heat_duty / dh_vap * sp_vol
+
+    if costing_package.component(steam_cost_type) is None:
+        costing_package.add_component(
+            steam_cost_type,
+            pyo.Var(initialize=0.004, units=pyo.units.USD_2018 / (pyo.units.meter**3)),
+        )
+        costing_package.find_component(steam_cost_type).fix()
+        costing_package.register_flow_type(
+            steam_cost_type, costing_package.find_component(steam_cost_type)
+        )
+    costing_package.cost_flow(
+        pyo.units.convert(
+            (steam_volume_flow),
+            to_units=pyo.units.m**3 / pyo.units.s,
+        ),
+        steam_cost_type,
+    )
+
+
+def _compute_steam_properties(steam_pressure):
+    """
+    Function for computing saturated steam properties for thermal heating estimation.
+
+    Args:
+        steam_pressure:   Steam gauge pressure in bar
+
+    Out:
+        Steam thermal capacity (latent heat of condensation * density) in kJ/m3
+    """
+    pressure_sat = steam_pressure
+    # 1. Compute saturation temperature of steam: computed from El-Dessouky expression
+    tsat_constants = [
+        42.6776 * pyo.units.K,
+        -3892.7 * pyo.units.K,
+        1000 * pyo.units.kPa,
+        -9.48654 * pyo.units.dimensionless,
+    ]
+    psat = (
+        pyo.units.convert(pressure_sat, to_units=pyo.units.kPa)
+        + 101.325 * pyo.units.kPa
+    )
+    temperature_sat = tsat_constants[0] + tsat_constants[1] / (
+        pyo.log(psat / tsat_constants[2]) + tsat_constants[3]
+    )
+
+    # 2. Compute latent heat of condensation/vaporization: computed from Sharqawy expression
+    t = temperature_sat - 273.15 * pyo.units.K
+    enth_mass_units = pyo.units.J / pyo.units.kg
+    t_inv_units = pyo.units.K**-1
+    dh_constants = [
+        2.501e6 * enth_mass_units,
+        -2.369e3 * enth_mass_units * t_inv_units**1,
+        2.678e-1 * enth_mass_units * t_inv_units**2,
+        -8.103e-3 * enth_mass_units * t_inv_units**3,
+        -2.079e-5 * enth_mass_units * t_inv_units**4,
+    ]
+    dh_vap = (
+        dh_constants[0]
+        + dh_constants[1] * t
+        + dh_constants[2] * t**2
+        + dh_constants[3] * t**3
+        + dh_constants[4] * t**4
+    )
+    dh_vap = pyo.units.convert(dh_vap, to_units=pyo.units.kJ / pyo.units.kg)
+
+    # 3. Compute specific volume: computed from Affandi expression (Eq 5)
+    t_critical = 647.096 * pyo.units.K
+    t_red = temperature_sat / t_critical  # Reduced temperature
+    sp_vol_constants = [
+        -7.75883 * pyo.units.dimensionless,
+        3.23753 * pyo.units.dimensionless,
+        2.05755 * pyo.units.dimensionless,
+        -0.06052 * pyo.units.dimensionless,
+        0.00529 * pyo.units.dimensionless,
+    ]
+    log_sp_vol = (
+        sp_vol_constants[0]
+        + sp_vol_constants[1] * (pyo.log(1 / t_red)) ** 0.4
+        + sp_vol_constants[2] / (t_red**2)
+        + sp_vol_constants[3] / (t_red**4)
+        + sp_vol_constants[4] / (t_red**5)
+    )
+    sp_vol = pyo.exp(log_sp_vol) * pyo.units.m**3 / pyo.units.kg
+
+    # 4. Return specific energy: density and density
+    return dh_vap, sp_vol
