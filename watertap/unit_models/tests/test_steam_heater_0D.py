971774a827410e0243e1cb793d78abf371156805
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2026-04-18 14:57:13 -0400

    Revert "fixed costing for consistencfy"
    This reverts commit dcd914a5bbe80f27b9d6e847201c3e50706eabef.

--- watertap/unit_models/tests/test_steam_heater_0D.py
+++ watertap/unit_models/tests/test_steam_heater_0D.py
@@ -15,7 +15,6 @@ from pyomo.environ import (
 )
 
 from idaes.core import FlowsheetBlock
-from watertap.costing import WaterTAPCosting
 import watertap.property_models.water_prop_pack as props_w
 import watertap.property_models.seawater_prop_pack as props_sw
 from watertap.unit_models.steam_heater_0D import SteamHeater0D
@@ -30,16 +29,15 @@ import idaes.core.util.scaling as iscale
 import pytest
 from watertap.unit_models.tests.unit_test_harness import UnitTestHarness
 
-from idaes.core import UnitModelCostingBlock
-
 solver = get_solver()
 
 
-def build(estimate_cooling_water=False, test_with_steam_cost=False):
+def build(estimate_cooling_water=False):
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
     m.fs.steam_properties = props_w.WaterParameterBlock()
     m.fs.cold_side_properties = props_sw.SeawaterParameterBlock()
+
     m.fs.unit = SteamHeater0D(
         hot_side_name="hot",
         cold_side_name="cold",
@@ -54,13 +52,6 @@ def build(estimate_cooling_water=False, test_with_steam_cost=False):
         estimate_cooling_water=estimate_cooling_water,
     )
 
-    if test_with_steam_cost:
-        m.fs.costing = WaterTAPCosting()
-        m.fs.unit.costing = UnitModelCostingBlock(
-            flowsheet_costing_block=m.fs.costing,
-            costing_method_arguments={"cost_steam_flow": True},
-        )
-        m.fs.costing.cost_process()
     m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"].fix(0.5)
     m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].fix(0)
     m.fs.unit.hot_side_inlet.temperature.fix(273.15 + 140)
@@ -97,39 +88,6 @@ class TestSteamHeater0D(UnitTestHarness):
             329.521210682437
         )
 
-        self.conservation_equality = {
-            "Check 1": {
-                "in": m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
-                + m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-                + m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-                + m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"],
-                "out": m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Vap", "H2O"]
-                + m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-                + m.fs.unit.cold_side_outlet.flow_mass_phase_comp[0, "Liq", "TDS"]
-                + m.fs.unit.cold_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"],
-            },
-        }
-        return m
-
-
-@pytest.mark.requires_idaes_solver
-class TestSteamHeater0D_with_costing(UnitTestHarness):
-    def configure(self):
-        m = build(test_with_steam_cost=True)
-        m.fs.costing.initialize()
-        self.unit_solutions[
-            m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
-        ] = 0.5
-        self.unit_solutions[
-            m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-        ] = 0.5
-        self.unit_solutions[m.fs.unit.hot_side_outlet.temperature[0]] = 341.356646567839
-        self.unit_solutions[m.fs.unit.cold_side_outlet.temperature[0]] = (
-            329.521210682437
-        )
-        # just checks these are properly constructed
-        self.unit_solutions[m.fs.costing.steam_cost] = 0.004
-        self.unit_solutions[m.fs.costing.aggregate_flow_steam] = 0.299398766101876
         self.conservation_equality = {
             "Check 1": {
                 "in": m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
