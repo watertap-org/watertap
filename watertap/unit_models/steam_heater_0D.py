c483ea5eba3dfc4d0f4342e4d2322a2ab65de304
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2026-04-23 21:03:32 -0400

    Update test_steam_heater_0D.py

--- watertap/unit_models/steam_heater_0D.py
+++ watertap/unit_models/steam_heater_0D.py
@@ -11,120 +11,113 @@
 #################################################################################
 
 
-from idaes.core import (
-    declare_process_block_class,
-)
+from pyomo.environ import NonNegativeReals, Var, units as pyunits
+
+from idaes.core import declare_process_block_class
+from idaes.core.util.model_serializer import to_json, from_json, StoreSpec
 from idaes.models.unit_models.heat_exchanger import HeatExchangerData
-from watertap.core.solvers import get_solver
+import idaes.core.util.scaling as iscale
 import idaes.logger as idaeslog
-from watertap.costing.unit_models.heat_exchanger import (
-    cost_heat_exchanger,
-)
-from pyomo.common.config import ConfigValue
 
-from watertap.core.util.initialization import interval_initializer
+from watertap.core.solvers import get_solver
+from watertap.costing.unit_models.heat_exchanger import cost_heat_exchanger
 
-__author__ = "Elmira Shamlou, Alexander V. Dudchenko"
-
-"""
-This unit model uses is based on the IDAES `feedwater_heater_0D` model.
-However, the constraints and properties defined in the IDAES unit model do not align with those in the available WaterTAP property packages.
-To address this, alternative constraints have been replaced to ensure full condensation based on the WaterTAP properties. 
-In addition, a condenser option and its corresponding initialization routine have been added, allowing the user to switch between the condenser and steam heater models.
-Note that additional components like desuperheaters, drain mixers, and coolers are not included. If necessary, these can be modeled separately by adding heat exchangers and mixers to the flowsheet.
-To do: Consider incorporating as a modification to IDAES' FeedWaterHeater model directly in the IDAES repo"
-"""
+_log = idaeslog.getLogger(__name__)
 
 
-@declare_process_block_class(
-    "SteamHeater0D",
-    doc="""Feedwater Heater Condensing Section
-The feedwater heater condensing section model is a normal 0D heat exchanger
-model with an added constraint to calculate the steam flow such that the outlet
-of shell is a saturated liquid.""",
-)
+__author__ = "Elmira Shamlou"
+
+
+@declare_process_block_class("SteamHeater0D")
 class SteamHeater0DData(HeatExchangerData):
     CONFIG = HeatExchangerData.CONFIG()
-    CONFIG.declare(
-        "estimate_cooling_water",
-        ConfigValue(
-            default=False,
-            domain=bool,
-            description="Estimate cooling water flow rate for condenser mode",
-        ),
-    )
 
     def build(self):
         super().build()
+        self.pressure_deltaP = Var(
+            self.flowsheet().time,
+            initialize=0.0,
+            bounds=(0.0, None),
+            domain=NonNegativeReals,
+            units=pyunits.Pa,
+        )
+        self.pressure_deltaP.fix(0)
 
-        # Assume total condensation for the unit
-        self.set_total_condensation()
-
-        @self.Constraint(self.flowsheet().time, doc="Saturation pressure constraint")
+        @self.Constraint(self.flowsheet().time)
         def outlet_pressure_sat(b, t):
             return (
                 b.hot_side.properties_out[t].pressure
-                >= b.hot_side.properties_out[t].pressure_sat
+                == b.hot_side.properties_out[t].pressure_sat + b.pressure_deltaP[t]
             )
 
-    def set_total_condensation(self):
-        """Set the flow of the vapor phase to zero at the outlet,
-        ensuring total condensation."""
-        for j in self.hot_side.config.property_package.component_list:
-            lb = self.hot_side.properties_out[0].flow_mass_phase_comp["Vap", j].lb
-            self.hot_side.properties_out[0].flow_mass_phase_comp["Vap", j].fix(lb)
+        self._apply_total_condensation()
+
+    def _apply_total_condensation(self):
+        for t in self.flowsheet().time:
+            for j in self.hot_side.config.property_package.component_list:
+                v = self.hot_side.properties_out[t].flow_mass_phase_comp["Vap", j]
+                v.fix(v.lb)
+
+    def set_subcooling_margin(self, margin_Pa):
+        for t in self.flowsheet().time:
+            self.pressure_deltaP[t].fix(margin_Pa)
+
+    def release_subcooling_margin(self):
+        for t in self.flowsheet().time:
+            self.pressure_deltaP[t].unfix()
 
     def initialize_build(self, *args, **kwargs):
-        """
-        Initialization routine for both heater and condenser modes. For condenser mode with cooling water estimation, the initialization is performed based on a specified design temperature rise on the cold side.
-        """
         solver = kwargs.get("solver", None)
         optarg = kwargs.get("optarg", {})
-        state_args = kwargs.get("state_args", None)
         outlvl = kwargs.get("outlvl", idaeslog.NOTSET)
         init_log = idaeslog.getInitLogger(self.name, outlvl, tag="unit")
         solve_log = idaeslog.getSolveLogger(self.name, outlvl, tag="unit")
 
-        # initialize inlet states (This might be redundant)
-        # but we should do it for consistency with other unit models
-        # as well as interval initializer needs this.
-        hot_source_flags = self.hot_side.initialize(
-            state_args=state_args,
-            outlvl=outlvl,
-            optarg=optarg,
-            solver=solver,
+        flags = to_json(
+            self,
+            return_dict=True,
+            wts=StoreSpec.value_isfixed_isactive(only_fixed=True),
         )
 
-        cold_source_flags = self.cold_side.initialize(
-            state_args=state_args,
-            outlvl=outlvl,
-            optarg=optarg,
-            solver=solver,
-        )
-        # pre-solve using interval arithmetic
-        if self.config.estimate_cooling_water:
-            cold_side_outlet_temperature = self.cold_side_outlet.temperature[0].value
-            self.cold_side_outlet.temperature[0].unfix()
+        self.hot_side_outlet.unfix()
+        self.cold_side_outlet.unfix()
 
-        interval_initializer(self)
+        for t in self.flowsheet().time:
+            for sb in (
+                self.hot_side.properties_in[t],
+                self.cold_side.properties_in[t],
+            ):
+                state_var_ids = set()
+                for _name, var in sb.define_state_vars().items():
+                    for idx in var:
+                        state_var_ids.add(id(var[idx]))
+                for v in sb.component_data_objects(Var, descend_into=False):
+                    if id(v) not in state_var_ids and v.fixed:
+                        v.unfix()
+
         super().initialize_build(*args, **kwargs)
-        if self.config.estimate_cooling_water:
-            self.cold_side_outlet.temperature[0].fix(cold_side_outlet_temperature)
-            self.cold_side.properties_in[0].mass_frac_phase_comp["Liq", "TDS"].fix()
-            for j in self.cold_side.config.property_package.component_list:
-                self.cold_side.properties_in[0].flow_mass_phase_comp["Liq", j].unfix()
-        opt = get_solver(solver, optarg)
 
+        from_json(
+            self,
+            sd=flags,
+            wts=StoreSpec.value_isfixed_isactive(only_fixed=True),
+        )
+
+        opt = get_solver(solver, optarg)
         with idaeslog.solver_log(solve_log, idaeslog.DEBUG) as slc:
             res = opt.solve(self, tee=slc.tee)
-        if self.config.estimate_cooling_water:
-            self.cold_side.properties_in[0].mass_frac_phase_comp["Liq", "TDS"].unfix()
-            self.cold_side.properties_in[0].flow_mass_phase_comp["Liq", "TDS"].fix()
+        init_log.info("Initialization Complete: {}".format(idaeslog.condition(res)))
 
-        init_log.info("Initialization Complete {}".format(idaeslog.condition(res)))
-
-        self.hot_side.release_state(hot_source_flags, outlvl)
-        self.cold_side.release_state(cold_source_flags, outlvl)
+    def calculate_scaling_factors(self):
+        super().calculate_scaling_factors()
+        for t in self.flowsheet().time:
+            sf_p = iscale.get_scaling_factor(
+                self.hot_side.properties_out[t].pressure, default=1e-5
+            )
+            if iscale.get_scaling_factor(self.pressure_deltaP[t]) is None:
+                iscale.set_scaling_factor(self.pressure_deltaP[t], sf_p)
+            if iscale.get_scaling_factor(self.outlet_pressure_sat[t]) is None:
+                iscale.constraint_scaling_transform(self.outlet_pressure_sat[t], sf_p)
 
     @property
     def default_costing_method(self):
--- watertap/unit_models/tests/test_steam_heater_0D.py
+++ watertap/unit_models/tests/test_steam_heater_0D.py
@@ -10,29 +10,27 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 
-from pyomo.environ import (
-    ConcreteModel,
-)
+import pytest
+
+from pyomo.environ import ConcreteModel
 
 from idaes.core import FlowsheetBlock
+from idaes.models.unit_models.heat_exchanger import HeatExchangerFlowPattern
+import idaes.core.util.scaling as iscale
+
 import watertap.property_models.water_prop_pack as props_w
 import watertap.property_models.seawater_prop_pack as props_sw
 from watertap.unit_models.steam_heater_0D import SteamHeater0D
-from watertap.core.solvers import get_solver
 from watertap.unit_models.mvc.components.lmtd_chen_callback import (
     delta_temperature_chen_callback,
 )
-from idaes.models.unit_models.heat_exchanger import (
-    HeatExchangerFlowPattern,
-)
-import idaes.core.util.scaling as iscale
-import pytest
+from watertap.core.solvers import get_solver
 from watertap.unit_models.tests.unit_test_harness import UnitTestHarness
 
 solver = get_solver()
 
 
-def build(estimate_cooling_water=False):
+def build():
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
     m.fs.steam_properties = props_w.WaterParameterBlock()
@@ -41,21 +39,17 @@ def build(estimate_cooling_water=False):
     m.fs.unit = SteamHeater0D(
         hot_side_name="hot",
         cold_side_name="cold",
-        hot={
-            "property_package": m.fs.steam_properties,
-        },
-        cold={
-            "property_package": m.fs.cold_side_properties,
-        },
+        hot={"property_package": m.fs.steam_properties},
+        cold={"property_package": m.fs.cold_side_properties},
         delta_temperature_callback=delta_temperature_chen_callback,
         flow_pattern=HeatExchangerFlowPattern.countercurrent,
-        estimate_cooling_water=estimate_cooling_water,
     )
 
-    m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"].fix(0.5)
+    m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"].set_value(1)
     m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].fix(0)
     m.fs.unit.hot_side_inlet.temperature.fix(273.15 + 140)
     m.fs.unit.hot_side_inlet.pressure[0].fix(201325)
+
     m.fs.unit.cold_side_inlet.pressure.fix(101325)
     m.fs.unit.cold_side_inlet.temperature.fix(273.15 + 25)
     total_flow_mass = 10
@@ -66,9 +60,12 @@ def build(estimate_cooling_water=False):
     m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].fix(
         (1 - TDS_mass_frac) * total_flow_mass
     )
+
     m.fs.unit.area.fix(10)
     m.fs.unit.overall_heat_transfer_coefficient.fix(2e3)
+
     iscale.calculate_scaling_factors(m.fs.unit)
+
     return m
 
 
@@ -79,13 +76,12 @@ class TestSteamHeater0D(UnitTestHarness):
 
         self.unit_solutions[
             m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
-        ] = 0.5
-        self.unit_solutions[
-            m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-        ] = 0.5
-        self.unit_solutions[m.fs.unit.hot_side_outlet.temperature[0]] = 341.356646567839
+        ] = 0.7474967732076081
+        self.unit_solutions[m.fs.unit.hot_side_outlet.temperature[0]] = (
+            393.5672256519623
+        )
         self.unit_solutions[m.fs.unit.cold_side_outlet.temperature[0]] = (
-            329.521210682437
+            340.9522230977077
         )
 
         self.conservation_equality = {
@@ -104,37 +100,80 @@ class TestSteamHeater0D(UnitTestHarness):
 
 
 @pytest.mark.requires_idaes_solver
-class TestCondenserwithEstimation(UnitTestHarness):
+class TestCondenserNoEstimation(UnitTestHarness):
     def configure(self):
-        m = build(estimate_cooling_water=True)
-        outlet_temperature = 340
-        m.fs.unit.cold_side_outlet.temperature[0].fix(outlet_temperature)
+        m = build()
         m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"].fix(0.5)
-        total_flow_mass = 12
-        TDS_mass_frac = 0.035
-        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].unfix()
-        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"].unfix()
-        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"].fix(
-            TDS_mass_frac * total_flow_mass
-        )
-        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].set_value(
-            (1 - TDS_mass_frac) * total_flow_mass
-        )
+        m.fs.unit.area.unfix()
 
         self.unit_solutions[m.fs.unit.hot_side_outlet.temperature[0]] = (
-            347.6197595677517
+            393.5672256519623
         )
+        self.unit_solutions[m.fs.unit.cold_side_outlet.temperature[0]] = (
+            326.68592002256395
+        )
+        self.unit_solutions[m.fs.unit.area] = 6.129593261211377
+        self.unit_solutions[
+            m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
+        ] = 0.5
+
+        self.conservation_equality = {
+            "Check 1": {
+                "in": m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
+                + m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
+                + m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
+                + m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"],
+                "out": m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Vap", "H2O"]
+                + m.fs.unit.hot_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"]
+                + m.fs.unit.cold_side_outlet.flow_mass_phase_comp[0, "Liq", "TDS"]
+                + m.fs.unit.cold_side_outlet.flow_mass_phase_comp[0, "Liq", "H2O"],
+            },
+        }
+
+        return m
+
+
+@pytest.mark.requires_idaes_solver
+class TestCondenserwithEstimation(UnitTestHarness):
+    def configure(self):
+        m = build()
+        m.fs.unit.area.unfix()
+        m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"].fix(0.5)
+
+        outlet_temperature = 340
+        m.fs.unit.cold_side_outlet.temperature[0].fix(outlet_temperature)
+
+        TDS_mass_frac = 0.035
+        total_flow_mass_guess = 10
+        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].unfix()
+        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"].unfix()
+        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"].set_value(
+            (1 - TDS_mass_frac) * total_flow_mass_guess
+        )
+        m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"].set_value(
+            TDS_mass_frac * total_flow_mass_guess
+        )
+        m.fs.unit.cold_side.properties_in[0].mass_frac_phase_comp["Liq", "TDS"].fix(
+            TDS_mass_frac
+        )
+
+        self.unit_solutions[
+            m.fs.unit.hot_side_inlet.flow_mass_phase_comp[0, "Vap", "H2O"]
+        ] = 0.5
         self.unit_solutions[m.fs.unit.cold_side_outlet.temperature[0]] = 340
-        self.unit_solutions[m.fs.unit.area] = 10
-        self.unit_solutions[
-            m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"]
-        ] = 0.26018868152521274
-        self.unit_solutions[
-            m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
-        ] = 7.173773647766579
+        self.unit_solutions[m.fs.unit.hot_side_outlet.temperature[0]] = (
+            393.5672256519623
+        )
         self.unit_solutions[
             m.fs.unit.cold_side.properties_in[0].mass_frac_phase_comp["Liq", "TDS"]
         ] = 0.035
+        self.unit_solutions[m.fs.unit.area] = 6.6472767942455455
+        self.unit_solutions[
+            m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "TDS"]
+        ] = 0.23938598366941452
+        self.unit_solutions[
+            m.fs.unit.cold_side_inlet.flow_mass_phase_comp[0, "Liq", "H2O"]
+        ] = 6.600213549742428
 
         self.conservation_equality = {
             "Check 1": {
