11393ab2ea8c838b0483dda52c78c71bec076b36
Adam Atia <aatia@keylogic.com>
2026-04-27 16:03:32 -0400

    revert mass and mole flow formulations

--- watertap/property_models/multicomp_aq_sol_prop_pack.py
+++ watertap/property_models/multicomp_aq_sol_prop_pack.py
@@ -1416,8 +1416,8 @@ class MCASStateBlockData(StateBlockData):
 
             def rule_flow_mol_phase_comp(b, p, j):
                 return (
-                    b.flow_mass_phase_comp[p, j]
-                    == b.flow_mol_phase_comp[p, j] * b.params.mw_comp[j]
+                    b.flow_mass_phase_comp[p, j] / b.params.mw_comp[j]
+                    == b.flow_mol_phase_comp[p, j] 
                 )
 
             self.eq_flow_mol_phase_comp = Constraint(
@@ -1439,8 +1439,8 @@ class MCASStateBlockData(StateBlockData):
 
             def rule_flow_mass_phase_comp(b, p, j):
                 return (
-                    b.flow_mass_phase_comp[p, j]
-                    == b.flow_mol_phase_comp[p, j] * b.params.mw_comp[j]
+                    b.flow_mass_phase_comp[p, j] / b.params.mw_comp[j]
+                    == b.flow_mol_phase_comp[p, j] 
                 )
 
             self.eq_flow_mass_phase_comp = Constraint(
