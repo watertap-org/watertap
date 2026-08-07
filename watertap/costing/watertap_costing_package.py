97304beb889af8a0430ab9818da5661f3dd6f5df
Carolina Tristan <ctristan@purdue.edu>
2026-08-07 15:02:34 -0400

    Adjust `flow_units` to remove `base_period` division for consistency

--- watertap/costing/watertap_costing_package.py
+++ watertap/costing/watertap_costing_package.py
@@ -408,15 +408,17 @@ class WaterTAPCostingBlockData(FlowsheetCostingBlockData):
         self._check_flow_basis_consistency(flow_rate, flow_basis)
 
         flow_units = {
-            "volumetric": pyo.units.m**3 / self.base_period,
-            "mass": pyo.units.kg / self.base_period,
-            "energy": pyo.units.kWh / self.base_period,
+            "volumetric": pyo.units.m**3,
+            "mass": pyo.units.kg,
+            "energy": pyo.units.kWh,
         }[flow_basis]
 
         self.add_component(
             name,
             pyo.Expression(
-                expr=pyo.units.convert(flow_rate, to_units=flow_units)
+                expr=pyo.units.convert(
+                    flow_rate, to_units=flow_units / self.base_period
+                )
                 * self.utilization_factor,
                 doc=f"Annual production based on flow {flow_rate.name}",
             ),
