85426833a70cbad0842008133e13730a713441d8
MarcusHolly <marcus.holly@keylogic.com>
2026-08-24 16:15:20 -0400

    Clean up BSM2 testing

--- watertap/flowsheets/full_water_resource_recovery_facility/test/test_full_WRRF_with_ASM1_ADM1.py
+++ watertap/flowsheets/full_water_resource_recovery_facility/test/test_full_WRRF_with_ASM1_ADM1.py
@@ -249,7 +249,6 @@ class TestFullFlowsheet:
         # Check condition number to confirm scaling
         jac, _ = get_jacobian(m.rescaled_model, scaled=False)
         assert (jacobian_cond(jac=jac, scaled=False)) == pytest.approx(
-            3.21649836e11,
-            # 2.71713e11,
+            2.39806851627e11,
             rel=1e-3,
         )
